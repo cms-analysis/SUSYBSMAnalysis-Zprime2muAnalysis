64f9c13c08f26add15147274961ef6d899a5291f
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2007-08-30 16:52:03 +0000

    update fwk

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -5,7 +5,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 DEFINE_FWK_MODULE(Zprime2muAnalysis);
-DEFINE_ANOTHER_FWK_MODULE(Zprime2muResolution);
-DEFINE_ANOTHER_FWK_MODULE(Zprime2muAsymmetry);
-DEFINE_ANOTHER_FWK_MODULE(Zprime2muMassReach);
+DEFINE_FWK_MODULE(Zprime2muResolution);
+DEFINE_FWK_MODULE(Zprime2muAsymmetry);
+DEFINE_FWK_MODULE(Zprime2muMassReach);
 

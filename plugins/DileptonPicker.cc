dbdd41e2024f03a9a339ff3e98b87f9a87583aa5
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2008-05-25 08:53:16 +0000

    Module to filter the output of CandCombiner, applying selection cuts and removing lepton overlap.

---
+++ plugins/DileptonPicker.cc
@@ -0,0 +1,90 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "SUSYBSMAnalysis/Zprime2muAnalysis/src/DileptonUtilities.h"
+#include "SUSYBSMAnalysis/Zprime2muAnalysis/src/TeVMuHelper.h"
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+class DileptonPicker : public EDProducer {
+public:
+  explicit DileptonPicker(const ParameterSet&);
+  ~DileptonPicker() {}
+  
+private:
+  virtual void beginJob(const EventSetup&) {}
+  virtual void produce(Event&, const EventSetup&);
+  virtual void endJob() {}
+  
+  InputTag src;
+  bool doingGen;
+  unsigned maxDileptons;
+  vector<int> pdgIds;
+};
+
+DileptonPicker::DileptonPicker(const ParameterSet& cfg)
+  : src(cfg.getParameter<InputTag>("src")),
+    doingGen(cfg.getParameter<bool>("doingGen")),
+    maxDileptons(cfg.getParameter<unsigned>("maxDileptons")),
+    pdgIds(cfg.getParameter<vector<int> >("pdgIds"))
+{
+  produces<CompositeCandidateCollection>();
+}
+
+void DileptonPicker::produce(Event& event, const EventSetup& eSetup) {
+  static const bool debug = false;
+
+  Handle<CompositeCandidateCollection> hdileptons;
+  bool ok = true;
+  try {
+    event.getByLabel(src, hdileptons);
+  } catch (...) {
+    ok = false;
+  }
+
+  // Make the output collection.
+  auto_ptr<CompositeCandidateCollection>
+    dileptons(new CompositeCandidateCollection);
+
+  ostringstream out;
+
+  if (ok && !hdileptons.failedToGet()) {
+    if (hdileptons->size() > 0) {
+      if (debug) out << hdileptons->size() << " dileptons before selection and overlap removal; ";
+
+      // Default cuts are pT < 20 GeV and isolation sumPt < 10.
+      const unsigned cuts = TeVMuHelper::PT | TeVMuHelper::ISO;
+      cutDileptons(*hdileptons, *dileptons, cuts, doingGen,
+		   pdgIds[0], pdgIds[1], debug);
+
+      if (debug) out << dileptons->size() << " dileptons before overlap removal; ";
+
+      // Remove dileptons of lower invariant mass that are comprised of a
+      // lepton that has been used by a higher invariant mass one.
+      if (dileptons->size() > 1)
+	removeDileptonOverlap(*dileptons, debug);
+
+      if (dileptons->size() > maxDileptons)
+	dileptons->erase(dileptons->begin() + maxDileptons, dileptons->end());
+      
+      if (debug) out << dileptons->size() << " dileptons kept.";
+    }
+    else {
+      if (debug) out << "No dileptons in source collection.";
+    }
+    
+    if (debug) LogInfo("DileptonPicker") << out.str();
+  }
+  else
+    LogWarning("DileptonPicker")
+      << "no collection " << src << " in event; producing empty collection";
+
+  event.put(dileptons);
+}
+
+DEFINE_FWK_MODULE(DileptonPicker);

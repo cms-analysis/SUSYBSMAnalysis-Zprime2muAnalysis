41ff62f43c2cb3e1d7ec8bc7e86c9d570e8518b5
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2008-05-25 09:30:06 +0000

    Experimental cuts (not used by default).

--- src/TeVMuHelper.h
+++ src/TeVMuHelper.h
@@ -3,35 +3,70 @@
 
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
 
 #include "SUSYBSMAnalysis/Zprime2muAnalysis/src/ToConcrete.h"
 
 class TeVMuHelper {
 public:
-  TeVMuHelper() : ptCut(20), isoCut(10) {}
+  enum CutResult {PASS, PT=0x01, ISO=0x02, CHI2DOF=0x04, D0=0x08, NSIHITS=0x10}; // To go in bitfields.
 
-  enum CutResult {PASS, PT, ISO};
+  // Default values are what was used in the 2007 AN (only applying pT
+  // and isolation cuts).
+  TeVMuHelper() :
+    ptCut(20), isoCut(10), chi2dofCut(5),
+    d0Cut(0.25), nSiHitsCut(7),
+    cutMask(PT | ISO) {}
+
+  void setCutMask(const unsigned mask) {
+    cutMask = mask;
+  }
 
   unsigned leptonIsCut(const reco::Candidate& lepton) const {
     unsigned retval = 0;
 
     // pT cut.
-    if (lepton.pt() <= ptCut)
+    if ((cutMask & PT) && lepton.pt() <= ptCut)
       retval |= PT;
 
-    // Sum pT in cone of dR=0.3 cut
     // Try to cast to reco::Muon.
     const reco::Muon* muon = toConcretePtr<reco::Muon>(lepton);
-    if (muon != 0 && muon->isIsolationValid() &&
-	muon->getIsolationR03().sumPt > isoCut)
-      retval |= ISO;
+    if (muon != 0) {
+      // Sum pT in cone of dR=0.3 cut
+      if ((cutMask & ISO) && muon->isIsolationValid() &&
+	  muon->getIsolationR03().sumPt > isoCut)
+	retval |= ISO;
+      
+      const reco::TrackRef& tk = muon->combinedMuon();
+      if (tk.isNonnull()) {
+	// Cut on chi2/dof.
+	if ((cutMask & CHI2DOF) && tk->normalizedChi2() > chi2dofCut)
+	  retval |= CHI2DOF;
 
-    return retval;
+	// Cut on d0.
+	if ((cutMask & D0) && fabs(tk->d0()) > d0Cut) // 2.5 mm
+	  retval |= D0;
+      }
+
+      const reco::TrackRef& tktk = muon->track();
+      // Cut on number of silicon hits for the tracker track.
+      if ((cutMask & NSIHITS) && tktk.isNonnull() &&
+	  tktk->numberOfValidHits() < nSiHitsCut)
+	retval |= NSIHITS;
+    }
+  
+    return retval & cutMask;
   }
 
 private:
   double ptCut;
   double isoCut;
+  double chi2dofCut;
+  double d0Cut;
+  int nSiHitsCut;
+
+  unsigned cutMask;
 };
 
 #endif

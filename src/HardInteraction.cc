e7ee4dd8991557af280b1224b4bdf3c0805a5da6
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2008-05-25 08:59:58 +0000

    Split out generator-event-record hard interaction extraction from Zprime2muAnalysis class.

---
+++ src/HardInteraction.cc
@@ -0,0 +1,194 @@
+#include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
+
+#include "SUSYBSMAnalysis/Zprime2muAnalysis/src/HardInteraction.h"
+
+using namespace std;
+using namespace reco;
+
+HardInteraction::HardInteraction(int lepFlavor, //vector<int> resIds,
+				 bool allowFakeRes)
+  : quark(0), resonance(0), lepPlus(0), lepMinus(0),
+    lepPlusNoIB(0), lepMinusNoIB(0),
+    leptonFlavor(lepFlavor), //resonanceIds(resIds),
+    allowFakeResonance(allowFakeRes), resonanceIsFake(false)
+{
+  // Do nothing here, since we want to be able to throw on error when
+  // filling the class (and it's very bad to throw from a
+  // constructor!)
+}
+
+HardInteraction::~HardInteraction() {
+  // If we faked the resonance by adding the l+ and l-, we own its
+  // pointer, so delete it. Caution: we own none of the other
+  // pointers, since they come directly from the genParticles
+  // collection!
+  if (resonanceIsFake)
+    delete resonance;
+}
+
+/*
+void SetResonanceIds(const std::vector<int>& resIds) {
+  resonanceIds = resIds;
+}
+*/
+
+bool HardInteraction::IsResonance(int pdgId) {
+  /*
+  vector<int>::const_iterator p = resonanceIds.begin();
+  for (; p != resonanceIds.end(); p++)
+    if (*p == pdgId)
+      return true;
+  return false;
+  */
+
+  // Z', Z0/gamma*, G, or G*
+  return pdgId == 32 || pdgId == 23 || pdgId == 39 || pdgId == 5000039;
+}
+
+void HardInteraction::Fill(const edm::Event& event) {
+  edm::Handle<CandidateCollection> genParticles;
+  event.getByLabel("genParticleCandidates", genParticles);
+  Fill(*genParticles);
+}
+
+void HardInteraction::Fill(const CandidateCollection& genParticles) {
+  // Clear out the list of brem photons.
+  bremPhotons.clear();
+
+  // Look in the doc lines for the hard-interaction resonance and
+  // leptons.
+  CandidateCollection::const_iterator genp = genParticles.begin();
+  for (; genp != genParticles.end(); genp++) {
+    if (genp->status() == 3) {
+      int pdgId = genp->pdgId();
+
+      if (IsResonance(pdgId)) {
+	// We found the resonance (Z0/Z'/etc.). Make sure we didn't
+	// find a second one.
+	if (resonance != 0)
+	  throw cms::Exception("HardInteraction")
+	    << "Found second resonance (pdgId: " << pdgId << ") in event!\n";
+
+	resonance = &*genp;
+      }
+      else if (pdgId == leptonFlavor) {
+	// We found the l-. Make sure we didn't find a second one.
+	if (lepMinus != 0)
+	  throw cms::Exception("HardInteraction")
+	    << "Found second l- in event!\n";
+
+	lepMinusNoIB = &*genp;
+      }
+      else if (pdgId == -leptonFlavor) {
+	// We found the l+. Make sure we didn't find a second one.
+	if (lepPlus != 0)
+	  throw cms::Exception("HardInteraction")
+	    << "Found second l+ in event!\n";
+
+	lepPlusNoIB = &*genp;
+      }
+    }
+  }
+
+  // We should always find the l+ and l-.
+  if (lepMinusNoIB == 0 || lepPlusNoIB == 0)
+    throw cms::Exception("HardInteraction")
+      << "Couldn't find at least one of the pre-brem l-l+! l- = "
+      << lepMinusNoIB << " l+ = " << lepPlusNoIB << endl;
+
+  // We get the quark/antiquark from either the resonance or directly
+  // from one of the leptons (see below). Start by assuming it is from
+  // the resonance (we'll check for the null pointer later if this is
+  // not true).
+  const Candidate* mothersAreQuarks = resonance;
+
+  // Some COMPHEP samples do not put the resonance back into the HepMC
+  // record. So, if we didn't find a resonance, and are allowed to,
+  // build one (we really only care about its four vector and charge
+  // at this point). Use the doc-line l+l- we found to do so.
+  if (resonance == 0) {
+    if (allowFakeResonance) {
+      Particle::Charge q = lepPlusNoIB->charge() + lepMinusNoIB->charge();
+      Particle::LorentzVector p4 = lepPlusNoIB->p4() + lepMinusNoIB->p4();
+      // Don't know the creation vertex of the resonance. Set to (0,0,0)
+      // by default.
+      Particle::Point vtx;
+      // We also don't know what the pdgId of the resonance was. Pass
+      // one in?
+      int pdgId = 99;      
+      int status = 3;
+    
+      // We have ownership of this pointer, and we will delete it in our
+      // destructor.
+      resonance = new GenParticleCandidate(q, p4, vtx, pdgId, status, true);
+      resonanceIsFake = true;
+
+      // In these resonanceless COMPHEP samples, the quark/antiquark are
+      // direct mothers of the leptons. So now, assume we can get them
+      // from the l-.
+      mothersAreQuarks = lepMinusNoIB;
+    }
+    else
+      throw cms::Exception("HardInteraction")
+	<< "Did not find the resonance in the event, and forbidden"
+	<< " from faking it!\n";
+  }
+
+  // Did we successfully identify a Candidate that has the
+  // quark/antiquark as its mothers? Try to get them.
+  if (mothersAreQuarks != 0 && mothersAreQuarks->numberOfMothers() == 2) {
+    for (unsigned m = 0; m < 2; m++) {
+      const Candidate* mom = mothersAreQuarks->mother(m);
+      int momId = mom->pdgId();
+
+      // For now, don't count gluons (id = 21), but we should
+      // implement them later here and in all the above (for gg->G*).
+      if (abs(momId) > 6 || momId == 0)
+	throw cms::Exception("HardInteraction")
+	  << "Mother " << m << " is not a quark! its pdgId = "
+	  << momId << endl;
+      
+      if (momId > 0)
+	quark = mom;
+      else
+	antiquark = mom;
+    }
+  }
+  
+  // If we didn't find the quark or antiquark, bomb.
+  if (quark == 0 || antiquark == 0)
+    throw cms::Exception("HardInteraction")
+      << "Couldn't find at least one of the quark/antiquark! quark = "
+      << quark << " antiquark = " << antiquark << endl;
+
+  // Now, pick up the leptons after brem. They are daughter leptons of
+  // the doc-line leptons and have status = 1, i.e. they are
+  // final-state. Also grab the brem photons.
+  for (Candidate::const_iterator dau = lepMinusNoIB->begin();
+       dau != lepMinusNoIB->end(); dau++) {
+    if (dau->status() == 1) {
+      int pdgId = dau->pdgId();
+      if (pdgId == leptonFlavor)
+	lepMinus = &*dau;
+      else if (pdgId == 22)
+	bremPhotons.push_back(&*dau);
+    }
+  }
+
+  for (Candidate::const_iterator dau = lepPlusNoIB->begin();
+       dau != lepPlusNoIB->end(); dau++) {
+    if (dau->status() == 1) {
+      int pdgId = dau->pdgId();
+      if (pdgId == -leptonFlavor)
+	lepPlus = &*dau;
+      else if (pdgId == 22)
+	bremPhotons.push_back(&*dau);
+    }
+  }
+
+  // If we didn't find the final-state l- or l+, bomb.
+  if (lepMinus == 0 || lepPlus == 0)
+    throw cms::Exception("HardInteraction")
+      << "Couldn't find at least one of the final-state l-l+! l- = "
+      << lepMinus << " l+ = " << lepPlus << endl;
+}
---
+++ src/HardInteraction.h
@@ -0,0 +1,62 @@
+#ifndef HARDINTERACTION_H
+#define HARDINTERACTION_H
+
+#include <vector>
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
+#include "FWCore/Framework/interface/Event.h"
+
+// A class for extracting and storing the generator-level particles
+// that were in the hard interaction (+ the final-state leptons after
+// bremsstrahlung) from the MC record, assuming the event was of the
+// form q qbar -> resonance -> l+l-, and that there was only one hard
+// interaction per event. We store pointers to all these particles.
+
+struct HardInteraction {
+  HardInteraction(int lepFlavor, // std::vector<int> resIds,
+		  bool allowFakeRes=false);
+  ~HardInteraction();
+
+  // Set the list of qualifying resonance ids.
+  //static void SetResonanceIds(const std::vector<int>& resIds);
+
+  // Determine whether the passed in pdgId is one of the resonances of
+  // interest (currently one of Z0 (inc. DY), Z', or G*).
+  static bool IsResonance(int pdgId);
+
+  // Store pointers to all the particles from the genParticles collection.
+  void Fill(const reco::CandidateCollection& genParticles);
+  
+  // The same, but get the genParticles from the event.
+  void Fill(const edm::Event& event);
+
+  // Pointers to the particles in the hard interaction (as well as the
+  // final-state leptons after brem.)
+  const reco::Candidate* quark;
+  const reco::Candidate* antiquark;
+  const reco::Candidate* resonance;
+  const reco::Candidate* lepPlus;
+  const reco::Candidate* lepMinus;
+  const reco::Candidate* lepPlusNoIB;
+  const reco::Candidate* lepMinusNoIB;
+  std::vector<const reco::Candidate*> bremPhotons;
+
+  // The lepton flavor to look for (e.g. 13 for muons, 11 for electrons).
+  int leptonFlavor;
+
+  // The PDG ids of the resonances of interest.
+  // JMTBAD Hard-coded for now in isResonance().
+  //static std::vector<int> resonanceIds;
+
+  // Whether to allow the building of the resonance from the doc-line
+  // leptons if the resonance is not found in the event record (this
+  // is the case for at least some COMPHEP samples).
+  bool allowFakeResonance;
+
+  // Flag declaring whether we built the resonance ourselves, and
+  // therefore own and should delete its pointer at destruction.
+  bool resonanceIsFake;
+};
+
+#endif

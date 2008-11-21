57607e2468d9a9b2058b417198a96713f61638ec
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2008-11-21 15:10:47 +0000

    Development from branch20X.

--- src/TriggerDecision.cc
+++ src/TriggerDecision.cc
@@ -3,6 +3,7 @@
 #include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMapRecord.h"
 #include "DataFormats/HLTReco/interface/TriggerEvent.h"
 #include "DataFormats/HLTReco/interface/TriggerObject.h"
+#include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
 
 #include "FWCore/Framework/interface/TriggerNames.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -15,39 +16,37 @@ void TriggerDecision::init(const edm::ParameterSet& config,
 			   const bool dbg) {
   debug = dbg;
 
-  doingElectrons = config.getParameter<bool>("doingElectrons");
-  useTrigger     = config.getParameter<bool>("useTrigger");
-  l1GtObjectMap  = config.getParameter<edm::InputTag>("l1GtObjectMap");
-  hltResults     = config.getParameter<edm::InputTag>("hltResults");
+  doingElectrons     = config.getParameter<bool>("doingElectrons");
+  useTrigger         = config.getParameter<bool>("useTrigger");
+  l1GtObjectMap      = config.getParameter<edm::InputTag>("l1GtObjectMap");
+  hltResults         = config.getParameter<edm::InputTag>("hltResults");
 
   if (useTrigger) {
     if (!doingElectrons) {
       // Level-1 paths we want to use for the trigger decision.
       l1Paths.push_back("L1_SingleMu7");
       l1Paths.push_back("L1_DoubleMu3");
-      
+
       // Level-2 paths (actually, the names of the modules ran)
       hltModules[0].push_back("SingleMuNoIsoL2PreFiltered");
       hltModules[0].push_back("DiMuonNoIsoL2PreFiltered");
       // Level-3 paths (module names)
       hltModules[1].push_back("SingleMuNoIsoL3PreFiltered");
       hltModules[1].push_back("DiMuonNoIsoL3PreFiltered");
-      
+
       // HLT paths (the logical ANDs of L2 and L3 single/dimuon paths
-      // above)
-      hltPaths.push_back("HLT_Mu15_L1Mu7"); // Closest to HLT1MuonNonIso from 1_6_X.
-      hltPaths.push_back("HLT_DoubleMu3");
+      // above) in 2E30 menu.
+      hltPaths.push_back("HLT_Mu15");      // former HLT1MuonNonIso
+      hltPaths.push_back("HLT_DoubleMu3"); // former HLT2MuonNonIso
     }
     else {
       l1Paths.push_back("L1_SingleEG15");
-      
+
       // For now, just look at the overall HLT decision for electrons.
       // JMTBAD trigger names have changed; which are the "right" ones for electrons?
-      /*
       hltPaths.push_back("HLT_EM80");
       hltPaths.push_back("HLT_EM200");
       //hltPaths.push_back("HLT1ElectronRelaxed");
-      */
     }
   }
 }
@@ -104,22 +103,30 @@ void TriggerDecision::storeHLTDecision(const edm::Event& event) {
   // path decisions can be extracted.
   edm::Handle<edm::TriggerResults> hltRes;
   event.getByLabel(hltResults, hltRes);
-  
+
   // Get the map between path names and indices.
   edm::TriggerNames hltTrigNames(*hltRes);
-  
+
   ostringstream out;
   if (debug) out << "storeHLTDecision:\n";
 
   // Get the official HLT results, and pack them.
   unsigned int hlt_trigbits = 0;
+  int paths_defined = hltRes->size();
   for (unsigned int i = 0; i < hltPaths.size(); i++) {
     int ndx = hltTrigNames.triggerIndex(hltPaths[i]);
-    bool fired = hltRes->accept(ndx);
-    if (debug)
-      out << "  " << hltPaths[i] << " (index " << ndx << "): " 
-	  << " decision " << fired << endl;
-    if (fired) hlt_trigbits |= 1 << i;
+    if (ndx >= paths_defined) {
+      edm::LogWarning("storeHLTDecision")
+	<< "+++ HLT path " << hltPaths[i]
+	<< " is not available in TriggerResults object; skipping it... +++";
+    }
+    else {
+      bool fired = hltRes->accept(ndx);
+      if (debug)
+	out << "  " << hltPaths[i] << " (index " << ndx << "): " 
+	    << " decision " << fired << endl;
+      if (fired) hlt_trigbits |= 1 << i;
+    }
   }
 
   // In the new HLT data model, filter decisions and objects firing
@@ -131,13 +138,55 @@ void TriggerDecision::storeHLTDecision(const edm::Event& event) {
   // TriggerObjects are in HLTDEBUG.) So, skip extracting L2 and L3
   // decisions separately for now.
 
-  //edm::Handle<trigger::TriggerEvent> trigEvent;
-  //event.getByLabel("hltTriggerSummaryAOD", trigEvent);
-
   trigWord[l2] = trigWord[l3] = hlt_trigbits;
   passTrig[l2] = passTrig[l3] = hlt_trigbits != 0;
   for (int l = l2; l <= l3; l++) 
     out << "  trigWord[l" << l << "]: " << trigWord[l] << endl;
+
+  edm::Handle<trigger::TriggerEvent> trigEvent;
+  event.getByLabel("hltTriggerSummaryAOD", trigEvent);
+  if (trigEvent.isValid()) {
+    // Get indices of L2 and L3 muon objects.
+    int l2ind_first = -99, l2ind_last = -99;
+    int l3ind_first = -99, l3ind_last = -99;
+    int ind_prev = 0;
+    const trigger::size_type nC(trigEvent->sizeCollections());
+    for (trigger::size_type iC = 0; iC < nC; ++iC) {
+      std::string encodedCollectionTag(trigEvent->collectionTag(iC).encode());
+      if (strstr(encodedCollectionTag.c_str(), "hltL2MuonCandidates")) {
+	l2ind_first = ind_prev;
+	l2ind_last  = trigEvent->collectionKey(iC)-1;
+      }
+      else if (strstr(encodedCollectionTag.c_str(), "hltL3MuonCandidates")) {
+	l3ind_first = ind_prev;
+	l3ind_last  = trigEvent->collectionKey(iC)-1;
+      }
+      ind_prev = trigEvent->collectionKey(iC);
+    }
+
+    // Access collections of L2 and L3 muons.  Shall we convert them to
+    // reco::muons, similarly to what is done in L3MuonSanitizer.cc, and
+    // save into allLeptons?
+    const trigger::TriggerObjectCollection& TOC(trigEvent->getObjects());
+    if (!doingElectrons) {
+      if (l2ind_first >= 0 && l2ind_last >= l2ind_first) {
+	out << "  L2 muon(s): #, id, pt, eta, phi\n";
+	for (trigger::size_type iO = l2ind_first; iO <= l2ind_last; ++iO) {
+	  const trigger::TriggerObject& TO(TOC[iO]);
+	  out << "  " << iO << " " << TO.id() << " " << TO.pt()
+	      << " " << TO.eta() << " " << TO.phi() << "\n";
+	}
+      }
+      if (l3ind_first >= 0 && l3ind_last >= l3ind_first) {
+	out << "  L3 muon(s): #, id, pt, eta, phi\n";
+	for (trigger::size_type iO = l3ind_first; iO <= l3ind_last; ++iO) {
+	  const trigger::TriggerObject& TO(TOC[iO]);
+	  out << "  " << iO << " " << TO.id() << " " << TO.pt()
+	      << " " << TO.eta() << " " << TO.phi() << "\n";
+	}
+      }
+    }
+  }
 
   if (debug) LogTrace("storeHLTDecision") << out.str();
 }

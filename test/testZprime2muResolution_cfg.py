4ec28fa182b4315661f092fb3323aa3c471ccf61
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2008-12-10 01:17:52 +0000

    Histos for data-only files, Resolution for files that include MC info.

--- test/testZprime2muResolution_cfg.py
+++ test/testZprime2muResolution_cfg.py
@@ -2,12 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muAnalysisCommon_cff import *
 from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muHistos_cfi import *
+from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muResolution_cfi import *
 
 files = [
     'file:/scratchdisk3/tucker/ZPSSMmumu_M1000_Mcut400_10TeV_IDEAL_V9_RAW2DIGI_RECO_1-10.root'
     ]
 
 process = makeZprime2muAnalysisProcess(files, skipPAT=True, electrons=([None]*9))
-attachHistos(process)
 
-process.Zprime2muHistos.verbosity = 2
+attachHistos(process) # Run Histos, too.
+attachResolution(process)
+
+process.Zprime2muResolution.verbosity = 2

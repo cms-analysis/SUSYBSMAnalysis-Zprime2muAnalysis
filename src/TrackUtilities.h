700fb8a86b2350c158ba9988b086ac8b8d0e6d7f
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2008-12-17 16:35:14 +0000

    Track error functions are now just inline wrappers.

--- src/TrackUtilities.h
+++ src/TrackUtilities.h
@@ -1,30 +1,29 @@
 #ifndef TRACKUTILITIES_H
 #define TRACKUTILITIES_H
 
-#include "DataFormats/Candidate/interface/CandidateFwd.h"
-#include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
 
-// Track utility functions
+// Get the error on the inverse quantity by simple error propagation.
+inline double invError(double val, double err) {
+  return 1/val/val*err;
+} 
 
-// There is no pError() in Track/TrackBase; calculate error on p
-// ourselves from error on qoverp().
-double pError(const reco::Track* cand);
-// There is a ptError() in Track/TrackBase but for symmetry with the
-// above let's have another.
-double ptError(const reco::Track* cand);
+// Track error wrapper functions.
+
+inline double pError(const reco::Track* tk) {
+  return invError(1/tk->p(), tk->qoverpError());
+}
+
+inline double ptError(const reco::Track* tk) {
+  return tk->ptError();
+}
+
+inline double invPtError(const reco::Track* tk) {
+  return invError(tk->pt(), tk->ptError());
+}
   
-// Propagate inverse errors...
-inline double invError(double val, double err) { return 1/val/val*err; } 
-// ... to 1/pT
-double invPtError(const reco::Track* tk);
-// ... and 1/p
-double invPError(const reco::Track* tk);
-
-// Return whether cand1 and cand2 are "close"; instead of a circle
-// in eta-phi space, we look at a square .5 on a side.
-bool matchTracks(const reco::CandidateBaseRef& cand1,
-		 const reco::CandidateBaseRef& cand2);
+inline double invPError(const reco::Track* tk) {
+  return tk->qoverpError();
+}
 
 #endif

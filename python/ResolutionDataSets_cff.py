329a48933c759d71b93c5fe1dfa3570ca82c821a
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2009-02-17 16:07:38 +0000

    Add some useful dataset parameters.

--- python/ResolutionDataSets_cff.py
+++ python/ResolutionDataSets_cff.py
@@ -2,6 +2,38 @@ import FWCore.ParameterSet.Config as cms
 
 dataSets = cms.PSet(
 
+SinglePt10 = cms.PSet(
+  peakMass = cms.double(30),
+  lowerMassWin = cms.double(0.0),
+  upperMassWin = cms.double(100.0),
+  binSize = cms.int32(2),
+  maxTrigMass = cms.double(0.1) # in TeV
+),
+
+SinglePt100 = cms.PSet(
+  peakMass = cms.double(200),
+  lowerMassWin = cms.double(40.0),
+  upperMassWin = cms.double(500.0),
+  binSize = cms.int32(50),
+  maxTrigMass = cms.double(0.3) # in TeV
+),
+
+SinglePt1000 = cms.PSet(
+  peakMass = cms.double(2000),
+  lowerMassWin = cms.double(40.0),
+  upperMassWin = cms.double(500.0),
+  binSize = cms.int32(50),
+  maxTrigMass = cms.double(0.3) # in TeV
+),
+
+SinglePtFlat = cms.PSet(
+  peakMass = cms.double(2000),
+  lowerMassWin = cms.double(40.0),
+  upperMassWin = cms.double(500.0),
+  binSize = cms.int32(50),
+  maxTrigMass = cms.double(0.3) # in TeV
+),
+
 DY40 = cms.PSet(
   peakMass = cms.double(91.188),
   lowerMassWin = cms.double(40.0),
@@ -12,18 +44,34 @@ DY40 = cms.PSet(
 
 DY200 = cms.PSet(
   peakMass = cms.double(200.0),
-  lowerMassWin = cms.double(0.0),
-  upperMassWin = cms.double(1000.0),
+  lowerMassWin = cms.double(100.0),
+  upperMassWin = cms.double(800.0),
   binSize = cms.int32(50),
-  maxTrigMass = cms.double(5.4),
+  maxTrigMass = cms.double(0.8),
 ),
 
 DY500 = cms.PSet(
   peakMass = cms.double(500.0),
-  lowerMassWin = cms.double(0.0),
-  upperMassWin = cms.double(1000.0),
+  lowerMassWin = cms.double(300.0),
+  upperMassWin = cms.double(1200.0),
   binSize = cms.int32(50),
-  maxTrigMass = cms.double(5.4),
+  maxTrigMass = cms.double(1.2),
+),
+
+DY1000 = cms.PSet(
+  peakMass = cms.double(1000.0),
+  lowerMassWin = cms.double(700.0),
+  upperMassWin = cms.double(2000.0),
+  binSize = cms.int32(50),
+  maxTrigMass = cms.double(2.0),
+),
+
+DY2000 = cms.PSet(
+  peakMass = cms.double(2000.0),
+  lowerMassWin = cms.double(1200.0),
+  upperMassWin = cms.double(3500.0),
+  binSize = cms.int32(50),
+  maxTrigMass = cms.double(3.5),
 ),
 
 Zp1000 = cms.PSet(

e34315f1f33ef0534edf31c4009957d8b6980538
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-06-21 08:41:02 +0000

    In new versions, CMSSW is strict about EDAnalyzer != EDFilter in python.

--- python/AsymmetryParametrizer_cfi.py
+++ python/AsymmetryParametrizer_cfi.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 from AsymFitManager_cff import AsymFitManager
 
-AsymmetryParametrizer = cms.EDAnalyzer('AsymmetryParametrizer',
-                                       AsymFitManager,
-                                       gen_particle_src = cms.InputTag('genParticles'),
-                                       lepton_flavor = cms.int32(13),
-                                       internal_brem_on = cms.bool(True),
-                                       assemble_only = cms.bool(False),
-                                       histos_fn = cms.string('AsymmetryParametrizer.root'),
-                                       postscript_fn = cms.string('AsymmetryParametrizer.ps'),
-                                       )
+AsymmetryParametrizer = cms.EDFilter('AsymmetryParametrizer',
+                                     AsymFitManager,
+                                     gen_particle_src = cms.InputTag('genParticles'),
+                                     lepton_flavor = cms.int32(13),
+                                     internal_brem_on = cms.bool(True),
+                                     assemble_only = cms.bool(False),
+                                     histos_fn = cms.string('AsymmetryParametrizer.root'),
+                                     postscript_fn = cms.string('AsymmetryParametrizer.ps'),
+                                     )

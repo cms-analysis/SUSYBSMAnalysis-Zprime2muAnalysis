c8bc5e77a3b04cba84a8e739dbcad26077af82cd
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-06-25 15:48:41 +0000

    Obsolete.

--- test/ntuplize.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muAnalysisCommon_cff import *
-
-files = ['file:/scratchdisk3/tucker/CMSSW_3_1_2/DYmumu_Mcut200-MC_31X_V3/PYTHIA6_DYmumu_Mcut200_10TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_HLT8E29_RECO_MC_31X_V3_1.root']
-process = makeZprime2muAnalysisProcess(files)
-dumpNtuple(process, files[0].replace('.root', '.zp2muntuple.root'))

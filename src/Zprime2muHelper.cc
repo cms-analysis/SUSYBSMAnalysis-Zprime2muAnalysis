5be1aa29da07ab6d6525c8dcf8b9bc0ba402e1aa
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-06-27 14:21:01 +0000

    Clean-up; implement resonanceP4().

--- src/Zprime2muHelper.cc
+++ src/Zprime2muHelper.cc
@@ -1,14 +1,12 @@
-#include "TStyle.h"
-
 #include "DataFormats/PatCandidates/interface/CompositeCandidate.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "SUSYBSMAnalysis/Zprime2muAnalysis/src/GeneralUtilities.h"
 #include "SUSYBSMAnalysis/Zprime2muAnalysis/src/Zprime2muHelper.h"
 
 Zprime2muHelper::Zprime2muHelper(const edm::ParameterSet& config) 
-  : verbosity(Verbosity(config.getUntrackedParameter<int>("verbosity", 0))),
-    maxDileptons(config.getParameter<unsigned>("maxDileptons")),
+  : maxDileptons(config.getParameter<unsigned>("maxDileptons")),
     doingElectrons(config.getParameter<bool>("doingElectrons")),
     useGen(config.getParameter<bool>("useGen")),
     useSim(config.getParameter<bool>("useSim")),
@@ -18,15 +16,12 @@ Zprime2muHelper::Zprime2muHelper(const edm::ParameterSet& config)
     leptonFlavor(doingElectrons ? 11 : 13),
     leptonMass(doingElectrons ? 0.000511 : 0.10566)
 {
-  InitROOT();
-
   // Hidden option for me (JMT) to make tkdiffing outputs when
   // validating new versions easier.
-  if (!config.getUntrackedParameter<bool>("dateHistograms", true))
-    gStyle->SetOptDate(0);
+  InitROOT(config.getUntrackedParameter<bool>("dateHistograms", true));
 
   if (useGen) hardInteraction.init(leptonFlavor, true);
-  if (useTrigger) trigDecision.init(config, verbosity >= V_SIMPLE);
+  if (useTrigger) trigDecision.init(config);
 }
 
 void Zprime2muHelper::initEvent(const edm::Event& event, const edm::EventSetup& eSetup) {
@@ -41,6 +36,35 @@ void Zprime2muHelper::initEvent(const edm::Event& event, const edm::EventSetup& 
 }
 
 reco::Particle::LorentzVector Zprime2muHelper::resonanceP4(const pat::CompositeCandidate& cand) const {
-  // JMTBAD todo, using the stored photon four-vectors in each daughter muon.
-  return cand.p4();
+  // Start with the p4 of the candidate, which is the sum of the
+  // daughter p4s by construction.
+  reco::Particle::LorentzVector p4 = cand.p4();
+
+  std::vector<int> used_already;
+
+  for (pat::CompositeCandidate::const_iterator dau = cand.begin(), daue = cand.end(); dau != daue; ++dau) {
+    // Only muons can have photons matched to them. Also, remember
+    // that the CompositeCandidates were formed using shallow clones,
+    // so need to get the daughters' masterClones to be able to get
+    // the original pat::Muon*.
+    const pat::Muon* mu = dynamic_cast<const pat::Muon*>(&*dau->masterClone());
+    if (mu == 0)
+      continue;
+
+    if (mu->hasUserInt("photon_index")) {
+      int ndx = mu->userInt("photon_index");
+
+      // Don't double count, in the case where two daughter muons
+      // ended up matching to the same photon.
+      if (std::find(used_already.begin(), used_already.end(), ndx) == used_already.end()) {
+	used_already.push_back(ndx);
+	const reco::Particle::LorentzVector* pho_p4 = mu->userData<reco::Particle::LorentzVector>("photon_p4");
+	if (pho_p4 == 0)
+	  throw cms::Exception("BadlyFormedPhotonUserData") << "candidate daughter had userInt for photon_index but no p4!\n";
+	p4 += *pho_p4;
+      }
+    }
+  }
+
+  return p4;
 }

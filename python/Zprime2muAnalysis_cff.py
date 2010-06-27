078b9938a89a8c66e153371235d6f60403909e89
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-06-27 14:19:04 +0000

    Note about the muon-photon matching.

--- python/Zprime2muAnalysis_cff.py
+++ python/Zprime2muAnalysis_cff.py
@@ -39,6 +39,12 @@ leptons = cms.EDProducer('Zprime2muLeptonProducer',
                          muon_photon_match_src = cms.InputTag('muonPhotonMatch')
                          )
 
+# JMTBAD muon-photon matching is done here using the default muon
+# momentum and not whichever refit momentum will be eventually used in
+# the analysis. For most muons this shouldn't make much difference as
+# the matching is in eta-phi, but if the default is really bad and the
+# selected refit manages to recover, the photon match may be
+# wrong. The photon brem recovery needs to be studied more anyway.
 muonPhotonMatch = cms.EDProducer('TrivialDeltaRViewMatcher',
                                  src     = cms.InputTag('cleanPatMuons'),
                                  matched = cms.InputTag('cleanPatPhotons'),

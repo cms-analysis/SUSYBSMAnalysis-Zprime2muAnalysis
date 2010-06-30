a602db6c59986a87f6021a84b3cbae473984bc2e
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-06-30 21:04:21 +0000

    - As we did before, include simTracks that have muons from decays-in-flight produced by GEANT into the collection of MC particles to which the reconstructed leptons will be matched by PAT.
    - Tweak the PAT MC matching so that the matching is solely in deltaR
    (the default PAT matching does not allow a match if gen q != reco q,
    or where abs(reco pT - gen pT)/gen pT > 0.5).
    
    - Default cut for selectedPatMuons is only 'isGlobalMuon'.

--- python/PATTuple_cfg.py
+++ python/PATTuple_cfg.py
@@ -24,6 +24,35 @@ process.GlobalTag.globaltag = cms.string('PlaceHolder::All')
 
 process.load('PhysicsTools.PatAlgos.patSequences_cff')
 
+# For muon and electron MC matching, want to be able to match to
+# decays-in-flight produced by GEANT, so make some GenParticles out of
+# the simTracks.
+process.simParticles = cms.EDProducer('PATGenCandsFromSimTracksProducer',
+                                      src = cms.InputTag('g4SimHits'),
+                                      setStatus = cms.int32(1),
+                                      filter = cms.string('(abs(pdgId) == 13 || abs(pdgId) == 11) && pt > 2')
+                                      )
+
+process.genSimParticles = cms.EDProducer('GenParticleMerger',
+                                         src = cms.VInputTag(cms.InputTag('genParticles'), cms.InputTag('simParticles'))
+                                         )
+
+# Before we prepend the above two modules to the patDefaultSequence,
+# save a copy of the old one to go back to later if the user calls
+# configure() to remove MC use.
+process.patDefaultSequenceBeforeZp2mu = process.patDefaultSequence
+process.patDefaultSequence = cms.Sequence(process.simParticles * process.genSimParticles * process.patDefaultSequence._seq)
+
+for x in (process.muonMatch, process.electronMatch):
+    # Use the new gen + sim MC particles created above.
+    x.matched = cms.InputTag('genSimParticles')
+
+    # PAT muon/electron-MC matching requires, in addition to deltaR < 0.5,
+    # the MC and reconstructed leptons to have the same charge, and (reco
+    # pt - gen pt)/gen pt < 0.5. Disable these two cuts.
+    x.checkCharge = False
+    x.maxDPtRel = 1e6
+
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContent
 process.out = cms.OutputModule('PoolOutputModule',
                                fileName = cms.untracked.string('pat.root'),
@@ -51,6 +80,6 @@ process.patMuons.embedTrack = True
 process.countPatMuons.minNumber = 1
 
 # Define simple quality cuts (analysis cuts to be done at the analysis level).
-process.selectedPatMuons.cut = 'isGlobalMuon && isTrackerMuon'
+process.selectedPatMuons.cut = 'isGlobalMuon'
 #process.selectedPatMuons.cut = 'isTrackerMuon && pt > 1 && p > 2.5 && innerTrack.hitPattern.numberOfValidTrackerHits > 12 && innerTrack.normalizedChi2 < 5 && abs(dB) < 0.5 && abs(dZ) < 5 && muonID("TMLastStationAngTight")'
 #process.selectedPatMuons.cut = 'muonID("GlobalMuonPromptTight") && muonID("TMOneStationLoose") && (globalTrack.hitPattern.numberOfValidMuonCSCHits + globalTrack.hitPattern.numberOfValidMuonDTHits) >= 1 && innerTrack.hitPattern.trackerLayersWithMeasurement >= 6'

dc9e956159aa106fbff92519dfee78cc3faec787
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-07-07 13:24:02 +0000

    Break out simple A_FB calculation routines; break out different frame analysis into its own module.

--- src/Zprime2muAsymmetry.h
+++ src/Zprime2muAsymmetry.h
@@ -7,9 +7,6 @@
 #include "TH2F.h"
 #include "TProfile.h"
 
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "SUSYBSMAnalysis/Zprime2muAnalysis/src/AsymFitData.h"
 #include "SUSYBSMAnalysis/Zprime2muAnalysis/src/TFMultiD.h"
 #include "SUSYBSMAnalysis/Zprime2muAnalysis/src/Zprime2muAnalysis.h"
@@ -28,19 +25,10 @@ class Zprime2muAsymmetry : public Zprime2muAnalysis {
   static const unsigned int nNormPoints;
   static const unsigned int nSmearPoints;
 
-  void bookFrameHistos();
   void bookFitHistos();
 
   void makeFakeData(int nEvents, double A_FB, double b);
   void smearGenData(const AsymFitData& data);
-
-  double calcAFBError(double f, double b);
-  void calcAsymmetry(double f, double b, double& A_FB, double& e_A_FB);
-  void calcAsymmetry(const TH1F* h_cos, double& A_FB, double& e_A_FB);
-  void calcAsymmetry(const TH1F* IdF, const TH1F* IdB, TH1F* IdA, fstream& out);
-
-  void calcFrameAsym();
-  void fillFrameHistos();
 
   void fillFitData(const edm::Event& event);
   void dumpFitData();
@@ -52,8 +40,6 @@ class Zprime2muAsymmetry : public Zprime2muAnalysis {
   TFMultiD* getNewFitFcn(int which);
   void fitAsymmetry();
 
-  void fitCosCS(TH1F* cos_hist, int params);
-  void drawFrameHistos();
   void drawFitHistos();
 
   TH1F* h_genCosNoCut;
@@ -64,50 +50,6 @@ class Zprime2muAsymmetry : public Zprime2muAnalysis {
   TH1F* AsymFitHistoGenSmeared[6];
   TH1F* AsymFitHistoGenByType[2][6];
   TH1F* AsymFitHistoRecByType[2][6];
-
-  TH1F* cosGJ[2][2];
-  TH1F* cosCS[2][2];
-  TH1F* cosBoost[2];
-  TH1F* cosW[2];
-  TH1F* cosCSRes;
-  TH2F* rap_vs_cosCS[2];
-  TH2F* rap3_vs_rap0;
-  TH1F* FMassGJ[2][2];
-  TH1F* FMassCS[2][2];
-  TH1F* BMassGJ[2][2];
-  TH1F* BMassCS[2][2];
-  TH1F* AMassGJ[2][2];
-  TH1F* AMassCS[2][2];
-  TH1F* FMassBoost[2];
-  TH1F* FMassW[2];
-  TH1F* BMassBoost[2];
-  TH1F* BMassW[2];
-  TH1F* AMassBoost[2];
-  TH1F* AMassW[2];
-  TH1F* FRapGJ[2][2];
-  TH1F* FRapCS[2][2];
-  TH1F* BRapGJ[2][2];
-  TH1F* BRapCS[2][2];
-  TH1F* ARapGJ[2][2];
-  TH1F* ARapCS[2][2];
-  TH1F* FRapBoost[2];
-  TH1F* FRapW[2];
-  TH1F* BRapBoost[2];
-  TH1F* BRapW[2];
-  TH1F* ARapBoost[2];
-  TH1F* ARapW[2];
-  TH1F* FPseudGJ[2];
-  TH1F* FPseudCS[2];
-  TH1F* BPseudGJ[2];
-  TH1F* BPseudCS[2];
-  TH1F* FPseudBoost[2];
-  TH1F* FPseudW[2];
-  TH1F* BPseudBoost[2];
-  TH1F* BPseudW[2];
-  TH1F* FMBoostCut[2][6];
-  TH1F* BMBoostCut[2][6];
-  TH1F* AsymMBoostCut[2][6];
-  TProfile* cosCS3_diffsq_vs_cosCS0;
 
   TH1F* h_cos_theta_true;
   TH1F* h_cos_theta_cs_acc;

cf72733f876715d7831b90312f4d041bfcf4bb27
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-07-08 20:35:53 +0000

    Orient for data.

--- test/PATTuple.py
+++ test/PATTuple.py
@@ -3,11 +3,13 @@
 import FWCore.ParameterSet.Config as cms
 
 from SUSYBSMAnalysis.Zprime2muAnalysis.PATTuple_cfg import process
+from SUSYBSMAnalysis.Zprime2muAnalysis.PATTools import configure
+configure(process, useMonteCarlo=False)
 
-#from SUSYBSMAnalysis.Zprime2muAnalysis.PATTools import configure
-#configure(process, muonsOnly=True) # useMonteCarlo=False)
+process.source.fileNames = ['file:csonia.root']
+process.GlobalTag.globaltag = 'GR10_P_V7::All'
 
-process.source.fileNames = ['/store/relval/CMSSW_3_6_3/RelValZMM/GEN-SIM-RECO/START36_V10-v1/0006/369DA6CE-5878-DF11-8688-0030486790BE.root']
-process.GlobalTag.globaltag = 'START36_V10::All'
+process.load('SUSYBSMAnalysis.Zprime2muAnalysis.goodData_cff')
+#process.selectedPatMuons.cut = 'muonID("GlobalMuonPromptTight") && muonID("TMOneStationLoose") && (globalTrack.hitPattern.numberOfValidMuonCSCHits + globalTrack.hitPattern.numberOfValidMuonDTHits) >= 1 && innerTrack.hitPattern.trackerLayersWithMeasurement >= 6'
 
-process.p = cms.Path(process.patDefaultSequence)
+process.p = cms.Path(process.goodData * process.patDefaultSequence)

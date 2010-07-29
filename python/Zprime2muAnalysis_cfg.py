48703eeef6bec6241cb574494947195cafcb5270
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-07-29 05:00:02 +0000

    process.options.wantSummary defined by default so it's easy to turn on in top-level configs.

--- python/Zprime2muAnalysis_cfg.py
+++ python/Zprime2muAnalysis_cfg.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process('Zprime2muAnalysis')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:pat.root'))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000

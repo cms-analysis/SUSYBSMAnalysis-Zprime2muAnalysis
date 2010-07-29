9069067d1cd1edde87d0420d05e8f6979469a424
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-07-29 04:55:06 +0000

    Move comment from PATTuple_cfg.py; clean up.

--- python/MuonPhotonMatch_cff.py
+++ python/MuonPhotonMatch_cff.py
@@ -1,13 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
+# JMTBAD muon-photon matching is done here using the default muon
+# momentum and not whichever refit momentum will be eventually used in
+# the analysis. For most muons this shouldn't make much difference as
+# the matching is in eta-phi, but if the default is really bad and the
+# selected refit manages to recover, the photon match may be
+# wrong. The photon brem recovery needs to be studied more anyway.
 muonPhotonMatch = cms.EDProducer('TrivialDeltaRViewMatcher',
                                  src     = cms.InputTag('cleanPatMuons'),
                                  matched = cms.InputTag('cleanPatPhotons'),
                                  distMin = cms.double(0.1)
                                  )
 
-## Helper function to add this info into a pat::Muon
-def addUserData(patMuonProducer, label="muonPhotonMatch"):
-    patMuonProducer.userData.userCands.src += [
-        cms.InputTag(label,"")
-        ]
+def addUserData(patMuonProducer, tag=cms.InputTag('muonPhotonMatch')):
+    patMuonProducer.userData.userCands.src.append(tag)

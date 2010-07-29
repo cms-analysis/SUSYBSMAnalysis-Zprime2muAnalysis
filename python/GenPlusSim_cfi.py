60b8b270a2763120fa3b770d763b2e1835d97742
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-07-29 04:54:43 +0000

    Need HepPDTESSource; remove use of PATGenCandsFromSimTracksProducer and just use GenPlusSimParticleProducer, since the latter seems to work better with the Pruner; tweak the pruner keep statements.

--- python/GenPlusSim_cfi.py
+++ python/GenPlusSim_cfi.py
@@ -1,42 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
+from SimGeneral.HepPDTESSource.pythiapdt_cfi import HepPDTESSource
+
 genSimLeptons = cms.EDProducer(
     "GenPlusSimParticleProducer",
     src           = cms.InputTag("g4SimHits"), # use "famosSimHits" for FAMOS
     setStatus     = cms.int32(8),             # set status = 8 for GEANT GPs
-    particleTypes = cms.vstring("mu+","e-"),
     filter        = cms.vstring('(abs(pdgId) == 13 || abs(pdgId) == 11) && pt > 2'),  # just for testing (optional)
     genParticles  = cms.InputTag("genParticles") # original genParticle list
     )
 
-prunedGenSimLeptons = cms.EDProducer(
-    "GenParticlePruner",
-    src = cms.InputTag("genParticles"),
-    select = cms.vstring(
-    "drop * ",
-    "keep status <= 3",
-    "++keep pdgId = {mu-} && pt > 2",
-    "++keep pdgId = {e-} && pt > 2",
-    "drop pdgId == 21 && status = 2"
-    #"+drop pdgId = {mu-}",
-    #"+drop pdgId = {e-}"
-    )
-    )
+prunedGenSimLeptons = cms.EDProducer('GenParticlePruner',
+                                     src = cms.InputTag('genSimLeptons'),
+                                     select = cms.vstring(
+                                         'drop *',
+                                         # keep all doc-line particles (e.g. the Z0/Z', its parents, daughters, etc.)
+                                         'keep status == 3',
+                                         # keep any final-state muon (either from the generator with status 1
+                                         # or from GEANT with status 8 as set above) that has potential to
+                                         # reach the muon system (should reevaluate for muons produced far from
+                                         # the IP), and all of their ancestors
+                                         '++keep abs(pdgId) == 13 && (status == 1 || status == 8) && pt > 2',
+                                         # keep any final-state non-GEANT electron and all of their ancestors
+                                         '++keep abs(pdgId) == 11 && status == 1'
+                                         )
+                                     )
 
-allGenSimMergerSequence = cms.Sequence(genSimLeptons + prunedGenSimLeptons)
-
-simParticles = cms.EDProducer(
-    'PATGenCandsFromSimTracksProducer',
-    src = cms.InputTag('g4SimHits'),
-    setStatus = cms.int32(8),
-    filter = cms.string('(abs(pdgId) == 13 || abs(pdgId) == 11) && pt > 2')
-    )
-
-genSimParticles = cms.EDProducer(
-    'GenParticleMerger',
-    src = cms.VInputTag(
-    cms.InputTag('genParticles'),
-    cms.InputTag('simParticles'))
-    )
-
-genSimMergerSequence = cms.Sequence(simParticles + genSimParticles)
+genSimSequence = cms.Sequence(genSimLeptons + prunedGenSimLeptons)

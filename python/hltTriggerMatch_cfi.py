1dcd324819f3d605c9f7f32b8b8ca429096a86d1
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-09-15 23:48:30 +0000

    Configure the matcher to use those trigger objects that include our highest-pT single muon path (currently HLT_Mu9).

--- python/hltTriggerMatch_cfi.py
+++ python/hltTriggerMatch_cfi.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-muonTriggerMatchHLTMuons = cms.EDProducer( "PATTriggerMatcherDRDPtLessByR",
-    src     = cms.InputTag( "cleanPatMuons" ),
-    matched = cms.InputTag( "patTrigger" ),
-    andOr          = cms.bool( False ),
-    filterIdsEnum  = cms.vstring( 'TriggerMuon' ), # 'TriggerMuon' is the enum from trigger::TriggerObjectType for HLT muons
-    filterIds      = cms.vint32( 0 ), # '83' would be the code for HLT muons here.
-    filterLabels   = cms.vstring( '*' ),
-    pathNames      = cms.vstring( '*' ),
-    collectionTags = cms.vstring( '*' ),
-    maxDPtRel = cms.double( 0.5 ),
-    maxDeltaR = cms.double( 0.5 ),
-    resolveAmbiguities    = cms.bool( True ),
-    resolveByMatchQuality = cms.bool( False )
+muonTriggerMatchHLTMuons = cms.EDProducer('PATTriggerMatcherDRDPtLessByR',
+    src                   = cms.InputTag('cleanPatMuons'),
+    matched               = cms.InputTag('patTrigger'),
+    andOr                 = cms.bool(False),
+    filterIdsEnum         = cms.vstring('TriggerMuon'),
+    filterIds             = cms.vint32(0),
+    filterLabels          = cms.vstring('*'),
+    pathNames             = cms.vstring('HLT_Mu9'),
+    collectionTags        = cms.vstring('*'),
+    maxDPtRel             = cms.double(1),
+    maxDeltaR             = cms.double(0.2),
+    resolveAmbiguities    = cms.bool(True),
+    resolveByMatchQuality = cms.bool(False)
 )

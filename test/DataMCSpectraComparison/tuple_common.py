cea5abf78f0fca54c2ac8e5305557b75b9c5a063
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-09-20 23:22:18 +0000

    Tupling stuff common to data and MC.

---
+++ test/DataMCSpectraComparison/tuple_common.py
@@ -0,0 +1,54 @@
+import os, FWCore.ParameterSet.Config as cms
+
+from SUSYBSMAnalysis.Zprime2muAnalysis.PATTuple_cfg import process
+process.load('SUSYBSMAnalysis.Zprime2muAnalysis.goodData_cff')
+process.p = cms.Path(process.patDefaultSequence)
+
+# Loose cut on muons; stronger cuts to be applied for different
+# sets of plots (e.g. add our isolation cut, or apply VBTF).
+process.selectedPatMuons.cut = 'isGlobalMuon && pt > 20'
+
+# Want to select only events that have at least two leptons (=
+# muons+electrons), where the electrons must pass HEEP id, but don't
+# want to force HEEP id on selectedPatElectrons so as not to screw up
+# the jet cleaning until we study this.
+process.heepPatElectrons = cms.EDFilter('PATElectronSelector',
+                                        src = cms.InputTag('patElectrons'),
+                                        cut = cms.string('userInt("HEEPId") == 0')
+                                        )
+process.patDefaultSequence.replace(process.patCandidates, process.patCandidates * process.heepPatElectrons)
+process.countPatMuons.minNumber = 0
+process.countPatLeptons.electronSource = cms.InputTag('heepPatElectrons')
+process.countPatLeptons.minNumber = 2
+
+# Save a few bytes -- don't need taus right now.
+process.out.outputCommands += ['drop *_patTrigger_*_*',
+                               'drop *_patTriggerEvent_*_*',
+                               'drop patTaus_cleanPatTaus__PAT',
+                               ]
+
+crab_cfg = '''
+[CRAB]
+jobtype = cmssw
+scheduler = %(scheduler)s
+
+[CMSSW]
+datasetpath = %(dataset)s
+pset = %(pset)s
+get_edm_output = 1
+%(job_control)s
+
+[USER]
+ui_working_dir = crab/crab_datamc_%(name)s
+copy_data = 1
+storage_element = T3_US_FNALLPC
+check_user_remote_dir = 0
+publish_data = 1
+publish_data_name = datamc_%(name)s
+dbs_url_for_publication = https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet
+
+[GRID]
+ce_black_list = T2_IN_TIFR, T2_ES_IFCA
+'''
+
+os.system('mkdir -p psets')

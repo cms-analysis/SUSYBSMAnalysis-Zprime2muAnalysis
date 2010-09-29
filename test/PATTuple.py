94239b8331a5edd3f10b6ebd28d7f6939a3bbbd9
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-09-29 02:56:22 +0000

    Remove goodData sequence (it is now run in PATTuple_cfg as a set of separate paths); remove L1 tech trigger stuff; switchHLTProcessName example, commented out.

--- test/PATTuple.py
+++ test/PATTuple.py
@@ -10,8 +10,8 @@ input_is_MC = True
 # which you wish to run, most especially the globalTag (since the list
 # of files will be configured by CRAB itself so it doesn't matter what
 # gets put here for it).
-globalTag = 'START36_V10::All'
-files = ['/store/mc/Summer10/Zmumu/GEN-SIM-RECO/START36_V9_S09-v1/0045/FC750941-147B-DF11-BA8C-00261834B569.root']
+globalTag = 'START37_V6::All'
+files = ['/store/mc/Spring10/DYToMuMu_M-120_7TeV-pythia6/GEN-SIM-RECO/START3X_V26-v2/0026/F44E2CAC-A35A-DF11-B61B-002481CFE804.root']
 
 ########################################################################
 
@@ -23,22 +23,15 @@ from SUSYBSMAnalysis.Zprime2muAnalysis.PATTuple_cfg import process
 process.GlobalTag.globaltag = globalTag
 process.source.fileNames = files
 
-# You can make some typical event selection cuts such as good vertex,
-# beam scraping, and L1 technical bits by including it in the path.
-process.load('SUSYBSMAnalysis.Zprime2muAnalysis.goodData_cff')
-
 # Do the right thing depending on whether MC truth is supposed to be
 # available.
-if input_is_MC:
-    # Modify L1 trigger bits if using MC (bit 0 not defined for MC).
-    process.L1T1.L1SeedsLogicalExpression = cms.string('(40 OR 41) AND NOT (36 OR 37 OR 38 OR 39)')
-else:
+if not input_is_MC:
     from SUSYBSMAnalysis.Zprime2muAnalysis.PATTools import removeMCUse
     removeMCUse(process)
 
 # Our path. The name 'p' is expected by the configuration of the
 # OutputModule in PATTuple_cfg.
-process.p = cms.Path(process.goodData * process.patDefaultSequence)
+process.p = cms.Path(process.patDefaultSequence)
 
 ########################################################################
 
@@ -57,8 +50,14 @@ process.p = cms.Path(process.goodData * process.patDefaultSequence)
 # within the 35X series. This requires some reconfigurations that are
 # done by the PAT for us with a canned function. Uncomment the next
 # two lines to use it.
+##save_discriminatorSources = process.patJets.discriminatorSources[:]
 #from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run36xOn35xInput
-#run36xOn35xInput(process)
+#run36xOn35xInput(process, 'ak5GenJets')
+##process.patJets.discriminatorSources = save_discriminatorSources
+
+# Some MC samples have the HLT process name different from "HLT".
+#from SUSYBSMAnalysis.Zprime2muAnalysis.PATTools import switchHLTProcessName
+#switchHLTProcessName(process, "REDIGI36X")
 
 # You can change the default muon cut from "isGlobalMuon ||
 # isTrackerMuon" to your taste.
@@ -79,4 +78,3 @@ process.p = cms.Path(process.goodData * process.patDefaultSequence)
 #process.maxEvents.input = 1000
 #process.out.fileName = 'myTuple.root'
 #process.options.wantSummary = False # to suppress the long output at the end of the job
-

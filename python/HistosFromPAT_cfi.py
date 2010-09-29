d20b758fa3f79988bc1e493c545a78fd3ba9bf22
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-09-29 02:18:16 +0000

    Remove useAllLeptons (to be reworked later); remove half-brained attempt at taking binning from cfg and use fine enough bins to leave it up to TH1::Rebin().

--- python/HistosFromPAT_cfi.py
+++ python/HistosFromPAT_cfi.py
@@ -3,8 +3,5 @@ import FWCore.ParameterSet.Config as cms
 HistosFromPAT = cms.EDAnalyzer('Zprime2muHistosFromPAT',
                                lepton_src = cms.InputTag('leptons', 'muons'),
                                dilepton_src = cms.InputTag('dimuons'),
-                               useAllLeptons = cms.bool(True),
                                leptonsFromDileptons = cms.bool(False),
-                               lowerMassWin = cms.double(0.5),
-                               upperMassWin = cms.double(100.0),
                                )

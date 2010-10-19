0b22e34fcc5b0dc2442d1b33d31bdb2d6e6e86a1
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-10-19 05:00:22 +0000

    Run on data using files, lumis_to_process from argv; PrintEvent, SimpleNtupler for data always.

--- test/DataMCSpectraComparison/histos.py
+++ test/DataMCSpectraComparison/histos.py
@@ -103,14 +103,21 @@ for cut_name, muon_cuts in cuts:
     setattr(process, pathname, path)
 
 if 'data' in sys.argv:
-    # "daata" since otherwise crab tries to pack up that dir and send
-    # it off to the worker nodes...
-    process.source.fileNames = ['file:work/daata/jul15.root', 'file:work/daata/prompt.root']
-    process.GlobalTag.globaltag = 'GR10_P_V7::All'
-    process.TFileService.fileName = 'ana_datamc_data.root'
+    process.GlobalTag.globaltag = 'GR10_P_V10::All'
+    out_fn = [x for x in sys.argv if x.endswith('.rout')]
+    process.TFileService.fileName = 'ana_datamc_data.root' if len(out_fn) == 0 else out_fn[0].replace('rout', 'root')
+    try:
+        files = ['file:%s' % x for x in sys.argv if '.root' in x]
+        assert(files)
+        lumis_fn = [x for x in sys.argv if x.endswith('.cmssw')][0]
+    except (AssertionError, IndexError):
+        raise RuntimeError('something wrong: files=%s, lumis_fn=%s' % (repr(files), lumis_fn))
+    process.source.fileNames = files
+    process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange(*open(lumis_fn).read().split(','))
 
-    #process.ppp = cms.EDAnalyzer('PrintAbove110', src = cms.InputTag('VBTFMuonsPlusMuonsMinus'), above = cms.double(50))
-    #process.pathVBTF *= process.ppp
+    process.PrintEvent = cms.EDAnalyzer('PrintEvent', dimuon_src = cms.InputTag('VBTFMuonsPlusMuonsMinus'))
+    process.SimpleNtupler = cms.EDAnalyzer('SimpleNtupler', hlt_src = cms.InputTag('TriggerResults', '', 'HLT'), dimu_src = cms.InputTag('VBTFMuonsPlusMuonsMinus'))
+    process.pathVBTF *= process.PrintEvent * process.SimpleNtupler
 
 if __name__ == '__main__' and 'submit' in sys.argv:
     crab_cfg = '''

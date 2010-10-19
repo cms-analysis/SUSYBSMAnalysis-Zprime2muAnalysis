99bce9146d6e9353e63bad1cba7257a771fae211
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-10-19 04:27:32 +0000

    Print some info about the event, mainly muon/dimuon info.

---
+++ plugins/PrintEvent.cc
@@ -0,0 +1,52 @@
+#include <boost/foreach.hpp>
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/PatCandidates/interface/CompositeCandidate.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "SUSYBSMAnalysis/Zprime2muAnalysis/src/Dumpers.h"
+
+class PrintEvent : public edm::EDAnalyzer {
+ public:
+  explicit PrintEvent(const edm::ParameterSet&);
+  void analyze(const edm::Event&, const edm::EventSetup&);
+
+ private:
+  edm::InputTag dimuon_src;
+};
+
+PrintEvent::PrintEvent(const edm::ParameterSet& cfg)
+  : dimuon_src(cfg.getParameter<edm::InputTag>("dimuon_src"))
+{
+}
+
+void PrintEvent::analyze(const edm::Event& event, const edm::EventSetup& setup) {
+  std::ostringstream out;
+
+  edm::Handle<edm::TriggerResults> res;
+  event.getByLabel(edm::InputTag("TriggerResults", "", "PAT"), res);
+  const edm::TriggerNames& names = event.triggerNames(*res);
+  for (size_t i = 0; i < res->size(); ++i)
+    out << "TriggerResults::PAT path #" << i << " name " << names.triggerName(i) << " fired? " << res->accept(i) << "\n";
+
+  edm::Handle<std::vector<reco::Vertex> > vtxs;
+  event.getByLabel("offlinePrimaryVertices", vtxs);
+  BOOST_FOREACH(const reco::Vertex& vtx, *vtxs)
+    out << "vtx with z " << vtx.z() << "\n";
+
+  edm::Handle<pat::CompositeCandidateCollection> dils;
+  event.getByLabel(dimuon_src, dils);
+
+  if (!dils.isValid())
+    out << "WARNING! tried to get dils and failed!\n";
+  else {
+    out << "dileptons:\n";
+    BOOST_FOREACH(const pat::CompositeCandidate& dil, *dils)
+      out << dil << "\n";
+  }
+
+  edm::LogInfo("PrintEvent") << out.str();
+}
+
+DEFINE_FWK_MODULE(PrintEvent);

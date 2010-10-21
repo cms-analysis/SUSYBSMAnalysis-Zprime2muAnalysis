d286321763d8b8f9d4d7b2a521f175610f79f710
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-10-21 15:53:38 +0000

    files_from_dbs: raise error if no files found from dbs output.

--- python/tools.py
+++ python/tools.py
@@ -16,7 +16,11 @@ def files_from_dbs(dataset, ana02=True):
     # could use DBSAPI but this is easier
     url = '--url https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet' if ana02 else ''
     cmd = 'dbs search %(url)s --query "find file where dataset=%(dataset)s"' % locals()
-    return [y.strip('\n') for y in os.popen(cmd).readlines() if '.root' in y]
+    cmdout = os.popen(cmd).readlines()
+    ret = [y.strip('\n') for y in cmdout if '.root' in y]
+    if not ret:
+        raise RuntimeError('no files for %s (ana02: %s) found. dbs command output:\n' % (dataset, ana02) + ''.join(cmdout))
+    return ret
 
 def parse_enum(cpp_file, enum_name, as_list=False, drop_last=False, remove_substr=None):
     """Poor man's C++ enum parsing."""

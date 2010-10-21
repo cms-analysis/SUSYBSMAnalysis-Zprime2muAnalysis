ce3f25afe4405491f0e0df1cca5dfc7b2f19daf4
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-10-21 16:14:47 +0000

    Remove eta, isolation cuts.

--- python/VBTFSelection_cff.py
+++ python/VBTFSelection_cff.py
@@ -1,13 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
+# The starting point is the VBTF selection, which is documented at
+#
 # https://twiki.cern.ch/twiki/bin/view/CMS/VbtfZMuMuBaselineSelection
 #
-# Both muons must pass this selection:
+# We remove the cuts on isolation and on muon pseudo-rapidity, and add
+# cuts on the 3D angle between muons and the common vertex chi2
+# probability (the latter two being implemented in
+# Zprime2muCompositeCandidatePicker, since they can't be done by the
+# StringCutParser).
+#
+# So, both muons must pass this selection:
 #
 # - muon must be a global muon
 # - pT > 20
-# - |eta| < 2.4
-# - tracker isolation < 3 GeV (isolationR03.sumPt < 3)
 # - number of tracker hits >= 10 (innerTrack.hitPattern.numberOfValidTrackerHits >= 10)
 #
 # Then one muon must pass the above plus a tighter set of cuts:
@@ -17,9 +23,7 @@ import FWCore.ParameterSet.Config as cms
 # - at least one pixel hit (innerTrack.hitPattern.numberOfValidPixelHits >= 1)
 # - at least two muon stations in the fit (globalTrack.hitPattern.muonStationsWithValidHits >= 2)
 # - must be a tracker muon
-# - |eta| < 2.1
 # - trigger matching to the single muon HLT path (e.g. !triggerObjectMatchesByPath("HLT_Mu9").empty())
-# - the L3 muon so matched has |eta| < 2.1 (e.g. abs(triggerObjectMatchesByPath("HLT_Mu9").at(0).eta()) < 2.1)
 #
 # (The single muon HLT path used will change as the trigger menu
 # evolves with luminosity.)
@@ -30,8 +34,6 @@ import FWCore.ParameterSet.Config as cms
 
 vbtf_loose = 'isGlobalMuon && ' \
              'pt > 20. && ' \
-             'abs(eta) < 2.4 && ' \
-             'isolationR03.sumPt < 3 && ' \
              'innerTrack.hitPattern.numberOfValidTrackerHits >= 10'
 
 # For the trigger match, currently HLT_Mu11 is the lowest-pT
@@ -39,16 +41,15 @@ vbtf_loose = 'isGlobalMuon && ' \
 # exist. Emulate it by using HLT_Mu9 (unprescaled in those runs) and a
 # pT cut.
 vbtf_trigger_match = '(' \
-                     '(!triggerObjectMatchesByPath("HLT_Mu9").empty() && abs(triggerObjectMatchesByPath("HLT_Mu9").at(0).eta()) < 2.1 && triggerObjectMatchesByPath("HLT_Mu9").at(0).pt() > 11) || ' \
-                     '(!triggerObjectMatchesByPath("HLT_Mu11").empty() && abs(triggerObjectMatchesByPath("HLT_Mu11").at(0).eta()) < 2.1)' \
+                     '(!triggerObjectMatchesByPath("HLT_Mu9").empty() && triggerObjectMatchesByPath("HLT_Mu9").at(0).pt() > 11) || ' \
+                     '!triggerObjectMatchesByPath("HLT_Mu11").empty()' \
                      ')'
 
 vbtf_tight = 'dB < 0.2 && ' \
              'globalTrack.normalizedChi2 < 10 && ' \
              'innerTrack.hitPattern.numberOfValidPixelHits >= 1 && ' \
              'globalTrack.hitPattern.muonStationsWithValidHits >= 2 && ' \
-             'isTrackerMuon && ' \
-             'abs(eta) < 2.1 && ' + vbtf_trigger_match
+             'isTrackerMuon && ' + vbtf_trigger_match
 
 allDimuons = cms.EDProducer('LooseTightCandViewShallowCloneCombiner',
                             decay = cms.string('leptons:muons@+ leptons:muons@-'),

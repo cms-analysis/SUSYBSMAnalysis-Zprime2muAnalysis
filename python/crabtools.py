7b05e08f1bf26319b6bc259af89bbe4a0e895cd2
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-03 14:43:52 +0000

    Improve error report in files_from_crab_dir.

--- python/crabtools.py
+++ python/crabtools.py
@@ -55,7 +55,7 @@ def files_from_crab_dir(crab_dir):
         exe_mo = exe_re.search(s)
         filename_mo = filename_re.search(s)
         if wrapper_mo is None or exe_mo is None or filename_mo is None:
-            raise RuntimeError('cannot parse %s for exit codes and output filename' % fjr)
+            raise RuntimeError('cannot parse %s for exit codes and output filename (wrapper_mo %s exe_mo %s filename_mo %s)' % (fjr, wrapper_mo, exe_mo, filename_mo))
         if wrapper_mo.group(1) != '0' or exe_mo.group(1) != '0':
             raise RuntimeError('exit codes for %s not 0 (wrapper %s, exe %s)' % (fjr, wrapper_mo.group(1), exe_mo.group(1)))
         files.append(filename_mo.group(1))

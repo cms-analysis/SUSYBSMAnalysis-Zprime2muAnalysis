9bc8e4edcfd57dafc5c1abf00a9369e7bb5ee02f
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-11 19:35:27 +0000

    Simple script to dump the TriggerResults and TriggerEvent objects.

---
+++ test/TriggerSummary.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('Zprime2muAnalysis')
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:pat.root'))
+process.load('FWCore.MessageLogger.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+from SUSYBSMAnalysis.Zprime2muAnalysis.cmsswtools import files_from_argv
+files_from_argv(process)
+
+hlt = 'REDIGI38X'
+
+process.load('HLTrigger.HLTcore.triggerSummaryAnalyzerAOD_cfi')
+process.triggerSummaryAnalyzerAOD.inputTag = cms.InputTag('hltTriggerSummaryAOD', '', hlt)
+
+process.printEvent = cms.EDAnalyzer('PrintEvent',
+                                    hlt_src = cms.InputTag('TriggerResults', '', hlt),
+                                    dimuon_src = cms.InputTag('no')
+                                    )
+process.MessageLogger.categories.append('PrintEvent')
+
+process.p = cms.Path(process.printEvent * process.triggerSummaryAnalyzerAOD)

8bcf639ee7c8e72dcb9e72fdec7907bfe536ced8
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-11 19:45:01 +0000

    In the pruner, remove pt > 2 cut on filter because it makes doing acceptance/etc. confusing; also just to be sure, keep all the descendants of the Z0/Z'/G*.

--- python/GenPlusSim_cfi.py
+++ python/GenPlusSim_cfi.py
@@ -5,8 +5,8 @@ from SimGeneral.HepPDTESSource.pythiapdt_cfi import HepPDTESSource
 genSimLeptons = cms.EDProducer(
     "GenPlusSimParticleProducer",
     src           = cms.InputTag("g4SimHits"), # use "famosSimHits" for FAMOS
-    setStatus     = cms.int32(8),             # set status = 8 for GEANT GPs
-    filter        = cms.vstring('(abs(pdgId) == 13 || abs(pdgId) == 11) && pt > 2'),  # just for testing (optional)
+    setStatus     = cms.int32(8),              # set status = 8 for GEANT GPs
+    filter        = cms.vstring('(abs(pdgId) == 13 || abs(pdgId) == 11) && pt > 2'),
     genParticles  = cms.InputTag("genParticles") # original genParticle list
     )
 
@@ -20,9 +20,11 @@ prunedGenSimLeptons = cms.EDProducer('GenParticlePruner',
                                          # or from GEANT with status 8 as set above) that has potential to
                                          # reach the muon system (should reevaluate for muons produced far from
                                          # the IP), and all of their ancestors
-                                         '++keep abs(pdgId) == 13 && (status == 1 || status == 8) && pt > 2',
+                                         '++keep abs(pdgId) == 13 && (status == 1 || status == 8)',
                                          # keep any final-state non-GEANT electron and all of their ancestors
-                                         '++keep abs(pdgId) == 11 && status == 1'
+                                         '++keep abs(pdgId) == 11 && status == 1',
+                                         # just to be sure, keep all the descendants of the Z0/Z'/G*
+                                         'keep++ pdgId == 23 || pdgId == 32 || pdgId == 39 || pdgId == 5000039',
                                          )
                                      )
 

b88dd1040526ec3129f01b0b73bc37efb134b28b
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-11 21:04:13 +0000

    'VBTF' no longer a misnomer -- implement exactly their selection by using this file; consequently, dimuons moved in from Zprime2muAnalysis_cff to remove the cosmic B2B and dimu vtx prob cuts.

--- python/VBTFSelection_cff.py
+++ python/VBTFSelection_cff.py
@@ -1,60 +1,70 @@
 import FWCore.ParameterSet.Config as cms
 
-# The starting point is the VBTF selection, which is documented at
+# This attempts to implement exactly the VBTF selection, which is
+# documented at
 #
 # https://twiki.cern.ch/twiki/bin/view/CMS/VbtfZMuMuBaselineSelection
 #
-# We remove the cuts on muon pseudo-rapidity, change the isolation cut
-# to 10 GeV, and add cuts on the 3D angle between muons and the common
-# vertex chi2 probability (the latter two being implemented in
-# Zprime2muCompositeCandidatePicker, since they can't be done by the
-# StringCutParser).
+# but this is slightly out of date. The most up-to-date info comes
+# from AN-10-264, AN-10-345, and e-mail with Michele de Gruttola.
 #
-# So, both muons must pass this selection:
+# Currently, their golden dimuons are formed from opposite-charged
+# pairs in which both muons must pass this selection:
 #
 # - muon must be a global muon
 # - pT > 20
-# - number of tracker hits >= 10 (innerTrack.hitPattern.numberOfValidTrackerHits >= 10)
-#
-# Then one muon must pass the above plus a tighter set of cuts:
-#
-# - dxy < 0.2 cm (dB < 0.2)
-# - muon global track chi2/ndof < 10 (globalTrack.normalizedChi2 < 10)
+# - |eta| < 2.1
+# - dxy wrt beamspot < 0.2 cm (dB < 0.2)
+# - relative combined isolation < 15% ((isolationR03.sumPt + isolationR03.emEt + isolationR03.hadEt) / innerTrack.pt < 0.15)
+# - number of tracker hits > 10 (innerTrack.hitPattern.numberOfValidTrackerHits > 10)
 # - at least one pixel hit (innerTrack.hitPattern.numberOfValidPixelHits >= 1)
 # - at least two muon stations in the fit (globalTrack.hitPattern.muonStationsWithValidHits >= 2)
-# - must be a tracker muon
-# - trigger matching to the single muon HLT path (e.g. !triggerObjectMatchesByPath("HLT_Mu9").empty())
 #
-# (The single muon HLT path used will change as the trigger menu
-# evolves with luminosity.)
+# At least one muon must be matched to a trigger object firing the
+# single muon HLT path
+# (e.g. !triggerObjectMatchesByPath("HLT_Mu9").empty()) The single
+# muon HLT path used will change as the trigger menu evolves with
+# luminosity.
 #
 # So we have a LooseTightCandViewShallowCloneCombiner that requires
-# both muons to pass the loose cut, and at least one must pass the
-# tight cut.
+# both muons to pass the above cuts ("loose" is then a misnomer), and
+# at least one must pass the trigger match requirement (the only
+# "tight" cut).
 
-vbtf_loose = 'isGlobalMuon && ' \
-             'pt > 20. && ' \
-             'isolationR03.sumPt < 10 &&' \
-             'innerTrack.hitPattern.numberOfValidTrackerHits >= 10'
+loose_cut = 'isGlobalMuon && ' \
+            'pt > 20. && ' \
+            'abs(eta) < 2.1 && ' \
+            'dB < 0.2 && ' \
+            '(isolationR03.sumPt + isolationR03.emEt + isolationR03.hadEt) / innerTrack.pt < 0.15 && ' \
+            'innerTrack.hitPattern.numberOfValidTrackerHits > 10 && ' \
+            'innerTrack.hitPattern.numberOfValidPixelHits >= 1 && ' \
+            'globalTrack.hitPattern.numberOFValidMuonHits > 0 && ' \
+            'globalTrack.hitPattern.muonStationsWithValidHits >= 2'
 
 # For the trigger match, currently HLT_Mu15_v1 is the lowest-pT
 # unprescaled single muon path. In runs <= 147119, HLT_Mu15_v1 did not
 # exist. Emulate it by using HLT_Mu9 (unprescaled in those runs) and a
 # pT cut.
-vbtf_trigger_match = '(' \
-                     '(!triggerObjectMatchesByPath("HLT_Mu9").empty() && triggerObjectMatchesByPath("HLT_Mu9").at(0).pt() > 15) || ' \
-                     '!triggerObjectMatchesByPath("HLT_Mu15_v1").empty()' \
-                     ')'
+#
+# Michele is actually using HLT_Mu9/HLT_Mu11 in these old runs...
+trigger_match = '(' \
+                '(!triggerObjectMatchesByPath("HLT_Mu9").empty() && triggerObjectMatchesByPath("HLT_Mu9").at(0).pt() > 15) || ' \
+                '!triggerObjectMatchesByPath("HLT_Mu15_v1").empty()' \
+                ')'
 
-vbtf_tight = 'dB < 0.2 && ' \
-             'globalTrack.normalizedChi2 < 10 && ' \
-             'innerTrack.hitPattern.numberOfValidPixelHits >= 1 && ' \
-             'globalTrack.hitPattern.muonStationsWithValidHits >= 2 && ' \
-             'isTrackerMuon && ' + vbtf_trigger_match
+tight_cut = trigger_match
 
 allDimuons = cms.EDProducer('LooseTightCandViewShallowCloneCombiner',
                             decay = cms.string('leptons:muons@+ leptons:muons@-'),
                             cut = cms.string(''),
-                            loose_cut = cms.string(vbtf_loose),
-                            tight_cut = cms.string(vbtf_tight)
+                            loose_cut = cms.string(loose_cut),
+                            tight_cut = cms.string(tight_cut)
                             )
+
+dimuons = cms.EDProducer('Zprime2muCompositeCandidatePicker',
+                         src = cms.InputTag('allDimuons'),
+                         cut = cms.string(''),
+                         max_candidates = cms.uint32(1),
+                         back_to_back_cos_angle_min = cms.double(-1),
+                         vertex_chi2_max = cms.double(-1),
+                         )

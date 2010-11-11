2cca73b947bffde956712fc1b927774aff006e17
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-11 19:29:04 +0000

    Update of trigger paths to use (HLT_Mu11 is the highest-pt path in the menus used on MC; for now, rely on caller to emulate higher-pt paths if required

--- python/TriggerDecision_cff.py
+++ python/TriggerDecision_cff.py
@@ -4,6 +4,6 @@ triggerDecision = cms.PSet(doingElectrons = cms.bool(False),
                            useTrigger = cms.bool(True),
                            l1GtObjectMap = cms.InputTag('hltL1GtObjectMap'),
                            hltResults = cms.InputTag('TriggerResults', '', 'HLT'),
-                           l1Paths = cms.vstring('L1_SingleMu7', 'L1_DoubleMu3'),
-                           hltPaths = cms.vstring('HLT_Mu9', 'HLT_DoubleMu3')
+                           l1Paths = cms.vstring('L1_SingleMu7'),
+                           hltPaths = cms.vstring('HLT_Mu11')
                            )

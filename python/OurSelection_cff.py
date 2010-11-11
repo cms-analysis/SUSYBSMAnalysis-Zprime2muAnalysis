9c8a05bade26b0052f58d0391d5565d06dd6c0be
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-11 21:05:12 +0000

    What used to be in VBTFSelection_cff, but is really our modified selection; dimuons moved from Zprime2muAnalysis_cff as-was with cosmic B2B and dimu vtx prob cuts.

---
+++ python/OurSelection_cff.py
@@ -0,0 +1,69 @@
+import FWCore.ParameterSet.Config as cms
+
+# The starting point is the (old) VBTF selection, which is documented
+# at
+#
+# https://twiki.cern.ch/twiki/bin/view/CMS/VbtfZMuMuBaselineSelection
+#
+# We remove the cuts on muon pseudo-rapidity, change the isolation cut
+# to 10 GeV, and add cuts on the 3D angle between muons and the common
+# vertex chi2 probability (the latter two being implemented in
+# Zprime2muCompositeCandidatePicker, since they can't be done by the
+# StringCutParser).
+#
+# So, both muons must pass this selection:
+#
+# - muon must be a global muon
+# - pT > 20
+# - number of tracker hits >= 10 (innerTrack.hitPattern.numberOfValidTrackerHits >= 10)
+#
+# Then one muon must pass the above plus a tighter set of cuts:
+#
+# - dxy < 0.2 cm (dB < 0.2)
+# - muon global track chi2/ndof < 10 (globalTrack.normalizedChi2 < 10)
+# - at least one pixel hit (innerTrack.hitPattern.numberOfValidPixelHits >= 1)
+# - at least two muon stations in the fit (globalTrack.hitPattern.muonStationsWithValidHits >= 2)
+# - must be a tracker muon
+# - trigger matching to the single muon HLT path (e.g. !triggerObjectMatchesByPath("HLT_Mu9").empty())
+#
+# (The single muon HLT path used will change as the trigger menu
+# evolves with luminosity.)
+#
+# So we have a LooseTightCandViewShallowCloneCombiner that requires
+# both muons to pass the loose cut, and at least one must pass the
+# tight cut.
+
+loose_cut = 'isGlobalMuon && ' \
+            'pt > 20. && ' \
+            'isolationR03.sumPt < 10 &&' \
+            'innerTrack.hitPattern.numberOfValidTrackerHits >= 10'
+
+# For the trigger match, currently HLT_Mu15_v1 is the lowest-pT
+# unprescaled single muon path. In runs <= 147119, HLT_Mu15_v1 did not
+# exist. Emulate it by using HLT_Mu9 (unprescaled in those runs) and a
+# pT cut.
+trigger_match = '(' \
+                '(!triggerObjectMatchesByPath("HLT_Mu9").empty() && triggerObjectMatchesByPath("HLT_Mu9").at(0).pt() > 15) || ' \
+                '!triggerObjectMatchesByPath("HLT_Mu15_v1").empty()' \
+                ')'
+
+tight_cut = 'dB < 0.2 && ' \
+            'globalTrack.normalizedChi2 < 10 && ' \
+            'innerTrack.hitPattern.numberOfValidPixelHits >= 1 && ' \
+            'globalTrack.hitPattern.muonStationsWithValidHits >= 2 && ' \
+            'isTrackerMuon && ' + trigger_match
+
+allDimuons = cms.EDProducer('LooseTightCandViewShallowCloneCombiner',
+                            decay = cms.string('leptons:muons@+ leptons:muons@-'),
+                            cut = cms.string(''),
+                            loose_cut = cms.string(loose_cut),
+                            tight_cut = cms.string(tight_cut)
+                            )
+
+dimuons = cms.EDProducer('Zprime2muCompositeCandidatePicker',
+                         src = cms.InputTag('allDimuons'),
+                         cut = cms.string(''),
+                         max_candidates = cms.uint32(1),
+                         back_to_back_cos_angle_min = cms.double(-0.9998), # this corresponds to the angle (pi - 0.02) rad = 178.9 deg
+                         vertex_chi2_max = cms.double(10),
+                         )

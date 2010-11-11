51f250aafdcb44b5cbeba5c1e5cb19f73208d320
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-11 21:07:57 +0000

    Do n-1 effs for Our selection instead of VBTF (mostly just changing module names appropriately).

--- test/NMinus1Effs/nminus1effs.py
+++ test/NMinus1Effs/nminus1effs.py
@@ -3,25 +3,23 @@
 import sys, os, FWCore.ParameterSet.Config as cms
 from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muAnalysis_cfg import process
 from SUSYBSMAnalysis.Zprime2muAnalysis.HistosFromPAT_cfi import HistosFromPAT
-from SUSYBSMAnalysis.Zprime2muAnalysis.VBTFSelection_cff import vbtf_loose, vbtf_trigger_match, vbtf_tight, allDimuons
+from SUSYBSMAnalysis.Zprime2muAnalysis.OurSelection_cff import loose_cut, trigger_match, tight_cut, allDimuons
 
-# Define the numerators and denominators, removing cuts from the VBTF
+# Define the numerators and denominators, removing cuts from the
 # allDimuons maker. "NoX" means remove cut X (i.e. the denominators),
-# "NoNo" means remove nothing (i.e. the numerators).
-#
-# This will break if vbtf_loose, vbtf_tight cut strings are changed...
-#
-process.allDimuonsVBTFNoIso     = allDimuons.clone(loose_cut = vbtf_loose.replace(' && isolationR03.sumPt < 10', ''))
-process.allDimuonsVBTFNoTkHits  = allDimuons.clone(loose_cut = vbtf_loose.replace(' && innerTrack.hitPattern.numberOfValidTrackerHits >= 10', ''))
-process.allDimuonsVBTFNoDB      = allDimuons.clone(tight_cut = vbtf_tight.replace('dB < 0.2 && ', ''))
-process.allDimuonsVBTFNoGlbChi2 = allDimuons.clone(tight_cut = vbtf_tight.replace(' && globalTrack.normalizedChi2 < 10', ''))
-process.allDimuonsVBTFNoPxHits  = allDimuons.clone(tight_cut = vbtf_tight.replace(' && innerTrack.hitPattern.numberOfValidPixelHits >= 1', ''))
-process.allDimuonsVBTFNoMuStns  = allDimuons.clone(tight_cut = vbtf_tight.replace(' && globalTrack.hitPattern.muonStationsWithValidHits >= 2', ''))
-process.allDimuonsVBTFNoTkMuon  = allDimuons.clone(tight_cut = vbtf_tight.replace(' && isTrackerMuon', ''))
-process.allDimuonsVBTFNoTrgMtch = allDimuons.clone(tight_cut = vbtf_tight.replace(' && ' + vbtf_trigger_match, ''))
-process.allDimuonsVBTFNoNo      = allDimuons.clone()
+# "NoNo" means remove nothing (i.e. the numerators). This will break
+# if loose_, tight_ cut strings are changed...
+process.allDimuonsNoIso     = allDimuons.clone(loose_cut = loose_cut.replace(' && isolationR03.sumPt < 10', ''))
+process.allDimuonsNoTkHits  = allDimuons.clone(loose_cut = loose_cut.replace(' && innerTrack.hitPattern.numberOfValidTrackerHits >= 10', ''))
+process.allDimuonsNoDB      = allDimuons.clone(tight_cut = tight_cut.replace('dB < 0.2 && ', ''))
+process.allDimuonsNoGlbChi2 = allDimuons.clone(tight_cut = tight_cut.replace(' && globalTrack.normalizedChi2 < 10', ''))
+process.allDimuonsNoPxHits  = allDimuons.clone(tight_cut = tight_cut.replace(' && innerTrack.hitPattern.numberOfValidPixelHits >= 1', ''))
+process.allDimuonsNoMuStns  = allDimuons.clone(tight_cut = tight_cut.replace(' && globalTrack.hitPattern.muonStationsWithValidHits >= 2', ''))
+process.allDimuonsNoTkMuon  = allDimuons.clone(tight_cut = tight_cut.replace(' && isTrackerMuon', ''))
+process.allDimuonsNoTrgMtch = allDimuons.clone(tight_cut = tight_cut.replace(' && ' + vbtf_trigger_match, ''))
+process.allDimuonsNoNo      = allDimuons.clone()
 
-alldimus = [x for x in dir(process) if 'allDimuonsVBTFNo' in x]
+alldimus = [x for x in dir(process) if 'allDimuonsNo' in x]
 process.p = cms.Path(process.goodDataFilter * process.muonPhotonMatch * process.leptons * reduce(lambda x,y: x*y, [getattr(process, x) for x in alldimus]))
 
 # For all the allDimuons producers, make dimuons producers, and
@@ -37,15 +35,14 @@ for alld in alldimus:
 # Handle the cuts that have to be applied at the
 # Zprime2muCompositeCandidatePicker level.
 process.p *= process.allDimuons
-process.dimuonsVBTFNoB2B     = process.dimuons.clone(back_to_back_cos_angle_min = -1)
-process.dimuonsVBTFNoVtxProb = process.dimuons.clone(vertex_chi2_max = -1)
+process.dimuonsNoB2B     = process.dimuons.clone(back_to_back_cos_angle_min = -1)
+process.dimuonsNoVtxProb = process.dimuons.clone(vertex_chi2_max = -1)
 
-for dimu in ['dimuonsVBTFNoB2B', 'dimuonsVBTFNoVtxProb']:
+for dimu in ['dimuonsNoB2B', 'dimuonsNoVtxProb']:
     hists = HistosFromPAT.clone(dilepton_src = dimu, leptonsFromDileptons = True)
     setattr(process, dimu.replace('dimuons', ''), hists)
     process.p *= getattr(process, dimu) * hists
-    
-################################################################################
+
 
 if 'olddata' in sys.argv:
     process.source.fileNames = ['file:work/daata/jul15.root', 'file:work/daata/prompt.root']

15da244d78da099c953c9a0f1f7d94545d4184e7
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-15 02:24:36 +0000

    Compare using tracker-only mass since that's what VBTF uses; reformat output a bit; show events with mass difference; don't show events where they are not in both JSONs.

--- test/DataMCSpectraComparison/comparetovbtf.py
+++ test/DataMCSpectraComparison/comparetovbtf.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python
 
 from collections import defaultdict
-from pprint import pprint
+from math import sinh, cos, sin
 from FWCore.PythonUtilities.LumiList import LumiList
-from SUSYBSMAnalysis.Zprime2muAnalysis.roottools import ROOT, ttree_iterator
+from SUSYBSMAnalysis.Zprime2muAnalysis.roottools import ROOT, ttree_iterator, set_zp2mu_style
+set_zp2mu_style()
 
 me = defaultdict(list)
 vbtf = defaultdict(list)
 
-me_json = LumiList('sept17.forlumi.json')
+me_json = LumiList('ana_datamc/ana_datamc_data.forlumi.json')
 vbtf_json = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/StreamExpress/Cert_132440-149442_7TeV_StreamExpress_Collisions10_JSON.txt')
 
 peak_only = False
+tracker_mass = True
 
-#fns = ['ana_datamc_vbtfvsours/ana_datamc_data_jul15_prompt.root', 'ana_datamc_vbtfvsours/ana_datamc_data_promptB_allgood.root']
-#fns = ['ana_datamc_data.root', 'ana_datamc_vbtfvsours/ana_datamc_data_promptB_allgood.root']
-fns = ['sept17.root']
+def ptetaphi2p4(pt,eta,phi):
+    px = pt * cos(phi)
+    py = pt * sin(phi)
+    pz = pt * sinh(eta)
+    e = (pt**2 + pz**2 + 0.10566**2)**0.5
+    return px,py,pz,e
+
+fns = ['ana_datamc/ana_datamc_data.root']
 for fn in fns:
     f = ROOT.TFile(fn)
     for j,t in ttree_iterator(f.SimpleNtuplerVBTF.Get('t')):
         if not peak_only or (t.dil_mass > 60 and t.dil_mass < 120):
             r,l,e,m = t.run, t.lumi, t.event, t.dil_mass
+            if tracker_mass:
+                px0,py0,pz0,e0 = ptetaphi2p4(t.lep_tk_pt[0], t.lep_tk_eta[0], t.lep_tk_phi[0])
+                px1,py1,pz1,e1 = ptetaphi2p4(t.lep_tk_pt[1], t.lep_tk_eta[1], t.lep_tk_phi[1])
+                m = ((e0+e1)**2 - (px0+px1)**2 - (py0+py1)**2 - (pz0+pz1)**2)**0.5
+            if m < 1:
+                print 'mass < 1 GeV:', m, t.lep_tk_pt[0], t.lep_tk_eta[0], t.lep_tk_phi[0], t.lep_tk_pt[1], t.lep_tk_eta[1], t.lep_tk_phi[1]
+            if m < 20 and m > 1:
+                print '    mass < 20 GeV:', m, t.lep_tk_pt[0], t.lep_tk_eta[0], t.lep_tk_phi[0], t.lep_tk_pt[1], t.lep_tk_eta[1], t.lep_tk_phi[1]
             for x in 'rle':
                 exec '%s = int(%s)' % (x,x) # python ints are c longs
             me[(r,l,e)].append(m)
@@ -58,6 +73,9 @@ for line in open('log_goldenZmumuCands_NoMassCuts_34p86.log'):
 mek = set(me.keys())
 vbtfk = set(vbtf.keys())
 
+mass_diffs = {}
+h_mass_diff = ROOT.TH1F('h_mass_diff', '', 10000, -30, 30)
+
 def m2s(l):
     return ' * '.join('%.1f' % m for m in l)
 
@@ -65,14 +83,40 @@ print 'counts:\tme\tvbtf'
 print 'events:\t%i\t%i' % (len(mek), len(vbtfk))
 print
 print 'diffs in dimu counts?'
-print '%6s%6s%14s%14s%14s%25s%25s' % ('mjs','vjs','run','lumi','event', 'me', 'vbtf')
+print '%14s%14s%14s%25s%25s' % ('run','lumi','event', 'me', 'vbtf')
 for rle in sorted(mek & vbtfk):
     if len(me[rle]) != len(vbtf[rle]):
-        r,l,e = rle
-        print '%6s%6s%14s%14s%14s%25s%25s' % (me_json.contains((r,l)), vbtf_json.contains((r,l)), r,l,e, m2s(me[rle]), m2s(vbtf[rle]))
+        print '%14s%14s%14s%25s%25s' % (rle + (m2s(me[rle]), m2s(vbtf[rle])))
+    else:
+        assert(len(me[rle]) == 1)
+        mass_diffs[rle] = dm = me[rle][0] - vbtf[rle][0]
+        h_mass_diff.Fill(dm)
 print
-print 'symmetric difference:'
-print '%6s%6s%14s%14s%14s%25s%25s' % ('mjs','vjs','run','lumi','event', 'me', 'vbtf')
+h_mass_diff.Draw()
+ROOT.c1.SetLogy(1)
+ROOT.c1.SaveAs('~/asdf/massdiffs.png')
+ROOT.c1.SaveAs('~/asdf/massdiffs.root')
+print 'max mass diff:', max(abs(m) for m in mass_diffs.values())
+print
+print '|delta m| > 0.01 GeV:\n'
+print '%14s%14s%14s%25s%25s' % ('run','lumi','event', 'me', 'vbtf')
+for rle in sorted(mass_diffs.keys()):
+    if mass_diffs[rle] > 0.01:
+        print '%14s%14s%14s%25s%25s' % (rle + (m2s(me[rle]), m2s(vbtf[rle])))
+print
+print 'symmetric difference where rle is in both jsons:'
+print '%14s%14s%14s%25s%25s' % ('run','lumi','event', 'me', 'vbtf')
+only_in_me_json = defaultdict(list)
+only_in_vbtf_json = defaultdict(list)
 for rle in sorted(mek.symmetric_difference(vbtfk)):
     r,l,e = rle
-    print '%6s%6s%14s%14s%14s%25s%25s' % (me_json.contains((r,l)), vbtf_json.contains((r,l)), r,l,e, m2s(me[rle]), m2s(vbtf[rle]))
+    if not me_json.contains((r,l)):
+        only_in_vbtf_json[rle].append(vbtf[rle])
+        continue
+    if not vbtf_json.contains((r,l)):
+        only_in_me_json[rle].append(me[rle])
+        continue
+    print '%14s%14s%14s%25s%25s' % (r,l,e, m2s(me[rle]), m2s(vbtf[rle]))
+print
+print 'events only in my json:', len(only_in_me_json)
+print 'events only in vbtf json:', len(only_in_vbtf_json)

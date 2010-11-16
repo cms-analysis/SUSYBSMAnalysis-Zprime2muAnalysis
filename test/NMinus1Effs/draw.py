028406ebb5ab0a7d7d9ec504c1f855ca0c3b0274
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-16 16:36:50 +0000

    Re-order bins, fix labels; remove Iso10 special case; make B&W-friendly.

--- test/NMinus1Effs/draw.py
+++ test/NMinus1Effs/draw.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 from SUSYBSMAnalysis.Zprime2muAnalysis.roottools import *
 set_zp2mu_style()
 ROOT.gStyle.SetPadTopMargin(0.02)
@@ -8,17 +8,18 @@ ROOT.gStyle.SetPadRightMargin(0.02)
 ROOT.gStyle.SetTitleX(0.12)
 #ROOT.gStyle.SetTitleH(0.07)
 c = ROOT.TCanvas('c', '', 820, 630)
+os.system('mkdir -p plots/nminus1effs')
 
 def integ(h,a,b=1e9):
     return h.Integral(h.FindBin(a), h.FindBin(b))
 
 nminus1s = [
-    'NoGlbChi2',
-    'NoTkMuon',
-    'NoDB',
     'NoTkHits',
+    'NoDB',
+    'NoGlbChi2',
     'NoPxHits',
     'NoMuStns',
+    'NoTkMuon',
     'NoTrgMtch',
     'NoB2B',
     'NoVtxProb',
@@ -31,14 +32,15 @@ pretty = {
     'NoDB': '|d_{xy}| < 0.2',
     'NoGlbChi2': 'glb #chi^{2}/ndf < 10',
     'NoTkMuon': 'isTrackerMuon',
-    'NoTrgMtch': 'HLT_Mu9 match',
-    'NoB2B': 'cos(angle) > 0.02,
-    'NoVtxProb': '#chi^{2} #mu#mu vtx < 10'
-    'ana_nminus1_data.root': 'Data, 3.0 pb ^{-1}',
+    'NoTrgMtch': 'HLT match',
+    'NoB2B': 'B2B cosmics',
+    'NoVtxProb': '#chi^{2} #mu#mu vtx < 10',
+    'ana_nminus1_data.root': 'Data, 35.5 pb ^{-1}',
     'ana_nminus1_zmumu.root': 'Z#rightarrow#mu#mu',
     'ana_nminus1_dy120.root': 'Z#rightarrow#mu#mu',
     'ana_nminus1_ttbar.root': 't#bar{t}',
-    'ana_nminus1_zp1000.root': 'Z\' SSM, M=1 TeV  ',
+    'ana_nminus1_inclmu15.root': 'QCD',
+    'ana_nminus1_zssm750.root': 'Z\' SSM, M=750 GeV',
     }
 
 def table(fn):
@@ -54,50 +56,38 @@ def table(fn):
         den120 = integ(hden, 120)
         print '%20s%20f%20f%20f%20f' % (nminus1, den60120, num60120/den60120, den120, num120/den120)
 
-    hnum = f.Get('NoNoIso10').Get('DileptonMass')
-    num60120 = integ(hnum, 60, 120)
-    num120 = integ(hnum, 120)
-    hden = f.Get('NoIso10').Get('DileptonMass')
-    den60120 = integ(hden, 60, 120)
-    den120 = integ(hden, 120)
-    print '%20s%20f%20f%20f%20f%20f%20f' % ('NoIso10/NoNoIso10', num60120, den60120, num60120/den60120, num120, den120, num120/den120)
-
 if '120' in sys.argv:
     items = [
-        ('ana_nminus1_data.root', (120, 1e9), ROOT.kBlack),
-#       ('ana_nminus1_zmumu.root', (120, 1e9), ROOT.kCyan),
-        ('ana_nminus1_dy120.root', (120, 1e9), ROOT.kBlue),
-        ('ana_nminus1_ttbar.root', (120, 1e9), ROOT.kRed),
-        ('ana_nminus1_zp1000.root', (120, 1e9), ROOT.kGreen+2)
+        ('ana_nminus1_data.root',    (120, 1e9), ROOT.kBlack, -1),
+        ('ana_nminus1_dy120.root',   (120, 1e9), ROOT.kBlue, 3006),
+        ('ana_nminus1_ttbar.root',   (120, 1e9), ROOT.kRed, 3007),
+        ('ana_nminus1_zssm750.root', (120, 1e9), ROOT.kGreen+2, 3004)
         ]
-    lg = ROOT.TLegend(0.14, 0.14, 0.60, 0.45)
+    lg = ROOT.TLegend(0.14, 0.14, 0.63, 0.34)
 else:         
     items = [
-        ('ana_nminus1_data.root', (60,120), ROOT.kBlack),
-        ('ana_nminus1_zmumu.root', (60,120), ROOT.kBlue),
-#       ('ana_nminus1_ttbar.root', (60, 120), ROOT.kRed),
+        ('ana_nminus1_data.root',     (60,120), ROOT.kBlack, -1),
+        ('ana_nminus1_zmumu.root',    (60,120), ROOT.kBlue, 3006),
+        ('ana_nminus1_ttbar.root',    (60,120), ROOT.kRed, 3007),
         ]
-    lg = ROOT.TLegend(0.14, 0.14, 0.60, 0.30)
+    lg = ROOT.TLegend(0.14, 0.14, 0.50, 0.32)
 
 same = 'A'
 effs = []
 lg.SetFillColor(0)
-for fn, mass_range, color in items:
+for fn, mass_range, color, fill in items:
     f = ROOT.TFile(fn)
 
     l = len(nminus1s)
     nminus1_num = ROOT.TH1F('num', '', l, 0, l)
     nminus1_den = ROOT.TH1F('den', '', l, 0, l)
 
-    hnum10 = f.Get('NoNoIso10').Get('DileptonMass')
-    num10 = integ(hnum10, *mass_range)
     hnum = f.Get('NoNo').Get('DileptonMass')
     num = integ(hnum, *mass_range)
     for i,nminus1 in enumerate(nminus1s):
         hden = f.Get(nminus1).Get('DileptonMass')
         den = integ(hden, *mass_range)
-        n = num if nminus1 != 'NoIso10' else num10
-        nminus1_num.SetBinContent(i+1, n)
+        nminus1_num.SetBinContent(i+1, num)
         nminus1_den.SetBinContent(i+1, den)
 
     eff = binomial_divide(nminus1_num, nminus1_den)
@@ -106,7 +96,7 @@ for fn, mass_range, color in items:
     else:
         eff.SetTitle('60 < M_{#mu#mu} < 120 GeV')
     eff.GetXaxis().SetRangeUser(0,12)
-#    eff.GetYaxis().SetRangeUser(0.5,1.05)
+    eff.GetYaxis().SetRangeUser(0.95,1.005)
     eff.GetXaxis().SetTitle('cut')
     eff.GetYaxis().SetLabelSize(0.03)
     eff.GetYaxis().SetTitle('n-1 efficiency')
@@ -117,7 +107,9 @@ for fn, mass_range, color in items:
         lg.AddEntry(eff, pretty[fn], 'L')
     else:
         draw = '2'
+        eff.SetLineColor(color)
         eff.SetFillColor(color)
+        eff.SetFillStyle(fill)
         lg.AddEntry(eff, pretty[fn], 'F')
     draw += same
     eff.Draw(draw)
@@ -132,5 +124,7 @@ for fn, mass_range, color in items:
     table(fn)
 
 lg.Draw()
-c.SaveAs('nminus1.png')
-c.SaveAs('nminus1.root')
+fn = 'plots/nminus1effs/nminus1%s.png' % ('120' if '120' in sys.argv else '')
+c.SaveAs(fn)
+c.SaveAs(fn.replace('png', 'root'))
+

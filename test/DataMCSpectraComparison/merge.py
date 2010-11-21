167cbfb3e60bfc6c2d1cb3f4c4a33baa8b7c667a
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-11-21 21:53:28 +0000

    Ability to take files to merge just from argv.

--- test/DataMCSpectraComparison/merge.py
+++ test/DataMCSpectraComparison/merge.py
@@ -10,18 +10,22 @@ process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-from SUSYBSMAnalysis.Zprime2muAnalysis.crabtools import last_crab_dir, files_from_crab_dir
-
-crab_dir = [x for x in sys.argv if os.path.isdir(x)][0]
-if not crab_dir:
-    crab_dir = last_crab_dir()
-
-files = files_from_crab_dir(crab_dir)
-print 'Files for %s to run over:' % crab_dir
+# Look for just a list of files in argv first.
+files = ['file:%s' % x for x in sys.argv if os.path.isfile(x) and '.root' in x]
+name = 'merged.root'
+if not files:
+    # Else, files from crab dir mode.
+    from SUSYBSMAnalysis.Zprime2muAnalysis.crabtools import last_crab_dir, files_from_crab_dir
+    crab_dir = [x for x in sys.argv if os.path.isdir(x)][0]
+    if not crab_dir:
+        crab_dir = last_crab_dir()
+        print 'Auto-crab dir:', crab_dir
+    files = files_from_crab_dir(crab_dir)
+    name = os.path.join(crab_dir, 'res', 'merged.root')
+    
+print 'Files to run over:'
 pprint(files)
 process.source.fileNames = files
-
-name = os.path.join(crab_dir, 'res', 'merged.root')
 print 'Merging to', name
 process.out = cms.OutputModule('PoolOutputModule', fileName = cms.untracked.string(name))
 process.outp = cms.EndPath(process.out)

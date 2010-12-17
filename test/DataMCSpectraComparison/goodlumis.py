72d7e1da21b055b7022fcb00e99a8dca80598c1f
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2010-12-17 00:46:23 +0000

    Switch to Nov4 rereco, for which run list is simpler.

--- test/DataMCSpectraComparison/goodlumis.py
+++ test/DataMCSpectraComparison/goodlumis.py
@@ -1,19 +1,10 @@
 import os
 from FWCore.PythonUtilities.LumiList import LumiList
 
-def combine(Sept17_ll, Prompt_ll):
-    Prompt_ll.removeRuns(xrange(132440,144114+1))
-    return Sept17_ll | Prompt_ll
-
-Sept17_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_132440-144114_7TeV_Sep17ReReco_Collisions10_JSON.txt')
-#Prompt_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/StreamExpress/Cert_132440-149442_7TeV_StreamExpress_Collisions10_JSON_v2.txt')  # gives 35/pb
-Prompt_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/StreamExpress/Cert_132440-149442_7TeV_StreamExpress_Collisions10_JSON_v3.txt')   # gives 33/pb
-Run2010AB_ll = combine(Sept17_ll, Prompt_ll)
-
-cmssw_base = os.environ['CMSSW_BASE']
-Sept17MuonsOnly_ll = LumiList(os.path.join(cmssw_base, 'src/HeavyFlavorAnalysis/Onia2MuMu/certification/7TeV/Collisions10/Reprocessing/Cert_132440-144114_7TeV_Sep17ReReco_Collisions10_JSON_BPAG.txt'))
-PromptMuonsOnly_ll = LumiList(os.path.join(cmssw_base, 'src/HeavyFlavorAnalysis/Onia2MuMu/certification/7TeV/Collisions10/StreamExpress/Cert_132440-149442_7TeV_StreamExpress_Collisions10_JSON_BPAG_v2.txt'))
-Run2010ABMuonsOnly_ll = combine(Sept17MuonsOnly_ll, PromptMuonsOnly_ll)
+Run2010AB_ll          = Nov4_ll          = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_136033-149442_7TeV_Nov4ReReco_Collisions10_JSON.txt')
+Run2010ABMuonsOnly_ll = Nov4MuonsOnly_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_136033-149442_7TeV_Nov4ReReco_Collisions10_JSON_MuonPhys.txt')
 
 Run2010AB = Run2010AB_ll.getCMSSWString().split(',')
 Run2010ABMuonsOnly = Run2010ABMuonsOnly_ll.getCMSSWString().split(',')
+
+

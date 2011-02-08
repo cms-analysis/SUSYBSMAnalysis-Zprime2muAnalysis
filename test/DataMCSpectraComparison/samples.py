ea4f5560f1f31c6bc4c6a9ee73ca836236c0013b
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-02-08 13:29:42 +0000

    Plot tweak; fix systematics for DY.

--- test/DataMCSpectraComparison/samples.py
+++ test/DataMCSpectraComparison/samples.py
@@ -54,19 +54,19 @@ class sample:
 # https://twiki.cern.ch/twiki/bin/view/CMS/CrossSections_3XSeries for
 # xsecs (all below in pb)
 samples = [
-    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Fall10-START38_V12-v1/GEN-SIM-RECO',          1998931, 432, 0.096, 1631 - 0.97*1.3, hlt_process_name='HLT'),
-    sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 433, 0.1,   0.97  - 0.027, k_factor=1.3, hlt_process_name='HLT'),
-    sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 434, 0.1,   0.027 - 0.003, k_factor=1.3, hlt_process_name='HLT'),
-    sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 435, 0.1,   0.003,          k_factor=1.3, hlt_process_name='HLT'),
+    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Fall10-START38_V12-v1/GEN-SIM-RECO',          1998931, 432, 0.05, 1631 - 0.97*1.3, hlt_process_name='HLT'),
+    sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 433, 0.05, 0.97  - 0.027, k_factor=1.3, hlt_process_name='HLT'),
+    sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 434, 0.05, 0.027 - 0.003, k_factor=1.3, hlt_process_name='HLT'),
+    sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 435, 0.05, 0.003,         k_factor=1.3, hlt_process_name='HLT'),
     sample('ttbar',        't#bar{t}',                                           '/TTJets_TuneZ2_7TeV-madgraph-tauola/Fall10-START38_V12-v2/GEN-SIM-RECO',                    1167759,   2, 0.15,  157,  hlt_process_name='HLT'),
     sample('singletop_tW', 'tW',                                                 '/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/Fall10-START38_V12-v2/GEN-SIM-RECO',                494961,   1, 0.075, 10.6, hlt_process_name='HLT'),
     sample('ww',           'WW',                                                 '/WWtoAnything_TuneZ2_7TeV-pythia6-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               2061760,   4, 0.035, 43,  scheduler='glite'),
     sample('wz',           'WZ',                                                 '/WZtoAnything_TuneZ2_7TeV-pythia6-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               2194752,   5, 0.038, 18,  scheduler='glite'),
     sample('zz',           'ZZ',                                                 '/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               2113368,   6, 0.025, 5.9, scheduler='glite'),
-    sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_CT10_TuneZ2_7TeV-powheg-pythia-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO', 1994719,  46, 0.096, 1631, hlt_process_name='HLT'),
-    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               15168266,   3, 0.086, 3.1e4, hlt_process_name='HLT'),
+    sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_CT10_TuneZ2_7TeV-powheg-pythia-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO', 1994719,  46, 0.05, 1631, hlt_process_name='HLT'),
+    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               15168266,   3, 0.05,  3.1e4, hlt_process_name='HLT'),
     sample('inclmu15',     'QCD (MuRich, muon p_{T} > 15 GeV)',                  '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',        29504866, 801, 0.1,   0.0002855 * 296600000),
-    sample('zssm750',      'Z\'_{SSM} (750 GeV) #rightarrow #mu^{+}#mu^{-}',     '/ZprimeSSMToMuMu_M-750_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                      55000,  40, 0.1,   0.355, k_factor=1.3, hlt_process_name='HLT'),
+    sample('zssm750',      'Z\'_{SSM} (750 GeV) #rightarrow #mu^{+}#mu^{-}',     '/ZprimeSSMToMuMu_M-750_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                      55000,  38, 0.05,  0.355, k_factor=1.3, hlt_process_name='HLT'),
 ]
 samples.reverse()
 
@@ -94,3 +94,15 @@ if False:
                 if 'CMSSW' in line:
                     print parent, line,
         print
+
+if False:
+    import os
+    from dbstools import dbsparents
+    for s in [ww,wz,zz]:
+        print s.dataset
+        parents = dbsparents(s.dataset)
+        print parents
+        os.system('dbsconfig %s > %s' % (parents[-1], s.name))
+
+    
+                

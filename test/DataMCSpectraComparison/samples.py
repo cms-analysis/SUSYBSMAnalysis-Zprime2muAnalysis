cb1211c754d876cf32bfd7471e239fcddb815239
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-03-25 16:07:30 +0000

    Spring11 samples.

--- test/DataMCSpectraComparison/samples.py
+++ test/DataMCSpectraComparison/samples.py
@@ -4,7 +4,7 @@ import os
 from SUSYBSMAnalysis.Zprime2muAnalysis.tools import files_from_dbs
 
 class sample:
-    def __init__(self, name, nice_name, dataset, nevents, color, syst_frac, cross_section, k_factor=1, filenames=None, scheduler='condor', hlt_process_name='REDIGI38X', ana_dataset=None):
+    def __init__(self, name, nice_name, dataset, nevents, color, syst_frac, cross_section, k_factor=1, filenames=None, scheduler='glite', hlt_process_name='REDIGI311X', ana_dataset=None):
         self.name = name
         self.nice_name = nice_name
         self.dataset = dataset
@@ -54,19 +54,19 @@ class sample:
 # https://twiki.cern.ch/twiki/bin/view/CMS/CrossSections_3XSeries for
 # xsecs (all below in pb)
 samples = [
-    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Fall10-START38_V12-v1/GEN-SIM-RECO',          1998931, 432, 0.05, 1631 - 0.97*1.3, hlt_process_name='HLT'),
-    sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 433, 0.05, 0.97  - 0.027, k_factor=1.3, hlt_process_name='HLT'),
-    sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 434, 0.05, 0.027 - 0.003, k_factor=1.3, hlt_process_name='HLT'),
-    sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                             55000, 435, 0.05, 0.003,         k_factor=1.3, hlt_process_name='HLT'),
-    sample('ttbar',        't#bar{t}',                                           '/TTJets_TuneZ2_7TeV-madgraph-tauola/Fall10-START38_V12-v2/GEN-SIM-RECO',                    1167759,   2, 0.15,  157,  hlt_process_name='HLT'),
-    sample('singletop_tW', 'tW',                                                 '/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/Fall10-START38_V12-v2/GEN-SIM-RECO',                494961,   1, 0.075, 10.6, hlt_process_name='HLT'),
-    sample('ww',           'WW',                                                 '/WWtoAnything_TuneZ2_7TeV-pythia6-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               2061760,   4, 0.035, 43,  scheduler='glite'),
-    sample('wz',           'WZ',                                                 '/WZtoAnything_TuneZ2_7TeV-pythia6-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               2194752,   5, 0.038, 18,  scheduler='glite'),
-    sample('zz',           'ZZ',                                                 '/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               2113368,   6, 0.025, 5.9, scheduler='glite'),
-    sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_CT10_TuneZ2_7TeV-powheg-pythia-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO', 1994719,  46, 0.05, 1631, hlt_process_name='HLT'),
-    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Fall10-START38_V12-v1/GEN-SIM-RECO',               15168266,   3, 0.05,  3.1e4, hlt_process_name='HLT'),
-    sample('inclmu15',     'QCD (MuRich, muon p_{T} > 15 GeV)',                  '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',        29504866, 801, 0.1,   0.0002855 * 296600000),
-    sample('zssm750',      'Z\'_{SSM} (750 GeV) #rightarrow #mu^{+}#mu^{-}',     '/ZprimeSSMToMuMu_M-750_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO',                      55000,  38, 0.05,  0.355, k_factor=1.3, hlt_process_name='HLT'),
+    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Spring11-PU_S1_START311_V1G1-v1/AODSIM',      1984154, 432, 0.05, 1631 - 0.97*1.3),
+#   sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                         55000, 433, 0.05, 0.97  - 0.027, k_factor=1.3, scheduler='condor'),
+#   sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                         55000, 434, 0.05, 0.027 - 0.003, k_factor=1.3, scheduler='condor'),
+#   sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                         55000, 435, 0.05, 0.003,         k_factor=1.3, scheduler='condor'),
+    sample('ttbar',        't#bar{t}',                                           '/TTJets_TuneZ2_7TeV-madgraph-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                1164208,   2, 0.15,  157),
+    sample('singletop_tW', 'tW',                                                 '/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/Spring11-PU_S1_START311_V1G1-v1/AODSIM',            489417,   1, 0.075, 10.6),
+    sample('ww',           'WW',                                                 '/WWtoAnything_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           2061760,   4, 0.035, 43),
+    sample('wz',           'WZ',                                                 '/WZtoAnything_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           2108416,   5, 0.038, 18),
+    sample('zz',           'ZZ',                                                 '/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           2108608,   6, 0.025, 5.9),
+    sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',        2057446,  46, 0.05, 1631),
+    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           15110974,   3, 0.05,  3.1e4, scheduler='condor'),
+    sample('inclmu15',     'QCD (MuRich, muon p_{T} > 15 GeV)',                  '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',    29429811, 801, 0.1,   0.0002855 * 296600000, scheduler='condor'),
+#   sample('zssm750',      'Z\'_{SSM} (750 GeV) #rightarrow #mu^{+}#mu^{-}',     '/ZprimeSSMToMuMu_M-750_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                  55000,  38, 0.05,  0.355, k_factor=1.3, scheduler='condor'),
 ]
 samples.reverse()
 
@@ -74,12 +74,7 @@ for sample in samples:
     exec '%s = sample' % sample.name
 
 from SUSYBSMAnalysis.Zprime2muAnalysis.tools import big_warn
-big_warn('modifying ttbar and singletop_tW nevents down by 25k since you lost one job from each')
-ttbar.nevents -= 25000
-singletop_tW.nevents -= 25000
-big_warn('also modifying wz down by 25k and and wjets down by 50k since 1/2 files of the tuple are stuck')
-wz.nevents -= 25000
-wjets.nevents -= 25000
+big_warn("don't worry, be happy")
 
 print 'samples are:'
 print ' '.join(s.name for s in samples)
@@ -104,5 +99,20 @@ if False:
         print parents
         os.system('dbsconfig %s > %s' % (parents[-1], s.name))
 
-    
-                
+    os.system('dbss nevents %s' % x.replace('RECO','RAW'))
+    os.system('dbss nevents %s' % x)
+
+if False:
+    import os
+    from dbstools import dbsparents
+    for s in samples:
+        print s.dataset
+        def fuf(y):
+            x = os.popen(y).read()
+            for line in x.split('\n'):
+                try:
+                    print int(line)
+                except ValueError:
+                    pass
+        fuf('dbss nevents %s' % s.dataset)
+        fuf('dbss nevents %s' % s.dataset.replace('AODSIM','GEN-SIM-RECO'))
--- test/DataMCSpectraComparison/tuple_mc.py
+++ test/DataMCSpectraComparison/tuple_mc.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
 import sys, os
-from SUSYBSMAnalysis.Zprime2muAnalysis.PATTools import switchHLTProcessName
+from SUSYBSMAnalysis.Zprime2muAnalysis.PATTools import switchHLTProcessName, AODOnly
 from tuple_common import cms, process, crab_cfg
 
-process.source.fileNames = ['/store/mc/Fall10/DYToMuMu_M-120_7TeV-pythia6/GEN-SIM-RECO/START38_V12-v1/0002/E8EE9B50-F1C8-DF11-B9B3-0018FE286F12.root']
+AODOnly(process)
+
+process.source.fileNames = ['file:/uscms/home/tucker/nobackup/Spring11/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/AODSIM/PU_S1_START311_V1G1-v1/0002/181B6610-494E-E011-A311-001A4BE1C5D4.root']
 process.maxEvents.input = 100
-process.GlobalTag.globaltag = 'START38_V14::All'
+process.GlobalTag.globaltag = 'START311_V2::All'
+
+switchHLTProcessName(process, 'REDIGI311X')
 
 if __name__ == '__main__' and 'submit' in sys.argv:
     job_control = '''
 total_number_of_events = -1
-events_per_job = 25000
+events_per_job = 50000
 '''
 
     just_testing = 'testing' in sys.argv
@@ -30,5 +34,3 @@ events_per_job = 25000
         if not just_testing:
             os.system('crab -create -submit all')
             os.system('rm crab.cfg')
-
-# 4 8 15 16 23 42

616227222ceb2436adf35d5fe696d592181d7b10
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-04-23 14:23:00 +0000

    Data through run 163078.

--- test/DataMCSpectraComparison/histos.py
+++ test/DataMCSpectraComparison/histos.py
@@ -159,7 +159,7 @@ return_data = 1
 '''
 
     just_testing = 'testing' in sys.argv
-    use_predefined_datasets = False
+    use_predefined_datasets = True
     
     # Run on data.
     if 'no_data' not in sys.argv:
@@ -169,7 +169,10 @@ return_data = 1
         dataset_details = []
         if use_predefined_datasets:
             dataset_details = [
-                ('SingleMu2011A_prompt_160329_161312', '/SingleMu/tucker-datamc_SingleMu2011A_prompt_160329_161312_20110330032441-b4af82d5ce94ab57cd9bc30d8eb6afae/USER', goodLumis_ll)
+                ('SingleMu2011A_prompt_160329_161312',              '/SingleMu/tucker-datamc_SingleMu2011A_prompt_160329_161312_20110330032441-b4af82d5ce94ab57cd9bc30d8eb6afae/USER', goodLumis_ll),
+                ('SingleMu2011A_prompt_162718_162828_minus_162822', '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162718_162828_20110421113929-44f5c264672bbeb96d2ae39b712749b4/USER', goodLumis_ll),
+                ('SingleMu2011A_prompt_162822',                     '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162822_162822_20110422155602-44f5c264672bbeb96d2ae39b712749b4/USER', goodLumis_ll),
+                ('SingleMu2011A_prompt_162829_163078_minus_162924', '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162829_163078_20110422160251-44f5c264672bbeb96d2ae39b712749b4/USER', goodLumis_ll),
                 ]
         else:
             # We'll try to figure out what the datasets are based on the publish logs.

25122c063587ccecbc067822b06b0b75d422f208
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-04-27 17:17:18 +0000

    Support specifying multiple versions of the same trigger path when only one of the specified set should exist per run, e.g. Mu24_v1/v2; note this is not meant to support checking e.g. Mu15_v2/Mu24_v1.

--- plugins/CheckPrescale.cc
+++ plugins/CheckPrescale.cc
@@ -13,13 +13,13 @@ private:
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
 
   const std::string hlt_process_name;
-  const std::string trigger_path;
+  const std::vector<std::string> trigger_paths;
   HLTConfigProvider hlt_cfg;
 };
 
 CheckPrescale::CheckPrescale(const edm::ParameterSet& cfg)
   : hlt_process_name(cfg.getParameter<std::string>("hlt_process_name")),
-    trigger_path(cfg.getParameter<std::string>("trigger_path"))
+    trigger_paths(cfg.getParameter<std::vector<std::string> >("trigger_paths"))
 {
 }
 
@@ -35,9 +35,26 @@ void CheckPrescale::beginRun(const edm::Run& run, const edm::EventSetup& setup) 
 }
 
 void CheckPrescale::analyze(const edm::Event& event, const edm::EventSetup& setup) {
-  int prescale = hlt_cfg.prescaleValue(event, setup, trigger_path);
-  if (prescale != 1)
-    throw cms::Exception("CheckPrescale") << "trigger path " << trigger_path << " has prescale != 1! prescale = " << prescale << "\n";
+  // The idea behind having trigger_paths be a vector is to handle
+  // cases like HLT_Mu24_v1, v2, etc. In that case we expect exactly
+  // one of the versions to be available, so check for this. This is
+  // not meant to handle checking more-fundamentally different trigger
+  // paths, e.g. HLT_Mu15_v2, HLT_Mu24_v1, but only different versions
+  // of the "same" path.
+  bool found = false;
+  for (std::vector<std::string>::const_iterator trigger_path = trigger_paths.begin(), end = trigger_paths.end(); trigger_path != end; ++trigger_path) {
+    if (hlt_cfg.triggerIndex(*trigger_path) != hlt_cfg.size()) {
+      if (found)
+	throw cms::Exception("CheckPrescale") << "a version of the trigger path " << *trigger_path << " was already found; probably you misconfigured.\n";
+      found = true;
+    }
+    else
+      continue;
+
+    int prescale = hlt_cfg.prescaleValue(event, setup, *trigger_path);
+    if (prescale != 1)
+      throw cms::Exception("CheckPrescale") << "trigger path " << *trigger_path << " has prescale != 1! prescale = " << prescale << "\n";
+  }
 }
 
 DEFINE_FWK_MODULE(CheckPrescale);

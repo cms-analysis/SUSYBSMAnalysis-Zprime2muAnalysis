e95ea94534ac985a6dabc5c28d53d7ffff70506a
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-04-27 12:25:33 +0000

    DCSOnlyForNewRuns.

--- python/goodlumis.py
+++ python/goodlumis.py
@@ -5,11 +5,14 @@ def for_cmssw(ll):
 
 DCSOnly_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt')
 
+DCSOnlyForNewRuns_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt')
+DCSOnlyForNewRuns_ll.removeRuns(xrange(160404, 162917+1))
+
 Prompt_ll          = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-162917_7TeV_PromptReco_Collisions11_JSON.txt')
 PromptMuonsOnly_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-162917_7TeV_PromptReco_Collisions11_JSON_MuonPhys.txt')
 
 Run2011A_ll = Prompt_ll
 Run2011AMuonsOnly_ll = PromptMuonsOnly_ll
 
-for x in ['DCSOnly', 'Prompt', 'PromptMuonsOnly', 'Run2011A', 'Run2011AMuonsOnly']:
+for x in ['DCSOnly', 'DCSOnlyForNewRuns', 'Prompt', 'PromptMuonsOnly', 'Run2011A', 'Run2011AMuonsOnly']:
     exec '%s = for_cmssw(%s_ll)' % (x,x)
--- test/DataMCSpectraComparison/histos.py
+++ test/DataMCSpectraComparison/histos.py
@@ -163,7 +163,7 @@ return_data = 1
     
     # Run on data.
     if 'no_data' not in sys.argv:
-        from SUSYBSMAnalysis.Zprime2muAnalysis.goodlumis import DCSOnly_ll, Run2011AMuonsOnly_ll
+        from SUSYBSMAnalysis.Zprime2muAnalysis.goodlumis import DCSOnlyForNewRuns_ll, Run2011AMuonsOnly_ll
         from SUSYBSMAnalysis.Zprime2muAnalysis.crabtools import dataset_from_publish_log
 
         dataset_details = []
@@ -173,14 +173,14 @@ return_data = 1
                 ('SingleMu2011A_prompt_162718_162828_minus_162822', '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162718_162828_20110421113929-44f5c264672bbeb96d2ae39b712749b4/USER', Run2011AMuonsOnly_ll),
                 ('SingleMu2011A_prompt_162822',                     '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162822_162822_20110422155602-44f5c264672bbeb96d2ae39b712749b4/USER', Run2011AMuonsOnly_ll),
                 ('SingleMu2011A_prompt_162829_163078_minus_162924', '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162829_163078_20110422160251-44f5c264672bbeb96d2ae39b712749b4/USER', Run2011AMuonsOnly_ll),
-                ('SingleMu2011A_prompt_162918_163078_minus_162924_DCSOnly', '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162829_163078_20110422160251-44f5c264672bbeb96d2ae39b712749b4/USER', DCSOnly_ll),
+                ('SingleMu2011A_prompt_162918_163078_minus_162924_DCSOnly', '/SingleMu/tucker-datamc_SingleMu2011A_prompt_162829_163078_20110422160251-44f5c264672bbeb96d2ae39b712749b4/USER', DCSOnlyForNewRuns_ll),
                 ]
         else:
             # We'll try to figure out what the datasets are based on the publish logs.
             for fn in glob.glob('crab/publish_logs/publish.crab_datamc_SingleMu2011A_prompt_*'):
                 name = fn.replace('crab/publish_logs/publish.crab_datamc_', '')
                 dataset = dataset_from_publish_log(fn)
-                dataset_details.append((name, dataset, Run2011AMuonsOnly_ll | DCSOnly_ll))
+                dataset_details.append((name, dataset, Run2011AMuonsOnly_ll | DCSOnlyForNewRuns_ll))
                 
         for name, ana_dataset, lumi_list in dataset_details:
             print name

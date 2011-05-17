d89c3e70a6ff320c865214a86e419ce820c8e8e3
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-05-17 23:34:31 +0000

    JSON through run 163869; hook for dumping JSON files from the LumiList objects.

--- python/goodlumis.py
+++ python/goodlumis.py
@@ -6,13 +6,20 @@ def for_cmssw(ll):
 DCSOnly_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt')
 
 DCSOnlyForNewRuns_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt')
-DCSOnlyForNewRuns_ll.removeRuns(xrange(160404, 162917+1))
+DCSOnlyForNewRuns_ll.removeRuns(xrange(160404, 163869+1))
 
-Prompt_ll          = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-162917_7TeV_PromptReco_Collisions11_JSON.txt')
-PromptMuonsOnly_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-162917_7TeV_PromptReco_Collisions11_JSON_MuonPhys.txt')
+Prompt_ll          = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-163869_7TeV_PromptReco_Collisions11_JSON.txt')
+PromptMuonsOnly_ll = LumiList('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-163869_7TeV_PromptReco_Collisions11_JSON_MuonPhys.txt')
 
 Run2011A_ll = Prompt_ll
 Run2011AMuonsOnly_ll = PromptMuonsOnly_ll
 
 for x in ['DCSOnly', 'DCSOnlyForNewRuns', 'Prompt', 'PromptMuonsOnly', 'Run2011A', 'Run2011AMuonsOnly']:
     exec '%s = for_cmssw(%s_ll)' % (x,x)
+
+if __name__ == '__main__':
+    import sys
+    if 'WriteGoodLumiJSONs' in sys.argv:
+        for name in ['DCSOnly_ll', 'DCSOnlyForNewRuns_ll', 'Prompt_ll', 'PromptMuonsOnly_ll', 'Run2011A_ll', 'Run2011AMuonsOnly_ll']:
+            obj = eval(name)
+            obj.writeJSON('zp2mu_goodlumis_%s.json' % name.replace('_ll', ''))

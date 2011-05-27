47bd69686b4427f67922ece4e8e364f93b54db43
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-05-27 02:13:36 +0000

    *** empty log message ***

--- test/DataMCSpectraComparison/tasks.py
+++ test/DataMCSpectraComparison/tasks.py
@@ -53,10 +53,10 @@ elif cmd == 'gatherhistos':
     extra = extra[0] if extra else 'renameme'
     do('''
 mkdir -p ana_datamc_%(extra)s
-ln -s /uscms_data/d2/tucker/zp2mu_ana_datamc_mc/V00-10-08 ana_datamc_%(extra)s/mc
-hadd ana_datamc_%(extra)s/ana_datamc_data.root crab/crab_ana_datamc_SingleMu2011A_*/res/*root
+ln -s /uscms_data/d2/tucker/zp2mu_ana_datamc_mc/V00-10-11 ana_datamc_%(extra)s/mc
+hadd ana_datamc_%(extra)s/ana_datamc_data.root crab/crab_ana_datamc_SingleMu2011A_May10_MuonsOnly/res/*root
 ''' % locals())
-    dirs = glob.glob('crab/crab_ana_datamc_SingleMu2011A_*')
+    dirs = glob.glob('crab/crab_ana_datamc_SingleMu2011A_May10_MuonsOnly')
     for dir in dirs:
         do('crab -c %s -report' % dir)
     jsons = [os.path.join(dir, 'res/lumiSummary.json') for dir in dirs]

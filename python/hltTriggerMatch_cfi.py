2461616da130c00f08d9555506f0155d00279f7a
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-05-27 00:23:09 +0000

    HLT_Mu30.

--- python/hltTriggerMatch_cfi.py
+++ python/hltTriggerMatch_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 muonTriggerMatchHLTMuons = cms.EDProducer('PATTriggerMatcherDRDPtLessByR',
     src                   = cms.InputTag('cleanPatMuons'),
     matched               = cms.InputTag('patTrigger'),
-    matchedCuts           = cms.string('type("TriggerMuon") && (path("HLT_Mu15*") || path("HLT_Mu20*") || path("HLT_Mu24*"))'), # (path("HLT_Mu*") || path("HLT_DoubleMu*"))
+    matchedCuts           = cms.string('type("TriggerMuon") && (path("HLT_Mu9*") || path("HLT_Mu15*") || path("HLT_Mu24*") || path("HLT_Mu30*"))'),
     # Follow VBTF's matching criteria.
     maxDPtRel             = cms.double(1),
     maxDeltaR             = cms.double(0.2),

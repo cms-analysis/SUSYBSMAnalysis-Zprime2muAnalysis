45cf5651fa80b9d0eea2b5f9ea8cf0a96eb4606e
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-01 15:21:11 +0000

    Start of Summer11 samples.

--- test/DataMCSpectraComparison/samples.py
+++ test/DataMCSpectraComparison/samples.py
@@ -5,7 +5,7 @@ from SUSYBSMAnalysis.Zprime2muAnalysis.tools import files_from_dbs
 from SUSYBSMAnalysis.Zprime2muAnalysis.crabtools import dataset_from_publish_log
 
 class sample:
-    def __init__(self, name, nice_name, dataset, nevents, color, syst_frac, cross_section, k_factor=1, filenames=None, scheduler='glite', hlt_process_name='REDIGI311X', ana_dataset=None):
+    def __init__(self, name, nice_name, dataset, nevents, color, syst_frac, cross_section, k_factor=1, filenames=None, scheduler='glite', hlt_process_name='HLT', ana_dataset=None):
         self.name = name
         self.nice_name = nice_name
         self.dataset = dataset
@@ -52,28 +52,32 @@ class sample:
 # https://twiki.cern.ch/twiki/bin/view/CMS/CrossSections_3XSeries for
 # xsecs (all below in pb)
 samples = [
-    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Spring11-PU_S1_START311_V1G1-v1/AODSIM',      1984154, 432, 0.05, 1631 - 0.97*1.3),
-#   sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                         55000, 433, 0.05, 0.97  - 0.027, k_factor=1.3, scheduler='condor'),
-#   sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                         55000, 434, 0.05, 0.027 - 0.003, k_factor=1.3, scheduler='condor'),
-#   sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                         55000, 435, 0.05, 0.003,         k_factor=1.3, scheduler='condor'),
-    sample('ttbar',        't#bar{t}',                                           '/TTJets_TuneZ2_7TeV-madgraph-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                1164208,   2, 0.15,  157),
+    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM',                   2148325, 432, 0.05, 1631 - 7.9*1.3),
+    sample('dy120',        'DY120',                                              '/DYToMuMu_M-120_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             54550, 433, 0.05, 7.9, k_factor=1.3),
+    sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 433, 0.05, 0.97  - 0.027,  k_factor=1.3),
+    sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 434, 0.05, 0.027 - 0.0031, k_factor=1.3),
+    sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 435, 0.05, 0.0031,         k_factor=1.3),
+    sample('dy1000',       'DY1000',                                             '/DYToMuMu_M-1000_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',            55000, 435, 0.05, 9.7e-4,         k_factor=1.3),
+    sample('ttbar',        't#bar{t}',                                           '/TT_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',                       1089625,   2, 0.15, 157),
     sample('singletop_tW', 'tW',                                                 '/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/Spring11-PU_S1_START311_V1G1-v1/AODSIM',            489417,   1, 0.075, 10.6),
     sample('ww',           'WW',                                                 '/WWtoAnything_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           2061760,   4, 0.035, 43),
     sample('wz',           'WZ',                                                 '/WZtoAnything_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           2108416,   5, 0.038, 18),
     sample('zz',           'ZZ',                                                 '/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           2108608,   6, 0.025, 5.9),
-    sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',        2057446,  46, 0.05, 1631),
+    sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',          2032536,  46, 0.05, 1631),
     sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           15110974,   3, 0.05,  3.1e4, scheduler='condor'),
     sample('inclmu15',     'QCD (MuRich, muon p_{T} > 15 GeV)',                  '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',    29429811, 801, 0.1,   0.0002855 * 296600000, scheduler='condor'),
 #   sample('zssm750',      'Z\'_{SSM} (750 GeV) #rightarrow #mu^{+}#mu^{-}',     '/ZprimeSSMToMuMu_M-750_7TeV-pythia6/Spring11-PU_S1_START311_V1G1-v1/AODSIM',                  55000,  38, 0.05,  0.355, k_factor=1.3, scheduler='condor'),
 ]
+for sample in samples:
+    if 'START311' in sample.dataset:
+        sample.hlt_process_name = 'REDIGI311X'
 samples.reverse()
 
 for sample in samples:
     exec '%s = sample' % sample.name
 
 from SUSYBSMAnalysis.Zprime2muAnalysis.tools import big_warn
-big_warn("dropping 100kevt from ttbar since you lost 2 jobs")
-ttbar.nevents -= 100000
+big_warn("roll that beautiful bean footage")
 
 print 'samples are:'
 print ' '.join(s.name for s in samples)

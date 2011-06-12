af418bf5d09e21eaf13d40f8d10987caabcb60f5
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-12 05:28:37 +0000

    Follow (old) code change.

--- test/TriggerSummary.py
+++ test/TriggerSummary.py
@@ -11,12 +11,12 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 from SUSYBSMAnalysis.Zprime2muAnalysis.cmsswtools import files_from_argv
 files_from_argv(process)
 
-hlt = 'REDIGI38X'
+hlt = 'HLT'
 
 process.load('HLTrigger.HLTcore.triggerSummaryAnalyzerAOD_cfi')
 process.triggerSummaryAnalyzerAOD.inputTag = cms.InputTag('hltTriggerSummaryAOD', '', hlt)
 
-process.printEvent = cms.EDAnalyzer('PrintEvent', hlt_src = cms.InputTag('TriggerResults', '', hlt))
+process.printEvent = cms.EDAnalyzer('PrintEvent', trigger_results_src = cms.InputTag('TriggerResults', '', hlt))
 process.MessageLogger.categories.append('PrintEvent')
 
 process.p = cms.Path(process.printEvent * process.triggerSummaryAnalyzerAOD)

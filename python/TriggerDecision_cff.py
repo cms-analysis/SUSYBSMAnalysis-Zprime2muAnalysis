e2bc55b145e14c0261606fd549906c73577d7a00
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-12 05:23:33 +0000

    To L1_SingleMu12 and HLT_Mu30_v1 (as in the 4_2_3_patch3 menu that the Summer11 MC used).

--- python/TriggerDecision_cff.py
+++ python/TriggerDecision_cff.py
@@ -4,6 +4,6 @@ triggerDecision = cms.PSet(doingElectrons = cms.bool(False),
                            useTrigger = cms.bool(True),
                            l1GtObjectMap = cms.InputTag('hltL1GtObjectMap'),
                            hltResults = cms.InputTag('TriggerResults', '', 'HLT'),
-                           l1Paths = cms.vstring('L1_SingleMu7'),
-                           hltPaths = cms.vstring('HLT_Mu11')
+                           l1Paths = cms.vstring('L1_SingleMu12'),
+                           hltPaths = cms.vstring('HLT_Mu30_v1')
                            )

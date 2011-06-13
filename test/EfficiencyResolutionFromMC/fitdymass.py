b5eec09378286177d67216fcb66e2d01adefdf7b
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-13 00:51:21 +0000

    Fix a couple of typos; improve a comment; improve axis titles; do residuals; binning to 5 GeV.

--- test/EfficiencyResolutionFromMC/fitdymass.py
+++ test/EfficiencyResolutionFromMC/fitdymass.py
@@ -10,14 +10,15 @@ ROOT.TH1.AddDirectory(0)
 ps = plot_saver('plots/fitdymass')
 
 int_lumi = 1000.
-rebin = 2
+rebin = 5
 
 masses = [20, 120, 200, 500, 800, 1000]
 nevents = [2148325, 54550] + [55000] * 4
-sigmas = [1631, 7.9, 0.97, 0.027, 0.0031, 9.7e-4]
+sigmas = [1631, 7.9, 0.97, 0.027, 0.0031, 9.7e-4] # in pb
 for i in xrange(len(sigmas) - 1):
     sigmas[i] - sigmas[i+1]
 weights = [int_lumi / nev * sig for nev,sig in zip(nevents, sigmas)]
+#weights = [x/weights[-1] for x in weights]
 
 hists = []
 for m,w in zip(masses, weights):
@@ -31,7 +32,7 @@ for m,w in zip(masses, weights):
     h.GetXaxis().SetRangeUser(40, 2000)
     h.Scale(w)
     h.Draw()
-    ps.save('rawmass%i')
+    ps.save('rawmass%i' % m)
     hists.append(h)
 
 htot = hists[0].Clone('htot')
@@ -40,31 +41,58 @@ for j in xrange(1, len(hists)):
     htot.Add(hists[j])
 htot.SetTitle('')
 htot.Draw()
-htot.GetXaxis().SetTitle('reconstructed M _{#mu#mu} (GeV)')
-htot.GetYaxis().SetTitle('Events/%i GeV' % rebin) # assumes original started out with 1 GeV bins
+htot.GetXaxis().SetTitle('reconstructed m(#mu^{+}#mu^{-}) (GeV)')
+htot.GetYaxis().SetTitle('Events/%i GeV/%.1f fb^{-1}' % (rebin, int_lumi/1000)) # assumes original started out with 1 GeV bins, and the in
 #htot.Scale(1/htot.Integral(htot.FindBin(300), htot.FindBin(2000)))
+
+#file = ROOT.TFile('dy.root', 'recreate')
+#htot.SetDirectory(file)
+#file.Write()
+#file.Close
+#raise 'done'
     
-def fit_just_tail(lo, hi):
-    fcn = ROOT.TF1('fcn', 'exp([0] + [1] * x**[2])', lo, hi)
-    fcn.SetParLimits(0, 0, 10000)
-    fcn.SetParLimits(1, -10, 0)
-    fcn.SetParLimits(2, 0, 1)
-    #fcn.FixParameter(2, 0.3)
-    fcn.SetParNames("Norm", "a", "b")
+def fit_it(lo, hi):
+ #   fcn = ROOT.TF1('fcn', 'exp([0] + [1] * x**[2])', lo, hi)
+    fcn = ROOT.TF1('fcn', 'exp([0] + [1]*x)/x**[2]', lo, hi)
+    fcn.SetParLimits(0, 0, 1000)
+    fcn.SetParLimits(1, -20, 0)
+    fcn.SetParLimits(2, 0, 10)
+    fcn.SetParNames("N", "a", "b")
     fcn.SetLineColor(ROOT.kBlue)
-        
-    htot.Fit(fcn, 'VR')
+
+    htot.Fit(fcn, 'LVR')
 
     ps.c.Update()
     s = htot.GetListOfFunctions().FindObject("stats")
+    s.SetX1NDC(0.73)
+    s.SetY1NDC(0.75)
+    s.SetOptStat(10)
     s.SetOptFit(111)
     s.Draw()
 
     ps.save('mass%i_%i' % (lo, hi))
 
-#l = [120] + range(200, 2000, 200) + [2000]
-#for lo,hi in zip(l,l[1:]):
-#    fit_just_tail(lo,hi)
-#    fit_just_tail(lo, 2000)
+    xax = htot.GetXaxis()
+    hres = ROOT.TH1F('hres_%i_%i' % (lo, hi), 'relative residual;m(#mu^{+}#mu^{-}) (GeV);f(m_{i})/h_{i} - 1', htot.GetNbinsX(), xax.GetBinLowEdge(1), xax.GetBinLowEdge(htot.GetNbinsX()+1))
+    for h in [hres]:
+        h.GetYaxis().SetLabelSize(0.02)
+        h.SetMarkerStyle(2)
+        h.SetStats(0)
+    for i in xrange(1, hres.GetNbinsX()+1):
+        xlo = xax.GetBinLowEdge(i)
+        xhi = xax.GetBinLowEdge(i+1)
+        if xlo >= lo and xhi <= hi:
+            res = fcn.Integral(xlo, xhi)/(xhi-xlo) - htot.GetBinContent(i)
+            if htot.GetBinContent(i) > 0:
+                hres.SetBinContent(i, res/htot.GetBinContent(i))
+                hres.SetBinError(i, htot.GetBinError(i)/htot.GetBinContent(i))
 
-fit_just_tail(300, 2000)
+    hres.Draw('e')
+    ps.save('res_%i_%i' % (lo, hi), log=False)
+    
+                              
+l = range(200, 2200, 200)
+l = [400, 600, 800]
+for lo in l:
+    fit_it(lo, 2000)
+#fit_it(400,2000)

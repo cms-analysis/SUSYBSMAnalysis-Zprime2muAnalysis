b34eff1d5afdca454e7a84d53b0848d1759d1490
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-14 14:21:25 +0000

    Beamspot and PV tags.

--- python/HistosFromPAT_cfi.py
+++ python/HistosFromPAT_cfi.py
@@ -4,4 +4,6 @@ HistosFromPAT = cms.EDAnalyzer('Zprime2muHistosFromPAT',
                                lepton_src = cms.InputTag('leptons', 'muons'),
                                dilepton_src = cms.InputTag('dimuons'),
                                leptonsFromDileptons = cms.bool(False),
+                               beamspot_src = cms.InputTag('offlineBeamSpot'),
+                               vertex_src = cms.InputTag('offlinePrimaryVertices'),
                                )

5c4ea4b80a25c964ebf7112d099d6870e1fded45
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-15 11:22:43 +0000

    Restructure, especially to allow for more mass ranges.

--- test/NMinus1Effs/draw.py
+++ test/NMinus1Effs/draw.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys, os, glob
 from SUSYBSMAnalysis.Zprime2muAnalysis.roottools import *
 set_zp2mu_style()
 ROOT.gStyle.SetPadTopMargin(0.02)
 ROOT.gStyle.SetPadRightMargin(0.02)
 ROOT.gStyle.SetTitleX(0.12)
 #ROOT.gStyle.SetTitleH(0.07)
-c = ROOT.TCanvas('c', '', 820, 630)
-os.system('mkdir -p plots/nminus1effs')
+
+ps = plot_saver('plots/nminus1effs', log=False)
 
 nminus1s = [
-    'NoTkHits',
+    #'NoPt',
     'NoDB',
     'NoGlbChi2',
+    'NoIso',
+    'NoTkHits',
     'NoPxHits',
     'NoMuHits',
-    'NoMuSegs',
-    'NoTkMuon',
+    'NoMuMatch',
     'NoTrgMtch',
     'NoB2B',
     'NoVtxProb',
+    'NoCosm',
 ]
 
-if 'test' in sys.argv:
-    nminus1s += ['NoPt', 'NoPt35Pt5']
+mass_ranges = [
+    ('60m120',  '60 < M < 120 GeV',  ( 60, 120)),
+    ('120m200', '120 < M < 200 GeV', (120, 200)),
+    ('200m400', '200 < M < 400 GeV', (200, 400)),
+    ('400m',    'M > 400 GeV',       (400, 1e9)),
+    ]
+
+to_use = {
+    '60m120':  ['ana_nminus1_data.root', 'ana_nminus1_zmumu.root', 'ana_nminus1_ttbar.root'],  #, 'ana_nminus1_inclmu15.root'],
+    '120m200': ['ana_nminus1_data.root', 'ana_nminus1_dy120.root', 'ana_nminus1_ttbar.root'],
+    '200m400': ['ana_nminus1_data.root', 'ana_nminus1_dy200.root', 'ana_nminus1_ttbar.root'],
+    '400m':    ['ana_nminus1_data.root', 'ana_nminus1_dy500.root', 'ana_nminus1_dy1000.root', 'ana_nminus1_ttbar.root'],
+    }
 
 pretty = {
     'NoTkHits': '# tk hits #gt 10',
@@ -36,110 +49,96 @@ pretty = {
     'NoTrgMtch': 'HLT match',
     'NoB2B': 'B2B cosmics',
     'NoVtxProb': '#chi^{2} #mu#mu vtx < 10',
+    'NoIso': 'rel. tk. iso.',
     'ana_nminus1_data.root': 'Data, 366 pb ^{-1}',
-    'ana_nminus1_zmumu.root': 'Z#rightarrow#mu#mu',
-    'ana_nminus1_dy120.root': 'Z#rightarrow#mu#mu',
+    'ana_nminus1_zmumu.root': 'Z#rightarrow#mu#mu, 60 < M < 120 GeV',
+    'ana_nminus1_dy120.root': 'DY, M > 120 GeV',
+    'ana_nminus1_dy200.root': 'DY, M > 200 GeV',
+    'ana_nminus1_dy500.root': 'DY, M > 500 GeV',
+    'ana_nminus1_dy1000.root': 'DY, M > 1000 GeV',
     'ana_nminus1_ttbar.root': 't#bar{t}',
     'ana_nminus1_inclmu15.root': 'QCD',
     'ana_nminus1_zssm1000.root': 'Z\' SSM, M=1000 GeV',
     }
 
-if 'debug' in sys.argv:
-    fn = 'zp2mu_histos.root'
-    f = ROOT.TFile(fn)
-    for nminus1 in nminus1s + ['NoNo']:
-        print nminus1, f.Get(nminus1).Get('DileptonMass').GetEntries()
-    sys.exit(0)
+styles = {
+    'ana_nminus1_data.root':      (ROOT.kBlack,     -1),
+    'ana_nminus1_zmumu.root':     (8,             3001),
+    'ana_nminus1_dy120.root':     (ROOT.kBlue,    3004),
+    'ana_nminus1_dy200.root':     (ROOT.kGreen+2, 3005),
+    'ana_nminus1_dy500.root':     (ROOT.kOrange,  3006),
+    'ana_nminus1_dy1000.root':    (ROOT.kPink,    3007),
+    'ana_nminus1_ttbar.root':     (46,            3016),
+    'ana_nminus1_inclmu15.root':  (28,            3020),
+    }
         
-def table(fn):
-    f = ROOT.TFile(fn)
+def table(f, mass_range):
     hnum = f.Get('NoNo').Get('DileptonMass')
-    num60120 = get_integral(hnum, 60, 120, integral_only=True, include_last_bin=False)
-    num120 = get_integral(hnum, 120, integral_only=True, include_last_bin=False)
-    print 'numerator: 60-120:', num60120, ' 120-:', num120
-    print '%20s%20s%20s%20s%20s' % ('name', 'den 60-120', 'eff 60-120', 'den 120', 'eff 120')
+    mlo, mhi = mass_range
+    num = get_integral(hnum, mlo, mhi, integral_only=True, include_last_bin=False)
+    print 'mass range %5i-%5i:' % mass_range
+    print 'numerator:', num
+    print '%20s%20s%20s%30s' % ('cut', 'denominator', 'efficiency', '68% CL')
     for nminus1 in nminus1s:
         hden = f.Get(nminus1).Get('DileptonMass')
-        den60120 = get_integral(hden, 60, 120, integral_only=True, include_last_bin=False)
-        den120 = get_integral(hden, 120, integral_only=True, include_last_bin=False)
-        print '%20s%20f%20f%20f%20f' % (nminus1, den60120, num60120/den60120, den120, num120/den120)
+        den = get_integral(hden, mlo, mhi, integral_only=True, include_last_bin=False)
+        e,l,h = clopper_pearson(num, den)
+        print '%20s%20f%20f%15f%15f' % (nminus1, den, e, l, h)
 
-if '120' in sys.argv:
-    items = [
-        ('ana_nminus1_data.root',    (120, 1e9), ROOT.kBlack, -1),
-        ('ana_nminus1_dy120.root',   (120, 1e9), ROOT.kBlue, 3006),
-        ('ana_nminus1_ttbar.root',   (120, 1e9), ROOT.kRed, 3007),
-        ('ana_nminus1_zssm1000.root', (120, 1e9), ROOT.kGreen+2, 3004)
-        ]
+for name, pretty_name, mass_range in mass_ranges:
+    print name, pretty_name
+
     lg = ROOT.TLegend(0.14, 0.14, 0.63, 0.34)
-elif 'test' in sys.argv:
-    items = [
-        ('ana_nminus1_testzmumu.root', (60, 120), 0, 0),
-        ('ana_nminus1_testzp500.root', (0, 1e9), 0, 0),
-        ]
-    lg = ROOT.TLegend(0.14, 0.14, 0.63, 0.34)
-else:         
-    items = [
-        ('ana_nminus1_data.root',     (60,120), ROOT.kBlack, -1),
-        ('ana_nminus1_zmumu.root',    (60,120), ROOT.kBlue, 3006),
-        ('ana_nminus1_ttbar.root',    (60,120), ROOT.kRed, 3007),
-        ]
-    lg = ROOT.TLegend(0.14, 0.14, 0.50, 0.32)
+    lg.SetFillColor(0)
+    
+    same = 'A'
+    effs = []
+    
+    for fn in to_use[name]:
+        print fn
+        f = ROOT.TFile(fn)
+        table(f, mass_range)
 
-same = 'A'
-effs = []
-lg.SetFillColor(0)
-for fn, mass_range, color, fill in items:
-    f = ROOT.TFile(fn)
+        color, fill = styles[fn]
 
-    l = len(nminus1s)
-    nminus1_num = ROOT.TH1F('num', '', l, 0, l)
-    nminus1_den = ROOT.TH1F('den', '', l, 0, l)
+        l = len(nminus1s)
+        nminus1_num = ROOT.TH1F('num', '', l, 0, l)
+        nminus1_den = ROOT.TH1F('den', '', l, 0, l)
 
-    hnum = f.Get('NoNo').Get('DileptonMass')
-    num = get_integral(hnum, *mass_range, integral_only=True, include_last_bin=False)
-    for i,nminus1 in enumerate(nminus1s):
-        hden = f.Get(nminus1).Get('DileptonMass')
-        den = get_integral(hden, *mass_range, integral_only=True, include_last_bin=False)
-        nminus1_num.SetBinContent(i+1, num)
-        nminus1_den.SetBinContent(i+1, den)
+        hnum = f.Get('NoNo').Get('DileptonMass')
+        num = get_integral(hnum, *mass_range, integral_only=True, include_last_bin=False)
+        for i,nminus1 in enumerate(nminus1s):
+            hden = f.Get(nminus1).Get('DileptonMass')
+            den = get_integral(hden, *mass_range, integral_only=True, include_last_bin=False)
+            nminus1_num.SetBinContent(i+1, num)
+            nminus1_den.SetBinContent(i+1, den)
 
-    eff = binomial_divide(nminus1_num, nminus1_den)
-    if '120' in sys.argv:
-        eff.SetTitle('M_{#mu#mu} > 120 GeV')
-    else:
-        eff.SetTitle('60 < M_{#mu#mu} < 120 GeV')
-    eff.GetXaxis().SetRangeUser(0,12)
-    eff.GetYaxis().SetRangeUser(0.95,1.005)
-    eff.GetXaxis().SetTitle('cut')
-    eff.GetYaxis().SetLabelSize(0.03)
-    eff.GetYaxis().SetTitle('n-1 efficiency')
-    eff.GetYaxis().SetTitle('n-1 efficiency')
-    if 'data' in fn:
-        draw = 'P'
-        eff.SetLineColor(color)
-        lg.AddEntry(eff, pretty.get(fn, fn), 'L')
-    else:
-        draw = '2'
-        eff.SetLineColor(color)
-        eff.SetFillColor(color)
-        eff.SetFillStyle(fill)
-        lg.AddEntry(eff, pretty.get(fn, fn), 'F')
-    draw += same
-    eff.Draw(draw)
-    effs.append(eff)
-    same = ' same'
-    bnr = eff.GetXaxis().GetNbins()/eff.GetN()
-    for i in xrange(1,l+1):
-        eff.GetXaxis().SetBinLabel((i-1)*bnr+1, pretty.get(nminus1s[i-1], nminus1s[i-1]))
-    eff.GetXaxis().LabelsOption('u')
+        eff = binomial_divide(nminus1_num, nminus1_den)
+        eff.SetTitle(pretty_name)
+        eff.SetTitle('')
+        eff.GetYaxis().SetRangeUser(0.75,1.005)
+        eff.GetXaxis().SetTitle('cut')
+        eff.GetYaxis().SetLabelSize(0.03)
+        eff.GetYaxis().SetTitle('n-1 efficiency')
+        if 'data' in fn:
+            draw = 'P'
+            eff.SetLineColor(color)
+            lg.AddEntry(eff, pretty.get(fn, fn), 'L')
+        else:
+            draw = '2'
+            eff.SetLineColor(color)
+            eff.SetFillColor(color)
+            eff.SetFillStyle(fill)
+            lg.AddEntry(eff, pretty.get(fn, fn), 'F')
+        draw += same
+        eff.Draw(draw)
+        effs.append(eff)
+        same = ' same'
+        bnr = eff.GetXaxis().GetNbins()/eff.GetN()
+        for i in xrange(1,l+1):
+            eff.GetXaxis().SetBinLabel((i-1)*bnr+1, pretty.get(nminus1s[i-1], nminus1s[i-1]))
+        eff.GetXaxis().LabelsOption('u')
 
-    print fn
-    table(fn)
-
-lg.Draw()
-fn = 'plots/nminus1effs/nminus1%s.png' % ('120' if '120' in sys.argv else '')
-if 'test' not in sys.argv:
-    c.SaveAs(fn)
-    c.SaveAs(fn.replace('png', 'root'))
-
+    lg.Draw()
+    ps.save(name)
+    print

8c83aedb6c12cd97045622897e30aca50bf5f7ca
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-18 23:07:14 +0000

    Use plot_saver.

--- test/EfficiencyResolutionFromMC/resfromdy.py
+++ test/EfficiencyResolutionFromMC/resfromdy.py
@@ -6,11 +6,12 @@ set_zp2mu_style()
 ROOT.gStyle.SetPadTopMargin(0.02)
 ROOT.gStyle.SetPadRightMargin(0.02)
 
+ps = plot_saver('plots/resfromdy')
+
 f = ROOT.TFile('dyall.root')
 d = f.Resolutionpmc
 h = d.Get('DileptonMassResVMass')
 h = make_rms_hist(h)
-c = ROOT.TCanvas('c', '', 820, 630)
 
 h.SetTitle('')
 h.GetXaxis().SetTitle('dilepton mass (GeV)')
@@ -21,7 +22,7 @@ h.Draw()
 
 line = ROOT.TF1('line', '[0] + x*[1] + x*x*[2]', 120, 1200)
 h.Fit(line, 'IVR')
-c.Update()
+ps.c.Update()
 s = h.GetListOfFunctions().FindObject("stats")
 s.SetOptStat(0)
 s.SetOptFit(111)
@@ -29,9 +30,9 @@ s.SetY1NDC(0.15)
 s.SetY2NDC(0.40)
 s.SetX1NDC(0.50)
 s.SetX2NDC(0.95)
-c.Update()
+ps.c.Update()
 
-if 0:
+if 1:
     chnam, val, err, xlolim, xhilim, iuint = ROOT.TString(''), ROOT.Double(1), ROOT.Double(1), ROOT.Double(1), ROOT.Double(1), ROOT.Long(1)
 
     z = [0,0,0]
@@ -47,6 +48,4 @@ if 0:
     t.SetNDC()
     t.Draw()
 
-os.system('mkdir -p plots/resfromdy')
-c.SaveAs('plots/resfromdy/mass_res.png')
-c.SaveAs('plots/resfromdy/mass_res.root')
+ps.save('mass_res')

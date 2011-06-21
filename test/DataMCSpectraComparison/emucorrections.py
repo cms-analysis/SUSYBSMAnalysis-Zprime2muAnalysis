383a416161db1058de33c80ff5b969dcdf3edf92
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-21 14:46:52 +0000

    Improve binning/drawing, select which samples are used.

--- test/DataMCSpectraComparison/emucorrections.py
+++ test/DataMCSpectraComparison/emucorrections.py
@@ -1,34 +1,49 @@
 from array import array
-from SUSYBSMAnalysis.Zprime2muAnalysis.roottools import ROOT, plot_saver, set_zp2mu_style
+from SUSYBSMAnalysis.Zprime2muAnalysis.roottools import *
 set_zp2mu_style()
 
 ps = plot_saver('plots/emucorrections')
 
-from samples import samples
-for sample in samples:
-    f = ROOT.TFile('ana_datamc_mc/ana_datamc_%s.root' % sample.name)
+bins = array('d', range(0, 2000, 50))
+bins = array('d', [0, 60, 120, 200, 400, 600, 1000, 3000])
 
-    cut = 'Our'
-    mumu = f.Get(cut + 'MuonsPlusMuonsMinusHistos').Get('DileptonMass')
-    emu = f.Get(cut + 'MuonsElectronsOppSignHistos').Get('DileptonMass')
+mumu = ROOT.TH1F('mumu', '', len(bins)-1, bins)
+emu  = ROOT.TH1F('emu',  '', len(bins)-1, bins)
 
-    #for h in (mumu,emu):
-    #    h.Rebin(5)
+from samples import *
+cut = 'OurNew'
 
-    bins = array('d', range(0, 201, 40) + [2000])
-    for h,n in [(mumu, 'mumu'), (emu, 'emu')]:
+def draw(name, this_mumu, this_emu):
+    for h,c in [(this_emu, 9), (this_mumu, 46)]:
+        h.SetFillColor(c)
+        h.SetLineColor(c)
+        h.SetStats(0)
+        h.SetTitle('%s;dilepton mass (GeV);events/50 GeV' % sample.name)
+
+    h1,h2 = sort_histogram_pair(this_mumu, this_emu)
+    h1.Draw('hist')
+    h2.Draw('hist same')
+    ps.save('%s_mumu_emu' % name)
+
+    h = this_mumu.Clone('h')
+    h.Divide(this_emu)
+    h.GetXaxis().SetRangeUser(120,2000)
+    h.Draw('hist')
+    ps.save('%s_div' % name, log=False)
+
+for sample in [ztautau, zz, wz, ww, singletop_tW, ttbar]:
+    f = ROOT.TFile('/uscms/home/tucker/nobackup/zp2mu_ana_datamc_mc/latest/ana_datamc_%s.root' % sample.name)
+
+    this_mumu = f.Get(cut + 'MuonsPlusMuonsMinusHistos')  .Get('DileptonMass')
+    this_emu  = f.Get(cut + 'MuonsElectronsOppSignHistos').Get('DileptonMass')
+
+    for h,n in [(this_mumu, 'this_mumu'), (this_emu, 'this_emu')]:
         z = h.Rebin(len(bins)-1, n, bins)
         exec '%s = z' % n
 
-    mumu.Draw('hist')
-    ps.save('%s_mumu' % sample.name)
-    emu.Draw('hist')
-    ps.save('%s_emu' % sample.name)
+    mumu.Add(this_mumu, sample.partial_weight)
+    emu .Add(this_emu,  sample.partial_weight)
 
-    h = mumu.Clone('h')
-    h.Divide(emu)
-    h.Draw('hist')
-    ps.save('%s_div' % sample.name)
-    h.GetXaxis().SetRangeUser(120,2000)
-    ps.save('%s_div_120' % sample.name)
-    
+    draw(sample.name, this_mumu, this_emu)
+
+draw('total', mumu, emu)

330a124d88f9da8807b300c52ed7397f633b6b3b
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-06-22 05:07:35 +0000

    Improve binning (120, 200, 400, 600); better organization of code; store histograms in the sample objects for ease of use; draw mumu/emu separate histograms with error bars; use binomial_divide in ratio of Poisson means mode to derive the correction factor and errors in each bin, and plot the overlay of all the channels; in each bin and for the different samples, derive weights to be used to combine the ratios (not used yet), and plot them; don't bother adding channels separately in mumu/emu.

--- test/DataMCSpectraComparison/emucorrections.py
+++ test/DataMCSpectraComparison/emucorrections.py
@@ -4,46 +4,95 @@ set_zp2mu_style()
 
 ps = plot_saver('plots/emucorrections')
 
-bins = array('d', range(0, 2000, 50))
-bins = array('d', [0, 60, 120, 200, 400, 600, 1000, 3000])
+bins = array('d', [120, 200, 300, 400, 600])
 
-mumu = ROOT.TH1F('mumu', '', len(bins)-1, bins)
-emu  = ROOT.TH1F('emu',  '', len(bins)-1, bins)
+total_mumu = ROOT.TH1F('mumu', '', len(bins)-1, bins)
+total_emu  = ROOT.TH1F('emu',  '', len(bins)-1, bins)
 
 from samples import *
+samples = [ttbar, ww, singletop_tW, ztautau, wz, zz]
 cut = 'OurNew'
 
-def draw(name, this_mumu, this_emu):
-    for h,c in [(this_emu, 9), (this_mumu, 46)]:
-        h.SetFillColor(c)
+def draw(sample):
+    for h,c in [(sample.emu, 9), (sample.mumu, 46)]:
+        #h.SetFillColor(c)
+        h.SetLineWidth(2)
         h.SetLineColor(c)
         h.SetStats(0)
-        h.SetTitle('%s;dilepton mass (GeV);events/50 GeV' % sample.name)
+        h.SetTitle('%s;dilepton mass (GeV);arb. units' % sample.name)
 
-    h1,h2 = sort_histogram_pair(this_mumu, this_emu)
-    h1.Draw('hist')
-    h2.Draw('hist same')
-    ps.save('%s_mumu_emu' % name)
+    h1,h2 = sort_histogram_pair(sample.mumu, sample.emu)
+    h1.Draw('e')
+    h2.Draw('e same')
+    ps.save('%s_mumu_emu' % sample.name)
 
-    h = this_mumu.Clone('h')
-    h.Divide(this_emu)
-    h.GetXaxis().SetRangeUser(120,2000)
-    h.Draw('hist')
-    ps.save('%s_div' % name, log=False)
+    sample.div = g = binomial_divide(sample.mumu, sample.emu, clopper_pearson_poisson_means, force_lt_1=False)
+    g.SetTitle(';dilepton mass (GeV);correction factor n(#mu#mu)/n(e#mu)')
+    g.Draw('AP')
+    ps.save('%s_div' % sample.name, log=False)
 
-for sample in [ztautau, zz, wz, ww, singletop_tW, ttbar]:
+for sample in samples:
     f = ROOT.TFile('/uscms/home/tucker/nobackup/zp2mu_ana_datamc_mc/latest/ana_datamc_%s.root' % sample.name)
 
-    this_mumu = f.Get(cut + 'MuonsPlusMuonsMinusHistos')  .Get('DileptonMass')
-    this_emu  = f.Get(cut + 'MuonsElectronsOppSignHistos').Get('DileptonMass')
+    h = f.Get(cut + 'MuonsPlusMuonsMinusHistos').Get('DileptonMass')
+    sample.mumu = h.Rebin(len(bins)-1, '%s_mumu' % sample.name, bins)
+    h = f.Get(cut + 'MuonsElectronsOppSignHistos').Get('DileptonMass')
+    sample.emu = h.Rebin(len(bins)-1, '%s_emu' % sample.name, bins)
 
-    for h,n in [(this_mumu, 'this_mumu'), (this_emu, 'this_emu')]:
-        z = h.Rebin(len(bins)-1, n, bins)
-        exec '%s = z' % n
+    sample.weight = sample.mumu.Clone('%s_weight' % sample.name)
+    sample.weight.SetDirectory(0)
+    sample.weight.Scale(sample.partial_weight)
+    
+    total_mumu.Add(sample.mumu, sample.partial_weight)
+    total_emu .Add(sample.emu,  sample.partial_weight)
 
-    mumu.Add(this_mumu, sample.partial_weight)
-    emu .Add(this_emu,  sample.partial_weight)
+    draw(sample)
 
-    draw(sample.name, this_mumu, this_emu)
+#draw('total', mumu, emu, simple_div=True)
 
-draw('total', mumu, emu)
+lg = ROOT.TLegend(0.62, 0.13, 0.81, 0.33)
+
+# Normalize by the sum of the weights.
+sum_weights = samples[0].weight.Clone('sum_weights')
+for sample in samples[1:]:
+    sum_weights.Add(sample.weight)
+first = True
+for sample in samples:
+    sample.weight.Divide(sum_weights)
+    sample.weight.SetTitle(';dilepton mass (GeV);relative weight in bin')
+    sample.weight.SetStats(0)
+    sample.weight.SetLineWidth(2)
+    sample.weight.SetLineColor(sample.color)
+    sample.weight.GetYaxis().SetLabelSize(0.025)
+    sample.weight.GetYaxis().SetRangeUser(1e-3,1)
+    for i in xrange(1, sample.weight.GetNbinsX()+1):
+        sample.weight.SetBinError(i, 0)
+    sample.weight.Draw('hist text00' if first else 'hist text00 same')
+    first = False
+    lg.AddEntry(sample.weight, sample.nice_name, 'LE')
+lg.Draw()
+ps.save('weights')
+        
+first = True
+for sample in samples:
+    g = sample.div
+    g.SetLineWidth(2)
+    g.SetLineColor(sample.color)
+    g.Draw('AP' if first else 'P same')
+    first = False
+
+    n = g.GetN()
+    x,y = ROOT.Double(), ROOT.Double()
+    print sample.name
+    for i in xrange(n):
+        g.GetPoint(i, x, y)
+        exl = g.GetErrorXlow(i)
+        exh = g.GetErrorXhigh(i)
+        eyl = g.GetErrorYlow(i)
+        eyh = g.GetErrorYhigh(i)
+        print '%20s%10.4f%20s' % ('%.f-%.f' % (x-exl, x+exh), y, '[%5.4f, %5.4f]' % (y-eyl, y+eyh))
+    print
+
+lg.Draw()
+ps.save('all')
+

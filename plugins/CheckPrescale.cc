391576e89e74141e1934de3f8b0d381d6f769f7d
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-07-04 18:44:09 +0000

    Check L1 prescale explicitly; when checking, if no version of the specified trigger path was found, throw; option to dump prescales into a TTree for every lumisection, and accompanying flags to turn off throwing/warning.

--- plugins/CheckPrescale.cc
+++ plugins/CheckPrescale.cc
@@ -1,7 +1,10 @@
 #include <boost/foreach.hpp>
+#include "TTree.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
 #include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
 
 class CheckPrescale : public edm::EDAnalyzer {
@@ -15,12 +18,31 @@ private:
   const std::string hlt_process_name;
   const std::vector<std::string> trigger_paths;
   HLTConfigProvider hlt_cfg;
+  const bool dump_prescales;
+  const bool throw_on_prescale;
+
+  struct tree_t {
+    unsigned run;
+    unsigned lumi;
+    int l1;
+    int hlt;
+  };
+
+  tree_t last_t, t;
+  TTree* tree;
 };
 
 CheckPrescale::CheckPrescale(const edm::ParameterSet& cfg)
   : hlt_process_name(cfg.getParameter<std::string>("hlt_process_name")),
-    trigger_paths(cfg.getParameter<std::vector<std::string> >("trigger_paths"))
+    trigger_paths(cfg.getParameter<std::vector<std::string> >("trigger_paths")),
+    dump_prescales(cfg.getUntrackedParameter<bool>("dump_prescales", false)),
+    throw_on_prescale(cfg.getUntrackedParameter<bool>("throw_on_prescale", !dump_prescales))
 {
+  if (dump_prescales) {
+    edm::Service<TFileService> fs;
+    tree = fs->make<TTree>("t", "");
+    tree->Branch("tt", &t, "run/i:lumi:l1/I:hlt");
+  }
 }
 
 void CheckPrescale::beginRun(const edm::Run& run, const edm::EventSetup& setup) {
@@ -51,10 +73,35 @@ void CheckPrescale::analyze(const edm::Event& event, const edm::EventSetup& setu
     else
       continue;
 
-    int prescale = hlt_cfg.prescaleValue(event, setup, *trigger_path);
-    if (prescale != 1)
-      throw cms::Exception("CheckPrescale") << "trigger path " << *trigger_path << " has prescale != 1! prescale = " << prescale << "\n";
+    std::pair<int, int> prescales = hlt_cfg.prescaleValues(event, setup, *trigger_path);
+    if (prescales.first != 1 || prescales.second != 1) {
+      std::ostringstream out;
+      out << "trigger path " << *trigger_path << " has prescale != 1! L1 prescale = " << prescales.first << "  HLT prescale: " << prescales.second << "\n";
+      if (throw_on_prescale)
+        throw cms::Exception("CheckPrescale") << out.str();
+      else
+        edm::LogWarning("CheckPrescale") << out.str();
+    }
+
+    if (dump_prescales) {
+      memset(&t, 0, sizeof(tree_t));
+
+      t.run  = event.id().run();
+      t.lumi = event.luminosityBlock();
+      t.l1   = prescales.first;
+      t.hlt  = prescales.second;
+
+      if (t.run == last_t.run && t.lumi == last_t.lumi)
+        assert(t.l1 == last_t.l1 && t.hlt == last_t.hlt);
+      else
+        tree->Fill();
+      
+      last_t = t;
+    }
   }
+
+  if (!found)
+    throw cms::Exception("CheckPrescale") << "none of the trigger paths specified were found!\n";
 }
 
 DEFINE_FWK_MODULE(CheckPrescale);

e3e62cafd1319e904796fa7f83fd9135a52632a4
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-07-06 15:23:42 +0000

    Script to make a micro-ntuple just for events passing our selection, extracting only the variables used in the limit setting/bump search.

---
+++ test/DataMCSpectraComparison/micro_ntuple.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from SUSYBSMAnalysis.Zprime2muAnalysis.roottools import ROOT
+
+path = 'ana_datamc_Run2011AMuonsOnly/ana_datamc_data.root'
+tmp_fn = 'micro_ntuple.temp.txt'
+branch_spec = 'dil_mass'
+cut = 'OurSelNew'
+
+f = ROOT.TFile(path)
+t = f.SimpleNtupler.Get('t')
+t.GetPlayer().SetScanRedirect(True)
+t.GetPlayer().SetScanFileName(tmp_fn)
+t.Scan(branch_spec, cut)
+t.GetPlayer().SetScanRedirect(False)
+f.Close()
+
+lines = [line.split(' *')[1:] for line in open(tmp_fn).readlines() if ' * ' in line and 'Row' not in line]
+lines = ['\t'.join(y.strip() for y in x) for x in lines]
+open(tmp_fn, 'wt').write('\n'.join(lines))
+
+f = ROOT.TFile(path.replace('.root', '.microtuple.root'), 'CREATE')
+t = ROOT.TTree('t','')
+t.ReadFile(tmp_fn, branch_spec)
+f.Write()
+f.Close()
+
+os.remove(tmp_fn)

b98f5bd688d25c028bcd4c108d98b4169d813ad5
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-07-06 13:17:04 +0000

    Increase events_per_job; create_only flag.

--- test/DataMCSpectraComparison/tuple_mc.py
+++ test/DataMCSpectraComparison/tuple_mc.py
@@ -15,10 +15,11 @@ process.GlobalTag.globaltag = 'START42_V11::All'
 if __name__ == '__main__' and 'submit' in sys.argv:
     job_control = '''
 total_number_of_events = -1
-events_per_job = 100000
+events_per_job = 150000
 '''
 
     just_testing = 'testing' in sys.argv
+    create_only = 'create_only' in sys.argv
     from samples import samples
     for sample in samples:
         if 'Summer11' not in sample.dataset:
@@ -34,5 +35,8 @@ events_per_job = 100000
         sample.job_control = job_control % sample.__dict__
         open('crab.cfg', 'wt').write(crab_cfg % sample.__dict__)
         if not just_testing:
-            os.system('crab -create -submit all')
+            if create_only:
+                os.system('crab -create')
+            else:
+                os.system('crab -create -submit all')
             os.system('rm crab.cfg')

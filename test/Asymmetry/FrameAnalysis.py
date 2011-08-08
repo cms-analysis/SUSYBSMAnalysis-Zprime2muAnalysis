79fde73a47472c53db49157b4c8449c8d5320d58
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-08-08 06:50:01 +0000

    hltFilter gone -- rely on selection to implement trigger in MC.

--- test/Asymmetry/FrameAnalysis.py
+++ test/Asymmetry/FrameAnalysis.py
@@ -11,4 +11,4 @@ process.AsymmetryFrameAnalysis = cms.EDAnalyzer('AsymmetryFrameAnalysis',
                                                 debug = cms.untracked.bool(True),
                                                 )
 
-process.p = cms.Path(process.hltFilter * process.Zprime2muAnalysisSequence * process.AsymmetryFrameAnalysis)
+process.p = cms.Path(process.Zprime2muAnalysisSequence * process.AsymmetryFrameAnalysis)

33b1a704aeaefe57720091141a4747ab59e0b9a3
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-08-08 11:12:32 +0000

    Remove ana_dataset property (always specify manually the ana_datasets); full Summer11 use; redoing tuples from scratch; default mode is now condor (only Z/DY samples do not exist at FNAL.

--- test/DataMCSpectraComparison/samples.py
+++ test/DataMCSpectraComparison/samples.py
@@ -5,7 +5,7 @@ from SUSYBSMAnalysis.Zprime2muAnalysis.tools import files_from_dbs
 from SUSYBSMAnalysis.Zprime2muAnalysis.crabtools import dataset_from_publish_log
 
 class sample:
-    def __init__(self, name, nice_name, dataset, nevents, color, syst_frac, cross_section, k_factor=1, filenames=None, scheduler='glite', hlt_process_name='HLT', ana_dataset=None):
+    def __init__(self, name, nice_name, dataset, nevents, color, syst_frac, cross_section, k_factor=1, filenames=None, scheduler='condor', hlt_process_name='HLT', ana_dataset=None):
         self.name = name
         self.nice_name = nice_name
         self.dataset = dataset
@@ -17,18 +17,11 @@ class sample:
         self.filenames_ = filenames
         self.scheduler = scheduler
         self.hlt_process_name = hlt_process_name
-        self.ana_dataset_ = ana_dataset
+        self.ana_dataset = ana_dataset
 
     @property
     def partial_weight(self):
         return self.cross_section / float(self.nevents) * self.k_factor # the total weight is partial_weight * integrated_luminosity
-
-    @property
-    def ana_dataset(self):
-        if self.ana_dataset_ is not None:
-            return self.ana_dataset_
-        self.ana_dataset_ = ds = dataset_from_publish_log('crab/publish_logs/publish.crab_datamc_%s' % self.name)
-        return ds
 
     @property
     def filenames(self):
@@ -48,72 +41,39 @@ class sample:
             print fn
             if redump_existing or not os.path.isfile(os.path.join(dst, os.path.basename(fn))):
                 os.system('dccp ~%s %s/' % (fn,dst))
-            
+
 # https://twiki.cern.ch/twiki/bin/view/CMS/CrossSections_3XSeries for
-# xsecs (all below in pb)
+# xsecs (all below in pb), except for t(bar)W from
+# https://twiki.cern.ch/twiki/bin/view/CMS/SingleTopSigma
 samples = [
-    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM',                   2148325, 432, 0.05, 1631 - 7.9*1.3),
-    sample('dy120',        'DY120',                                              '/DYToMuMu_M-120_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             54550, 433, 0.05, 7.9, k_factor=1.3),
-    sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 433, 0.05, 0.97  - 0.027,  k_factor=1.3),
-    sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 434, 0.05, 0.027 - 0.0031, k_factor=1.3),
-    sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 435, 0.05, 0.0031,         k_factor=1.3),
-    sample('dy1000',       'DY1000',                                             '/DYToMuMu_M-1000_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',            55000, 435, 0.05, 9.7e-4,         k_factor=1.3),
+    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM',                   2148325, 432, 0.05, 1631 - 7.9*1.3), # The subtraction here and below is because we drop the events with generated M in ranges in the other datasets, e.g. drop here M > 120.
+    sample('dy120',        'DY120',                                              '/DYToMuMu_M-120_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             54550, 433, 0.05, 7.9    - 0.97,   k_factor=1.3),
+    sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 433, 0.05, 0.97   - 0.027,  k_factor=1.3),
+    sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 434, 0.05, 0.027  - 0.0031, k_factor=1.3),
+    sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 435, 0.05, 0.0031 - 9.7e-4, k_factor=1.3),
+    sample('dy1000',       'DY1000',                                             '/DYToMuMu_M-1000_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',            55000, 435, 0.05, 9.7e-4,          k_factor=1.3),
     sample('ttbar',        't#bar{t}',                                           '/TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                  3701947,   2, 0.15, 157),
-    sample('singletop_tW', 'tW',                                                 '/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/Spring11-PU_S1_START311_V1G1-v1/AODSIM',            489417,   1, 0.075, 10.6),
+    sample('tW',           'tW',                                                 '/T_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',            814390,   1, 0.03,   7.9),
+    sample('tbarW',        'tbarW',                                              '/Tbar_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',         809984,   1, 0.03,   7.9),
     sample('ww',           'WW',                                                 '/WW_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                       4225916,   4, 0.035, 43),
-    sample('wz',           'WZ',                                                 '/WZ_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                       4265243,  30, 0.038, 18, scheduler='condor'),
-    sample('zz',           'ZZ',                                                 '/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           2108608,   6, 0.025,  5.9),
+    sample('wz',           'WZ',                                                 '/WZ_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                       4265243,  30, 0.038, 18),
+    sample('zz',           'ZZ',                                                 '/ZZ_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                       4187885,   6, 0.025,  5.9),
     sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',          2032536,  46, 0.05, 1631),
-    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Spring11-PU_S1_START311_V1G1-v1/AODSIM',           15110974,   3, 0.05,  3.1e4, scheduler='condor'),
-#    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',             56789563,   3, 0.05, 3.1e4, scheduler='condor'),
+    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',             81352581,   3, 0.05, 3.1e4),
     sample('inclmu15',     'QCD (MuRich, muon p_{T} > 15 GeV)',                  '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',      20416038, 801, 0.1,   2.855e-4 * 2.966e8),
     sample('zssm1000',     'Z\'_{SSM} (1 TeV) #rightarrow #mu^{+}#mu^{-}',       '/ZprimeSSMToMuMu_M-1000_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',            20328,  38, 0.05,  0.089, k_factor=1.3),
 ]
 
-
-for sample in samples:
-    if 'Spring11' in sample.dataset:
-        sample.hlt_process_name = 'REDIGI311X'
-        
 samples.reverse()
 
 for sample in samples:
     exec '%s = sample' % sample.name
 
-use_predefined_datasets = True
-if use_predefined_datasets:
-    wjets.ana_dataset_ = '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/tucker-datamc_wjets-396bcdaa1e090647f7fc37d15e445b1a/USER'
-    zz.ana_dataset_ = '/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/tucker-datamc_zz-396bcdaa1e090647f7fc37d15e445b1a/USER'
-    singletop_tW.ana_dataset_ = '/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/tucker-datamc_singletop_tW-396bcdaa1e090647f7fc37d15e445b1a/USER'
-
-    zssm1000.ana_dataset_ = '/ZprimeSSMToMuMu_M-1000_TuneZ2_7TeV-pythia6/tucker-datamc_zssm1000-5222c20b53e3c47b6c8353d464ee954c/USER'
-    inclmu15.ana_dataset_ = '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/tucker-datamc_inclmu15-5222c20b53e3c47b6c8353d464ee954c/USER'
-    ztautau.ana_dataset_ = '/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/tucker-datamc_ztautau-5222c20b53e3c47b6c8353d464ee954c/USER'
-    wz.ana_dataset_ = '/WZ_TuneZ2_7TeV_pythia6_tauola/tucker-datamc_wz-883ffc4067260a06d7fb8a4a77726490/USER'
-    ww.ana_dataset_ = '/WW_TuneZ2_7TeV_pythia6_tauola/tucker-datamc_ww-883ffc4067260a06d7fb8a4a77726490/USER'
-    ttbar.ana_dataset_ = '/TTJets_TuneZ2_7TeV-madgraph-tauola/tucker-datamc_ttbar-883ffc4067260a06d7fb8a4a77726490/USER'
-    dy1000.ana_dataset_ = '/DYToMuMu_M-1000_TuneZ2_7TeV-pythia6-tauola/tucker-datamc_dy1000-5222c20b53e3c47b6c8353d464ee954c/USER'
-    dy800.ana_dataset_ = '/DYToMuMu_M-800_TuneZ2_7TeV-pythia6-tauola/tucker-datamc_dy800-5222c20b53e3c47b6c8353d464ee954c/USER'
-    dy500.ana_dataset_ = '/DYToMuMu_M-500_TuneZ2_7TeV-pythia6-tauola/tucker-datamc_dy500-5222c20b53e3c47b6c8353d464ee954c/USER'
-    dy200.ana_dataset_ = '/DYToMuMu_M-200_TuneZ2_7TeV-pythia6-tauola/tucker-datamc_dy200-5222c20b53e3c47b6c8353d464ee954c/USER'
-    dy120.ana_dataset_ = '/DYToMuMu_M-120_TuneZ2_7TeV-pythia6-tauola/tucker-datamc_dy120-5222c20b53e3c47b6c8353d464ee954c/USER'
-    zmumu.ana_dataset_ = '/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/tucker-datamc_zmumu-5222c20b53e3c47b6c8353d464ee954c/USER'
-
-    # Sanity check.
-    for sample in samples:
-        d,a = sample.dataset.split('/')[1], sample.ana_dataset.split('/')[1]
-        if d != a:
-            raise ValueError(d + ' != ' + a)
-            
+    if sample.name == 'zmumu' or sample.name.startswith('dy'):
+        sample.scheduler = 'glite'
 
 from SUSYBSMAnalysis.Zprime2muAnalysis.tools import big_warn
-big_warn("subtracting 50kevt from wz since two jobs are stuck")
-wz.nevents -= 50000
-big_warn("subtracting (4*1e5 + 23420)evt from ttbar since five jobs are stuck")
-ttbar.nevents -= 423420
-
-print 'samples are:'
-print ' '.join(s.name for s in samples)
+big_warn("it's gettin' real in the whole foods parking lot")
 
 if __name__ == '__main__':
     if False:

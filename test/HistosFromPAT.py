6035b948c53b43e8a675a1b918a50e4719af7406
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-08-08 06:49:13 +0000

    Not used.

--- test/HistosFromPAT.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import FWCore.ParameterSet.Config as cms
-
-from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muAnalysis_cfg import process
-
-process.load('SUSYBSMAnalysis.Zprime2muAnalysis.HistosFromPAT_cfi')
-process.p = cms.Path(process.hltFilter * process.Zprime2muAnalysisSequence * process.HistosFromPAT)

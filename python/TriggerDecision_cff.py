73d4eebdf427fff67c82c95932678b7b35346703
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-08-08 06:47:09 +0000

    Comment warning against changing the path.

--- python/TriggerDecision_cff.py
+++ python/TriggerDecision_cff.py
@@ -4,6 +4,13 @@ triggerDecision = cms.PSet(doingElectrons = cms.bool(False),
                            useTrigger = cms.bool(True),
                            l1GtObjectMap = cms.InputTag('hltL1GtObjectMap'),
                            hltResults = cms.InputTag('TriggerResults', '', 'HLT'),
+                           # Note: these next two paths should not be
+                           # changed just because the trigger menu for
+                           # data-taking changes. This module is meant
+                           # for MC studies, so if the pT threshold to
+                           # be studied is e.g. 40 GeV, that has to be
+                           # taken care of in code as there is no such
+                           # path in the Spring11/Summer11 MC.
                            l1Paths = cms.vstring('L1_SingleMu12'),
                            hltPaths = cms.vstring('HLT_Mu30_v1')
                            )

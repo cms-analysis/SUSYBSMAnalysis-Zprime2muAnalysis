875a6486d5af9035409f1448fe8cbb4805e07a98
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2011-08-08 06:38:24 +0000

    Module for prescaling a set (e.g. HLT_Mu15_v*) of paths by a common value.

---
+++ python/PrescaleToCommon_cff.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+RandomNumberGeneratorService = cms.Service('RandomNumberGeneratorService',
+                                           PrescaleToCommon = cms.PSet(initialSeed = cms.untracked.uint32(1219))
+                                           )
+
+PrescaleToCommon = cms.EDFilter('PrescaleToCommon',
+                                hlt_process_name = cms.string('HLT'),
+                                trigger_paths = cms.vstring(),
+                                overall_prescale = cms.int32(1),
+                                assume_simulation_has_prescale_1 = cms.bool(True) # Current PAT tuples of MC samples don't have both L1 branches :-(
+                                )

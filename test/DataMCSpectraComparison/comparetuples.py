e95447956290f8d295699ed12dc73bc0f4d35be8
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2012-01-06 06:46:46 +0000

    use_jsons flag (useful to disable for MC comparison).

--- test/DataMCSpectraComparison/comparetuples.py
+++ test/DataMCSpectraComparison/comparetuples.py
@@ -18,12 +18,22 @@ def m2s(l):
         l = [l]
     return ' * '.join('%.1f' % m for m in l)
 
-def doit2(skip_understood_events=False, max_mass_diff=0.1):
-    old = doit('data/Run2011MuonsOnly/ana_datamc_data.root', 'dil_mass', 'SimpleNtupler', 't', 'OurSelNew')
-    new = doit('m10t_ntuple_SingleMuRun2011.root',           'mass',     'OppSign',       't', '')
 
-    old_json = LumiList('data/Run2011MuonsOnly/ana_datamc_data.forlumi.json')
-    new_json = LumiList('m10t_ntuple_SingleMuRun2011.report.json')
+def doit2(skip_understood_events=False, max_mass_diff=0.1, use_jsons=True):
+    old = doit('data/Run2011MuonsOnly/ana_datamc_data.root', 'dil_mass', 'SimpleNtupler', 't', 'OurSelNewNoSign && SameSign')
+    new = doit('m10t/ntuple_SingleMuRun2011.root',           'mass',     'SameSign',       't', '')
+    #old = doit('mc/ana_datamc_ttbar.root',  'dil_mass', 'SimpleNtupler', 't', 'OurSelNewNoSign && SameSign')
+    #new = doit('m10t/ntuple_MC_ttbar.root', 'mass',     'SameSign',       't', '')
+    #use_jsons = False
+
+    if use_jsons:
+        old_json = LumiList('data/Run2011MuonsOnly/ana_datamc_data.forlumi.json')
+        new_json = LumiList('m10t/ntuple_SingleMuRun2011.report.json')
+    else:
+        class dummy:
+            def contains(*args):
+                return 'N/A'
+        old_json = new_json = dummy()
 
     oldk = set(old.keys())
     newk = set(new.keys())

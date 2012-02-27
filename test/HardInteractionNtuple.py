8af11cdcd3622e5f539968e385ad64715459d6e4
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2012-02-27 06:58:26 +0000

    muon/picky_src in the ntupler.

--- test/HardInteractionNtuple.py
+++ test/HardInteractionNtuple.py
@@ -3,10 +3,10 @@
 import sys, os, FWCore.ParameterSet.Config as cms
 
 from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muAnalysis_cfg import process
-process.source.fileNames = ['file:/uscms/home/tucker/scratch/zmumu.root']
+process.source.fileNames = ['/store/mc/Summer11/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/AODSIM/PU_S3_START42_V11-v2/0000/24D6F0D2-E77C-E011-8C27-003048D43944.root']
 
 from SUSYBSMAnalysis.Zprime2muAnalysis.HardInteraction_cff import hardInteraction
-process.HardInteractionNtuple = cms.EDAnalyzer('HardInteractionNtuple', hardInteraction = hardInteraction)
+process.HardInteractionNtuple = cms.EDAnalyzer('HardInteractionNtuple', hardInteraction = hardInteraction, muon_src = cms.InputTag('muons'), picky_src = cms.InputTag('tevMuons', 'picky'))
 process.HardInteractionNtuple.hardInteraction.src = 'genParticles'
 process.p = cms.Path(process.HardInteractionNtuple)
 
@@ -20,25 +20,23 @@ scheduler = %(scheduler)s
 datasetpath = %(dataset)s
 pset = HardInteractionNtuple.py
 total_number_of_events = -1
-events_per_job = 50000
+events_per_job = 100000
 
 [USER]
-ui_working_dir = crab/crab_genntuple_%(name)s
+ui_working_dir = crab/crab_hintuple_%(name)s
 return_data = 1
 '''
 
+    scheduler = 'condor'
     samples = [
-        ('pythia36',     '/Zmumu/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO'),
-        ('powheg36',     '/DYToMuMu_M-20_7TeV-powheg-pythia6/Spring10-START3X_V26-v1/GEN-SIM-RECO'),
-        ('pythia',       '/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Fall10-START38_V12-v1/GEN-SIM-RECO'),
-        ('powheg',       '/DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia/Fall10-START38_V12-v1/GEN-SIM-RECO'),
-        ('powheg36ct66', '/Zmumu_M20_CTEQ66-powheg/Summer10-START36_V9_S09-v2/GEN-SIM-RECO'),
+        ('zmumu', '/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM'),
+        ('dy120', '/DYToMuMu_M-120_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM'),
+        ('dy200', '/DYToMuMu_M-200_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM'),
         ]
 
     just_testing = 'testing' in sys.argv
 
     for name, dataset in samples:
-        scheduler = 'glite' if name == 'pythia36' else 'condor'
         open('crab.cfg', 'wt').write(crab_cfg % locals())
         if not just_testing:
             os.system('crab -create -submit all')

b793794aa1d3aa24cfaffd9beb7e76daee97a627
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2012-04-18 18:21:18 +0000

    Paths for 2012.

--- python/hltTriggerMatch_cfi.py
+++ python/hltTriggerMatch_cfi.py
@@ -14,11 +14,11 @@ muonTriggerMatchHLTMuons = cms.EDProducer('PATTriggerMatcherDRDPtLessByR',
 
 trigger_pt_threshold = 40
 offline_pt_threshold = 45
-trigger_paths = ['HLT_Mu40_eta2p1_v%i' % i for i in (1,4,5)] + ['HLT_Mu40_v%i' % i for i in (1,2,3,5)] + ['HLT_Mu30_v1', 'HLT_Mu30_v2']
+trigger_paths = ['HLT_Mu40_eta2p1_v%i' % i for i in (9,)]
 trigger_match = 'userFloat("TriggerMatchPt") > %(trigger_pt_threshold)i && abs(userFloat("TriggerMatchEta")) < 2.1' % locals()
 
-overall_prescale = 2000
+overall_prescale = 600
 prescaled_trigger_pt_threshold = 15
 prescaled_offline_pt_threshold = 20
-prescaled_trigger_paths = ['HLT_Mu15_v%i' % i for i in (2,3,4,5,6,8,9,12,13)]
+prescaled_trigger_paths = ['HLT_Mu15_eta2p1_v%i' % i for i in (3,)]
 prescaled_trigger_match = trigger_match.replace('Trigger', 'prescaledTrigger')

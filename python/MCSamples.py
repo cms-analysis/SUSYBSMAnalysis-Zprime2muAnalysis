174aba617e3b8b3df22655d797bac26c1d9fd98c
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2012-04-19 00:04:54 +0000

    Start of Summer12 samples.

--- python/MCSamples.py
+++ python/MCSamples.py
@@ -47,38 +47,33 @@ class tupleonlysample(sample):
     def __init__(self, name, dataset, scheduler='condor', hlt_process_name='HLT'):
         super(tupleonlysample, self).__init__(name, 'dummy', dataset, 1, 1, 1, 1, scheduler=scheduler, hlt_process_name=hlt_process_name)
 
-# https://twiki.cern.ch/twiki/bin/view/CMS/CrossSections_3XSeries for
-# xsecs (all below in pb), except for t(bar)W from
-# https://twiki.cern.ch/twiki/bin/view/CMS/SingleTopSigma
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat8TeV for xsecs (all below in pb)
+# commented out samples are not available for Summer12/8 TeV
 samples = [
-    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/Summer11-PU_S3_START42_V11-v2/AODSIM',                   2148325, 432, 0.05, 1631 - 7.9*1.3), # The subtraction here and below is because we drop the events with generated M in ranges in the other datasets, e.g. drop here M > 120.
-    sample('dy120',        'DY120',                                              '/DYToMuMu_M-120_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             54550, 433, 0.05, 7.9    - 0.97,   k_factor=1.3),
-    sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 433, 0.05, 0.97   - 0.027,  k_factor=1.3),
-    sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 434, 0.05, 0.027  - 0.0031, k_factor=1.3),
-    sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 435, 0.05, 0.0031 - 9.7e-4, k_factor=1.3),
-    sample('dy1000',       'DY1000',                                             '/DYToMuMu_M-1000_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',            55000, 435, 0.05, 9.7e-4,          k_factor=1.3),
-    sample('ttbar',        't#bar{t}',                                           '/TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                  3701947,   2, 0.15, 157),
-    sample('tW',           'tW',                                                 '/T_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',            814390,   1, 0.03,   7.9),
-    sample('tbarW',        'tbarW',                                              '/Tbar_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',         809984,   1, 0.03,   7.9),
-    sample('ww',           'WW',                                                 '/WW_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                       4225916,   4, 0.035, 43),
-    sample('wz',           'WZ',                                                 '/WZ_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                       4265243,  30, 0.038, 18),
-    sample('zz',           'ZZ',                                                 '/ZZ_TuneZ2_7TeV_pythia6_tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',                       4187885,   6, 0.025,  5.9),
-    sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',          2032536,  46, 0.05, 1631),
-    sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',             81352581,   3, 0.05, 3.1e4),
-    sample('inclmu15',     'QCD',                                                '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',      25080241, 801, 0.1,   2.855e-4 * 2.966e8),
-    sample('zssm1000',     'Z\'_{SSM} (1 TeV) #rightarrow #mu^{+}#mu^{-}',       '/ZprimeSSMToMuMu_M-1000_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',            20328,  38, 0.05,  0.089, k_factor=1.3, is_zprime=True),
-#    sample('zpsi1000',     'Z\'_{#psi} (1 TeV) #rightarrow #mu^{+}#mu^{-}',      '/ZprimePSIToMuMu_M-1000_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',            21020,  48, 0.05,  0.089, k_factor=1.3, is_zprime=True),
+    sample('zmumu',        '#gamma/Z #rightarrow #mu^{+}#mu^{-}',                '/DYToMuMu_M_20_TuneZ2star_8TeV_pythia6/Summer12-PU_S7_START50_V15-v1/AODSIM',               1963296, 432, 0.05, 1915 - 0), # The subtraction here and below is because we drop the events with generated M in ranges in the other datasets, e.g. drop here M > 120.
+#   sample('dy120',        'DY120',                                              '/DYToMuMu_M-120_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             54550, 433, 0.05, 7.9    - 0.97,   k_factor=1.3),
+#   sample('dy200',        'DY200',                                              '/DYToMuMu_M-200_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 433, 0.05, 0.97   - 0.027,  k_factor=1.3),
+#   sample('dy500',        'DY500',                                              '/DYToMuMu_M-500_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 434, 0.05, 0.027  - 0.0031, k_factor=1.3),
+#   sample('dy800',        'DY800',                                              '/DYToMuMu_M-800_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',             55000, 435, 0.05, 0.0031 - 9.7e-4, k_factor=1.3),
+#   sample('dy1000',       'DY1000',                                             '/DYToMuMu_M-1000_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',            55000, 435, 0.05, 9.7e-4,          k_factor=1.3),
+    sample('ttbar',        't#bar{t}',                                           '/TTJets_TuneZ2star_8TeV-madgraph-tauola/Summer12-PU_S7_START52_V5-v1/AODSIM',               1203373,   2, 0.15, 225),
+#   sample('tW',           'tW',                                                 '/T_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',            814390,   1, 0.03,   7.9),
+#   sample('tbarW',        'tbarW',                                              '/Tbar_TuneZ2_tW-channel-DR_7TeV-powheg-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',         809984,   1, 0.03,   7.9),
+    sample('ww',           'WW',                                                 '/WW_TuneZ2star_8TeV_pythia6_tauola/Summer12-PU_S7_START50_V15-v1/AODSIM',                  10000431,   4, 0.035, 57),
+    sample('wz',           'WZ',                                                 '/WZ_TuneZ2star_8TeV_pythia6_tauola/Summer12-PU_S7_START50_V15-v1/AODSIM',                  10000283,  30, 0.038, 32),
+    sample('zz',           'ZZ',                                                 '/ZZ_TuneZ2star_8TeV_pythia6_tauola/Summer12-PU_S7_START50_V15-v1/AODSIM',                   9799908,   6, 0.025,  8),
+#   sample('ztautau',      'Z #rightarrow #tau^{+}#tau^{-}',                     '/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/Summer11-PU_S3_START42_V11-v2/AODSIM',          2032536,  46, 0.05, 1631),
+#   sample('wjets',        'W+jets',                                             '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM',             81352581,   3, 0.05, 3.1e4),
+#   sample('inclmu15',     'QCD',                                                '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',      25080241, 801, 0.1,   2.855e-4 * 2.966e8),
+#   sample('zssm1000',     'Z\'_{SSM} (1 TeV) #rightarrow #mu^{+}#mu^{-}',       '/ZprimeSSMToMuMu_M-1000_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',            20328,  38, 0.05,  0.089, k_factor=1.3, is_zprime=True),
+#   sample('zpsi1000',     'Z\'_{#psi} (1 TeV) #rightarrow #mu^{+}#mu^{-}',      '/ZprimePSIToMuMu_M-1000_TuneZ2_7TeV-pythia6/Summer11-PU_S4_START42_V11-v1/AODSIM',            21020,  48, 0.05,  0.089, k_factor=1.3, is_zprime=True),
 ]
 
 samples.reverse()
 
 for sample in samples:
     exec '%s = sample' % sample.name
-    sample.ana_dataset = '/%s/tucker-datamc_%s-e5275934e6f4238b636d1bf2848643b3/USER' % (sample.dataset.split('/')[1], sample.name)
-
-ttbar.ana_dataset = '/TTJets_TuneZ2_7TeV-madgraph-tauola/tucker-datamc_ttbar-a972f07199dd1bd57caa708c2dcf050c/USER'
-wjets.ana_dataset = '/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/tucker-merge_20110829085858_wjets-a3691da421b8c16b08067510400469a1/USER'
-inclmu15.ana_dataset = '/QCD_Pt-20_MuEnrichedPt-15_TuneZ2_7TeV-pythia6/tucker-merge_20110829085858_inclmu15-a3691da421b8c16b08067510400469a1/USER'
+    sample.ana_dataset = '/%s/tucker-datamc_%s-NONEYET/USER' % (sample.dataset.split('/')[1], sample.name)
 
 #big_warn('nothing')
 

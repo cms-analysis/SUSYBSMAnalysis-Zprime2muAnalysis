83dca155cf95769aebb51cbab67cb0aac7d8484f
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2012-04-20 16:54:21 +0000

    Path names (relevant to CheckPrescale downstream).

--- python/hltTriggerMatch_cfi.py
+++ python/hltTriggerMatch_cfi.py
@@ -14,11 +14,11 @@ muonTriggerMatchHLTMuons = cms.EDProducer('PATTriggerMatcherDRDPtLessByR',
 
 trigger_pt_threshold = 40
 offline_pt_threshold = 45
-trigger_paths = ['HLT_Mu40_eta2p1_v%i' % i for i in (9,)]
+trigger_paths = ['HLT_Mu40_eta2p1_v%i' % i for i in (6,9)]
 trigger_match = 'userFloat("TriggerMatchPt") > %(trigger_pt_threshold)i && abs(userFloat("TriggerMatchEta")) < 2.1' % locals()
 
 overall_prescale = 600
 prescaled_trigger_pt_threshold = 15
 prescaled_offline_pt_threshold = 20
-prescaled_trigger_paths = ['HLT_Mu15_eta2p1_v%i' % i for i in (3,)]
+prescaled_trigger_paths = ['HLT_Mu15_eta2p1_v3', 'HLT_Mu15_v14']
 prescaled_trigger_match = trigger_match.replace('Trigger', 'prescaledTrigger')

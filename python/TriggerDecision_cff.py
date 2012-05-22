e173e605b029929f14c8167b973fbdb45e7969d8
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2012-05-22 16:27:01 +0000

    Revert to 'HLT' tag for hltResults.

--- python/TriggerDecision_cff.py
+++ python/TriggerDecision_cff.py
@@ -3,8 +3,7 @@ import FWCore.ParameterSet.Config as cms
 triggerDecision = cms.PSet(doingElectrons = cms.bool(False),
                            useTrigger = cms.bool(True),
                            l1GtObjectMap = cms.InputTag('hltL1GtObjectMap'),
-#                           hltResults = cms.InputTag('TriggerResults', '', 'HLT'),
-                           hltResults = cms.InputTag('TriggerResults', '', 'PAT'),
+                           hltResults = cms.InputTag('TriggerResults', '', 'HLT'),
                            # Note: these next two paths should not be
                            # changed just because the trigger menu for
                            # data-taking changes. This module is meant

349c9ea668a40fc198b16830b8eb33fcd31d7e16
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2012-05-28 16:40:38 +0000

    Use JSON files of May 25th: runs 190456-194479.

--- python/goodlumis.py
+++ python/goodlumis.py
@@ -7,8 +7,8 @@ def for_cmssw(ll):
 # These run numbers guide the combination of the prompt and DCS-only
 # JSONs.
 first_run = 190456
-last_prompt_run = 193557
-last_run = 193700
+last_prompt_run = 194479
+last_run = 195200
 
 # Sometimes the same run-range json gets made in other versions.
 prompt_version = ''

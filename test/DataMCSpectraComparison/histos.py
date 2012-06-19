540ea42e2063354de359a758680654e0702c47a0
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2012-06-19 10:14:43 +0000

    Include runs 195521-195775 in one go; add runs 195776-195947 (disabled for ICHEP).

--- test/DataMCSpectraComparison/histos.py
+++ test/DataMCSpectraComparison/histos.py
@@ -259,11 +259,8 @@ return_data = 1
             ('SingleMuRun2012A_Prompt_190450_193686', '/SingleMu/slava-datamc_SingleMuRun2012A_Prompt_190450_193686_20120515044043-f1c162e9ac6a53572830a92e8730d513/USER'),
             ('SingleMuRun2012B_Prompt_193752_194500', '/SingleMu/slava-datamc_SingleMuRun2012B_Prompt_193752_194500_20120528075253-d6e8741107b6d0f570b2cfba8ae4760b/USER'),
             ('SingleMuRun2012B_Prompt_194501_195520', '/SingleMu/slava-datamc_SingleMuRun2012B_Prompt_194501_195520_20120611022947-d6e8741107b6d0f570b2cfba8ae4760b/USER'),
-            ('SingleMuRun2012B_Prompt_195501_195552', '/SingleMu/kypreos-datamc_SingleMuRun2012B_Prompt_195501_195552_20120613105052-7c597bb00be1176600a40617c385b74d/USER'),
-            ('SingleMuRun2012B_Prompt_195553_195635', '/SingleMu/kypreos-datamc_SingleMuRun2012B_Prompt_195553_195635_20120613105321-7c597bb00be1176600a40617c385b74d/USER'),
-            ('SingleMuRun2012B_Prompt_195636_195650', '/SingleMu/kypreos-datamc_SingleMuRun2012B_Prompt_195636_195650_20120613105529-7c597bb00be1176600a40617c385b74d/USER'),
-            ('SingleMuRun2012B_Prompt_195651_195660', '/SingleMu/kypreos-datamc_SingleMuRun2012B_Prompt_195651_195660_20120613105830-7c597bb00be1176600a40617c385b74d/USER'),
-            ('SingleMuRun2012B_Prompt_195661_195775', '/SingleMu/kypreos-datamc_SingleMuRun2012B_Prompt_195661_195775_20120613110006-7c597bb00be1176600a40617c385b74d/USER'),
+            ('SingleMuRun2012B_Prompt_195521_195775', '/SingleMu/slava-datamc_SingleMuRun2012B_Prompt_195521_195775_20120615114641-d6e8741107b6d0f570b2cfba8ae4760b/USER'),
+#            ('SingleMuRun2012B_Prompt_195776_195947', '/SingleMu/slava-datamc_SingleMuRun2012B_Prompt_195776_195947_20120617080738-d6e8741107b6d0f570b2cfba8ae4760b/USER'),
             ]
 
         lumi_lists = [

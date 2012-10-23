800c28cba2d8681d7a328472761fdf2ff4ad172d
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2012-10-23 14:47:48 +0000

    Update trigger paths.

--- python/hltTriggerMatch_cfi.py
+++ python/hltTriggerMatch_cfi.py
@@ -13,11 +13,12 @@ muonTriggerMatchHLTMuons = cms.EDProducer('PATTriggerMatcherDRLessByR',
 
 trigger_pt_threshold = 40
 offline_pt_threshold = 45
-trigger_paths = ['HLT_Mu40_eta2p1_v%i' % i for i in (6,9)]
+trigger_paths = ['HLT_Mu40_eta2p1_v%i' % i for i in (9,10,11)]
 trigger_match = 'userFloat("TriggerMatchPt") > %(trigger_pt_threshold)i && abs(userFloat("TriggerMatchEta")) < 2.1' % locals()
 
 overall_prescale = 250
 prescaled_trigger_pt_threshold = 24
 prescaled_offline_pt_threshold = 27
-prescaled_trigger_paths = ['HLT_Mu24_eta2p1_v3']
+prescaled_trigger_paths = ['HLT_Mu24_eta2p1_v%i' % i for i in (3,4,5)]
+print trigger_paths, prescaled_trigger_paths
 prescaled_trigger_match = trigger_match.replace('Trigger', 'prescaledTrigger').replace('%i' % trigger_pt_threshold, '%i' % prescaled_trigger_pt_threshold)

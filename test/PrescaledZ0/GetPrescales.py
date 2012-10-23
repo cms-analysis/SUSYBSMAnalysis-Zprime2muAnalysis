a27de47379e475f67c3aca7e539184b35fe1d360
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2012-10-23 16:43:02 +0000

    Update for 53X and 2012C and D periods.

--- test/PrescaledZ0/GetPrescales.py
+++ test/PrescaledZ0/GetPrescales.py
@@ -1,8 +1,8 @@
 import sys, os
 from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muAnalysis_cfg import cms, process
 
-process.source.fileNames = ['/store/data/Run2012A/SingleMu/AOD/PromptReco-v1/000/190/539/288ACFA2-BC81-E111-99E3-001D09F2527B.root']
-process.GlobalTag.globaltag = 'GR_R_52_V7::All'
+process.source.fileNames = ['/store/data/Run2012C/SingleMu/AOD/PromptReco-v2/000/202/272/94E42528-89F9-E111-BE44-BCAEC53296F8.root']
+process.GlobalTag.globaltag = 'GR_P_V42_AN2::All'
 process.maxEvents.input = 5000
 process.options.wantSummary = True
 process.MessageLogger.cerr.FwkReport.reportEvery = 100000
@@ -12,12 +12,13 @@ process.CheckPrescale.dump_prescales = True
 
 process.Mu17       = process.CheckPrescale.clone(trigger_paths=cms.vstring('HLT_Mu17_v3'))
 process.Mu15eta2p1 = process.CheckPrescale.clone(trigger_paths=cms.vstring('HLT_Mu15_eta2p1_v3', 'HLT_Mu15_eta2p1_v4'))
-process.Mu24eta2p1 = process.CheckPrescale.clone(trigger_paths=cms.vstring('HLT_Mu24_eta2p1_v3', 'HLT_Mu24_eta2p1_v4'))
- 
+process.Mu24eta2p1 = process.CheckPrescale.clone(trigger_paths=cms.vstring('HLT_Mu24_eta2p1_v3', 'HLT_Mu24_eta2p1_v4', 'HLT_Mu24_eta2p1_v5'))
+
 process.MessageLogger.suppressWarning = cms.untracked.vstring('Mu17', 'Mu15eta2p1', 'Mu24eta2p1')
 
-process.p = cms.Path(process.Mu17 * process.Mu15eta2p1 * process.Mu24eta2p1)
-    
+#process.p = cms.Path(process.Mu17 * process.Mu15eta2p1 * process.Mu24eta2p1)
+process.p = cms.Path(process.Mu24eta2p1)
+
 if __name__ == '__main__' and 'submit' in sys.argv:
     crab_cfg = '''
 [CRAB]
@@ -29,7 +30,7 @@ datasetpath = %(dataset)s
 pset = GetPrescales.py
 total_number_of_lumis = -1
 number_of_jobs = 50
-lumi_mask = /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-195775_8TeV_PromptReco_Collisions12_JSON_MuonPhys.txt
+lumi_mask = /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-204601_8TeV_PromptReco_Collisions12_JSON_MuonPhys.txt
 
 [USER]
 ui_working_dir = crab/crab_getprescales_%(name)s
@@ -40,7 +41,10 @@ return_data = 1
 
     dataset_details = [
         # ('SingleMu2012A_Prompt', '/SingleMu/Run2012A-PromptReco-v1/AOD'),
-        ('SingleMu2012B_Prompt', '/SingleMu/Run2012B-PromptReco-v1/AOD'),
+        # ('SingleMu2012B_Prompt', '/SingleMu/Run2012B-PromptReco-v1/AOD'),
+        ('SingleMu2012C_24Aug2012', '/SingleMu/Run2012C-24Aug2012-v1/AOD'),
+        ('SingleMu2012C_Prompt',    '/SingleMu/Run2012C-PromptReco-v2/AOD'),
+        ('SingleMu2012D_Prompt',    '/SingleMu/Run2012D-PromptReco-v1/AOD'),
         ]
 
     for name, dataset in dataset_details:

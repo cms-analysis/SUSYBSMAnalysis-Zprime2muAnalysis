bb5f75825fad77320e5753fd464972dd9d1a3b08
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2012-11-23 17:40:49 +0000

    Add comparison of mass resolution vs mass for "tracker only", "Tune P", and re-optimized "Tune P".

--- test/EfficiencyResolutionFromMC/resfromzp.py
+++ test/EfficiencyResolutionFromMC/resfromzp.py
@@ -11,20 +11,18 @@ ROOT.gStyle.SetPadRightMargin(0.02)
 
 ps = plot_saver('plots/resfromzp')
 
-
+res_vs_mass = []
 def drawMassReso(basein='Resolutioninner',refit='tkonly',space='DileptonMassRes',nrms=1.5):
     sigma = []
     esigma = []
-
 
     masses = [750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000]
     chnam, val, err, xlolim, xhilim, iuint = ROOT.TString(''), ROOT.Double(1), ROOT.Double(1), ROOT.Double(1), ROOT.Double(1), ROOT.Long(1)
     baseout = "%s_%s"%(refit,space)
     for m in masses:
-#        f = ROOT.TFile('crab/effres_histos/ana_effres_zp%i.root' % m)
-        f = ROOT.TFile('root/crab_ana_effres_zp%i.root' % m)
+        f = ROOT.TFile('crab/effres_histos/Mu40/ana_effres_zp%i.root' % m)
         h = f.Get(basein).Get(space)
-        fcn = ROOT.TF1('mg', 'gaus', h.GetMean() - nrms*h.GetRMS() , h.GetMean() + nrms*h.GetRMS() )
+        fcn = ROOT.TF1('mg', 'gaus', h.GetMean() - nrms*h.GetRMS(), h.GetMean() + nrms*h.GetRMS() )
         h.Fit(fcn, 'ILVR')
         ps.save('%s_%i' % (baseout, m))
         ROOT.gMinuit.mnpout(2, chnam, val, err, xlolim, xhilim, iuint)
@@ -48,21 +46,56 @@ def drawMassReso(basein='Resolutioninner',refit='tkonly',space='DileptonMassRes'
     g.Fit(poly2)
     g.GetXaxis().SetTitle('dilepton mass (GeV)')
     g.GetYaxis().SetTitle('dilepton relative mass resolution')
-    g.GetYaxis().SetLabelSize(0.02)
+    g.GetYaxis().SetTitleOffset(1.2);
     g.SetTitle('')
     g.Draw("AP")
     ps.c.Update()
     s = g.GetListOfFunctions().FindObject("stats")
     s.SetOptFit(111)
     s.SetY1NDC(0.15)
-    s.SetY2NDC(0.40)
+    s.SetY2NDC(0.35)
     s.SetX1NDC(0.50)
-    s.SetX2NDC(0.95)
+    s.SetX2NDC(0.90)
     ps.c.Update()
     ps.save(baseout, log=False)
+    if space == 'DileptonMassRes':
+        res_vs_mass.append((refit, g))
 
-for basein, baseout in [('Resolutioninner', 'tkonly'), ('Resolutiontunep', 'tunep')]:
+for basein, baseout in [('Resolutioninner', 'tkonly'), ('Resolutiontunep', 'tunep'), ('Resolutiontunepnew', 'tunep_new')]:
     for histname in ['DileptonMassRes','DileptonMassInvRes']:
         if 'Inv' in histname: drawMassReso(basein,baseout,histname,3.0)
         else: drawMassReso(basein,baseout,histname)
 
+#- Compare the resolution given by different methods
+ps.c.Clear()
+# ROOT.gStyle.SetOptFit(0)
+lg = ROOT.TLegend(0.50, 0.15, 0.90, 0.35)
+for refit, graph in res_vs_mass:
+#    s = graph.GetListOfFunctions().FindObject("stats")
+#    s.SetOptFit(0)
+    graph.SetMaximum(0.17)
+    graph.SetMarkerSize(1.0)
+    fcn = graph.GetFunction('poly2')
+    if refit == 'tkonly':
+        graph.SetMarkerStyle(20)
+        graph.SetLineColor(ROOT.kRed)
+        graph.SetMarkerColor(graph.GetLineColor())
+        fcn.SetLineColor(graph.GetLineColor())
+        graph.Draw("AP")
+        lg.AddEntry(graph, 'Tracker-only', 'LPE')
+    if refit == 'tunep':
+        graph.SetMarkerStyle(24)
+        graph.SetLineColor(ROOT.kBlue)
+        graph.SetMarkerColor(graph.GetLineColor())
+        fcn.SetLineColor(graph.GetLineColor())
+        graph.Draw("P same")
+        lg.AddEntry(graph, 'Tune P', 'LPE')
+    if refit == 'tunep_new':
+        graph.SetMarkerStyle(21)
+        graph.SetLineColor(ROOT.kGreen)
+        graph.SetMarkerColor(graph.GetLineColor())
+        fcn.SetLineColor(graph.GetLineColor())
+        graph.Draw("P same")
+        lg.AddEntry(graph, 'New Tune P', 'LPE')
+lg.Draw()
+ps.save('res_vs_mass', log=False)

4ac44e0e7a0f3ebac9106c7a50556b87bcfa9a72
Kurt <randomk91@gmail.com>
2015-07-31 05:44:35 +0200

    no eta

--- python/SkimMiniAOD_cff.py
+++ python/SkimMiniAOD_cff.py
@@ -4,7 +4,7 @@ from math import fabs
 ##process.load('PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi')
 from PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi import*
 selectedPatMuons.src = cms.InputTag('slimmedMuons')
-selectedPatMuons.cut = cms.string('isGlobalMuon && pt>20 && abs(eta)<2.4')
+selectedPatMuons.cut = cms.string('isGlobalMuon && pt>20')
 
 def addUserData(patMuonProducer, tag=cms.InputTag('selectedPatMuons')):
     patMuonProducer.userData.userCands.src.append(tag)

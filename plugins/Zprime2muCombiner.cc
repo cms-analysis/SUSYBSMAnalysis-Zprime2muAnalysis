4d96f5bffd4f09486698b3115256fd5a6abf018c
Jan-Frederik <jschulte@cern.ch>
2018-11-05 16:45:16 +0100

    fix application of electron event selections

--- plugins/Zprime2muCombiner.cc
+++ plugins/Zprime2muCombiner.cc
@@ -8,19 +8,25 @@
 struct Zprime2muPairSelector {
   StringCutObjectSelector<reco::Candidate, true> loose;
   StringCutObjectSelector<reco::Candidate, true> tight;
+  StringCutObjectSelector<reco::Candidate, true> loose_ele;
+  StringCutObjectSelector<reco::Candidate, true> tight_ele;
   const unsigned electron_cut_mask;
   const bool ele_match_l1 = false;
   const std::string module_label;
 
   Zprime2muPairSelector(const std::string& loose_cut,
 			const std::string& tight_cut,
+			const std::string& loose_cut_ele,
+			const std::string& tight_cut_ele,
 			const unsigned el_mask,
 			const bool l1_match, 
 			const std::string& label)
-    : loose(loose_cut),
+    : 
+      loose(loose_cut),
       tight(tight_cut),
+      loose_ele(loose_cut_ele),
+      tight_ele(tight_cut_ele),
       electron_cut_mask(el_mask),
-      ele_match_l1(l1_match),
       module_label(label)
   {}
 
@@ -29,11 +35,6 @@ struct Zprime2muPairSelector {
     if (!el.hasUserInt("cutFor"))
       throw cms::Exception("BadCandidateForLeptonSelector") << "electron does not have cutFor userInt!\n";
     return (el.userInt("cutFor") & electron_cut_mask) == 0;
-  }
-  bool electron_l1(const reco::Candidate& cel) const {
-    const pat::Electron& el = static_cast<const pat::Electron&>(cel);
-    if (!(el.userFloat("L1TriggerMatchPt") > 0)) return false;
-    else return true;
   }
 
   bool operator()(const reco::Candidate& c1, const reco::Candidate& c2) const {
@@ -47,8 +48,7 @@ struct Zprime2muPairSelector {
     const bool e1 = typeid(c1) == typeid(pat::Electron);
     const bool e2 = typeid(c2) == typeid(pat::Electron);
     if (e1 && e2){
-	if (ele_match_l1) return electron_ok(c1) && electron_ok(c2) && (electron_l1(c1) || electron_l1(c2));
-	else              return electron_ok(c1) && electron_ok(c2);
+	return electron_ok(c1) && electron_ok(c2) && loose_ele(c1) && loose_ele(c2) && (tight_ele(c1) || tight_ele(c2));
     }
     else if (e1)
       return electron_ok(c1) && loose(c2) && tight(c2);
@@ -74,8 +74,10 @@ namespace reco {
       static Zprime2muPairSelector make(const edm::ParameterSet& cfg) {
 	return Zprime2muPairSelector(cfg.getParameter<std::string>("loose_cut"),
 				     cfg.getParameter<std::string>("tight_cut"),
+				     cfg.existsAs<std::string>("loose_cut_ele") ? cfg.getParameter<std::string>("loose_cut_ele") : "",
+				     cfg.existsAs<std::string>("tight_cut_ele") ? cfg.getParameter<std::string>("tight_cut_ele") : "",
 				     cfg.existsAs<unsigned>("electron_cut_mask") ? cfg.getParameter<unsigned>("electron_cut_mask") : 0xFFFFFFFF,
-				     cfg.existsAs<unsigned>("ele_match_l1") ? cfg.getParameter<unsigned>("ele_match_l1") : false,
+				     cfg.existsAs<bool>("ele_match_l1") ? cfg.getParameter<bool>("ele_match_l1") : false,
 				     cfg.getParameter<std::string>("@module_label"));
       }
     };
--- python/ElectronSelection_cff.py
+++ python/ElectronSelection_cff.py
@@ -1,14 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-loose_cut = ''
+loose_cut = 'et > 35 && abs(userFloat("etaSC")) < 2.5 && !(abs(userFloat("etaSC")) > 1.4442 && abs(userFloat("etaSC")) < 1.566) && userFloat("HLT_TriggerMatchPt") > 33'
 
-tight_cut = 'userFloat("HLTTriggerMatchPt") > 33'
+tight_cut = 'userFloat("L1_TriggerMatchPt") > 33'
 
 allDielectrons = cms.EDProducer('Zprime2muCombiner',
                             decay = cms.string('leptons:electrons@+ leptons:electrons@-'),
                             cut = cms.string(''),
-                            loose_cut = cms.string(loose_cut),
-                            tight_cut = cms.string(tight_cut),
+			    loose_cut = cms.string(""),
+			    tight_cut = cms.string(""),
+                            loose_cut_ele = cms.string(loose_cut),
+                            tight_cut_ele = cms.string(tight_cut),
 			    ele_match_l1 = cms.bool(True)
                             )
 
